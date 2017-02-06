9b3f3c076c6e368d8105a85ad05815aa94cc81f3
Jan Philipp Dietrich <dietrich@pik-potsdam.de>
2017-02-06 18:04:14 +0100

    - added detailed and trafficlight based validation to output scripts

---
+++ scripts/output/single/validation_trafficlight.R
@@ -0,0 +1,27 @@
+# (C) 2008-2016 Potsdam Institute for Climate Impact Research (PIK),
+# authors, and contributors see AUTHORS file
+# This file is part of MAgPIE and licensed under GNU AGPL Version 3 
+# or later. See LICENSE file or go to http://www.gnu.org/licenses/
+# Contact: magpie@pik-potsdam.de
+
+
+
+library(mip)
+library(magpie4)
+
+############################# BASIC CONFIGURATION #############################
+validation_mif        <- "input/validation.mif"
+
+if(!exists("source_include")) {
+  outputdir    <-"."
+  title<-"dummy"
+}
+###############################################################################
+
+gdx<-path(outputdir,"fulldata.gdx")
+x <- getReport(gdx)
+
+validationpdf(x=x, hist=validation_mif, file=path(outputdir,paste0("tlval_",title,".pdf")), style="trafficlight")
+
+  
+
