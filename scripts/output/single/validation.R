763900108da1c3decefb8f33d39113e8e0e2dbfd
dietrich <dietrich@pik-potsdam.de>
2017-06-19 10:17:57 +0200

    bugfix in validation.R (wrong runinfo has been linked so far)

--- scripts/output/single/validation.R
+++ scripts/output/single/validation.R
@@ -1,6 +1,6 @@
 # (C) 2008-2017 Potsdam Institute for Climate Impact Research (PIK),
 # authors, and contributors see AUTHORS file
-# This file is part of MAgPIE and licensed under GNU AGPL Version 3 
+# This file is part of MAgPIE and licensed under GNU AGPL Version 3
 # or later. See LICENSE file or go to http://www.gnu.org/licenses/
 # Contact: magpie@pik-potsdam.de
 
@@ -11,12 +11,11 @@ if(!exists("source_include")) {
   outputdir    <-"."
 }
 load(paste0(outputdir, "/config.Rdata"))
-hist    <- c(paste0(outputdir, "/validation.mif"), "input/validation.mif")
-file    <- paste0(outputdir, "/", cfg$title, "_validation.pdf")
+hist       <- c(paste0(outputdir, "/validation.mif"), "input/validation.mif")
+file       <- paste0(outputdir, "/", cfg$title, "_validation.pdf")
 reportfile <- paste0(outputdir, "/", cfg$title, "_report.mif")
-gdx     <- paste0(outputdir, "/fulldata.gdx")
-runinfo <- Sys.glob("output/bau__default/*.RData")[1]
-if(is.na(runinfo)) runinfo <- NULL
+gdx        <- paste0(outputdir, "/fulldata.gdx")
+runinfo    <- paste0(outputdir, "/", cfg$title, ".RData")
 ###############################################################################
 
 #### Choose validation data ###
