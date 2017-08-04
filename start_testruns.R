3408747780adfd914e83b2d7dc97bd139e115070
dietrich <dietrich@pik-potsdam.de>
2017-08-04 13:20:37 +0100

    regioncode for MAgPIE mapping changed due to change in mapping itself (move of REU from SAS to AFR)

--- start_testruns.R
+++ start_testruns.R
@@ -27,7 +27,7 @@ buildInputVector <- function(regionmapping   = "h11",
                              additional_data = "additional_data_rev2.01.tgz") {
   mappings <- c(h11="8a828c6ed5004e77d1ba2025e8ea2261",
                 h12="690d3718e151be1b450b394c1064b1c5",
-                mag="2da01c534a80df894e0384d999e415d4")
+                mag="c30c1c580039c2b300d86cc46ff4036a")
   archive <- paste0(archive_name, "_rev", archive_rev, "_", resolution, "_", mappings[regionmapping], ".tgz")
   madrat  <- paste0("magpie_", mappings[regionmapping], "_rev", madrat_rev, ".tgz")
   return(c(archive,madrat,additional_data))
