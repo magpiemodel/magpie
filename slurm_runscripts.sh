1d37e8a7d0f38c76ebc2ebb1dc30104aae50383a
bodirsky <bodirsky@pik-potsdam.de>
2017-09-22 16:12:19 +0200

    bugfix

--- slurm_runscripts.sh
+++ slurm_runscripts.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-#SBATCH --qos=short
-#SBATCH --job-name=mag-submit
-#SBATCH --output=magpietest-%j.out
-#SBATCH --mail-type=END
-#SBATCH --mem-per-cpu=32000
+echo "select one of the following runscripts by name:"
 
-echo select one of the following runscripts by name:
 ls scripts/runscripts/
 read runscript
-Rscript scripts/runscripts/"$runscript" 
+
+echo "test $runscript"
+sbatch  --qos=short --job-name=mag-submit --output=magpietest-%j.out --mail-type=END --mem-per-cpu=32000 Rscript scripts/runscripts/"$runscript"
