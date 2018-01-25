a6623cde1b3aa7e332602ab6ee16715729ff8620
Kristine Karstens <karstens@pik-potsdam.de>
2018-01-25 15:31:24 +0100

    add start-script for fix_som
    Former-commit-id: 7a0e39898cfc24e2a1c2b735af82b4eb936a8ac7 [formerly 4a4f74d326e97f2104583c9983f689a033943a13] [formerly 442c0e5686c88b33cb179ac0e52bd8d213766c8f] [formerly 7ebfb6c1d38edc86561d508191fd1e3c696d88af [formerly 560ebdd11c06ff27d5d50c21e77779829429be89] [formerly 442c0e5686c88b33cb179ac0e52bd8d213766c8f [formerly 85a1351a0891137717ced30f0cbc4a07177dbcff]]] [formerly c844febd1b1ed69868f8e6fb3bf8d8ef20d1f5be [formerly 871f40ee1a43bd92eee10c3df833853ac14a0057] [formerly 5e2120121c724ab6dd1116af745e9a237fadbd24] [formerly 0b966c64f695feb102768f806b2ded4de9698a51 [formerly 37273e3852061eac46a2f0619951b0dcfb35134a] [formerly 5e2120121c724ab6dd1116af745e9a237fadbd24 [formerly 794ddecc0795ffd897f6c0b628367c2abf01f781]]]] [formerly 10ebdfee79ab086cb0e0672ace3d04d91e53b384 [formerly dc36d91dc780f1f358bb4eb8f9a86a25f6bb2e47] [formerly 9e0cc1be168db54ae43b4fd51936e138919ae07e] [formerly 682ee036693cab3802f885972026f1f29f3273ae [formerly 331f9104ba4635f5d0d245ef6b73122e126ad739] [formerly 9e0cc1be168db54ae43b4fd51936e138919ae07e [formerly a6c955e4aa86c0a56aba7b6bbfdaea39eba17851]]] [formerly 7ef625d2bf6e06613a62301d80a9dcee743d1a4b [formerly a380d375ffa0d8de8936ac722932f02ad504c766] [formerly 7eeef9af06a5e4b9808ff8807806a87b1876efee] [formerly d11da7f5e63241f7c385a6c8d663026953cc38f8 [formerly 7ef72817afd996b937437db8d9312487e5b0ac2d] [formerly 7eeef9af06a5e4b9808ff8807806a87b1876efee [formerly 1ee15fc8a513c78719bbd244e7d3674cfc9c48b9]]]]]
    Former-commit-id: 4a2f13426ddf3eaed9e1953031b7c648b96a1432 [formerly af199a5f9a2b07704f344d6ab4e8bcce8d77014e] [formerly 0f7890c013397d4da2f7b7fdee79592409b92f66] [formerly f83c9eff8fef36b577ebb77426b373e63a44425d [formerly 2ba85e1814c7acf7fa4dac74e3f96fd7c5edb640] [formerly 0f7890c013397d4da2f7b7fdee79592409b92f66 [formerly f85fc9b0dcf26a0ef12ab538e87323e5303dc143]]] [formerly 2d233ab8032cc10e235715736e034e6f3275cb25 [formerly 6f521a7597a71f5e6db5fe7902d2c732ac63feef] [formerly 82c9d256eb51847535fe4637f4e5a358cea1d692] [formerly 29ba774b9451b2378a841d2fec58077428602652 [formerly bb120c00840b0dee5b31deeee30def8404608627] [formerly 82c9d256eb51847535fe4637f4e5a358cea1d692 [formerly dfbe70e4d92d36ab0039a5f145a3b8fe9df0f69d]]]]
    Former-commit-id: 4cdc5d909736d5ae52f0fc32e2fb5ed89f81c369 [formerly 0254bc5f2f0bb7f4cc069abcce0d628b01e6f390] [formerly dc8bba34227d24f2fbf39cc7ae96846721c1892e] [formerly 1a786c904f17960a14e5c4a5fa8875fa770123d1 [formerly d67d07187db07905ec473cfe8f2ecfd38b5904c1] [formerly dc8bba34227d24f2fbf39cc7ae96846721c1892e [formerly 79c3adda32eabbc613fbb04855c39b263351f897]]]
    Former-commit-id: 01de997bf7d78f2dcd057aaba663440bed8889d2 [formerly 080e979e3345a33e9d94da23af4f8a020573ff19] [formerly 17219b2c81c839b0510ef415e356729657686b4f [formerly d0ee4425fb8b4a6d7b12e6513259f06bfc2b6f40]]
    Former-commit-id: 17219b2c81c839b0510ef415e356729657686b4f
    Former-commit-id: b801860bf72236dfea4a1d46ace6bd0afe305415
    Former-commit-id: c00b24b3ca6c80d364170d8aadd15bf8230118d1

---
+++ scripts/start/fix_som.R
@@ -0,0 +1,24 @@
+# |  (C) 2008-2018 Potsdam Institute for Climate Impact Research (PIK),
+# |  authors, and contributors see AUTHORS file
+# |  This file is part of MAgPIE and licensed under GNU AGPL Version 3
+# |  or later. See LICENSE file or go to http://www.gnu.org/licenses/
+# |  Contact: magpie@pik-potsdam.de
+
+
+##########################################################
+#### Script to MAgPIE test runs ####
+##########################################################
+
+library(lucode)
+source("scripts/start_functions.R")
+source("config/default.cfg")
+
+#general settings
+cfg$gms$c_timesteps <- 7
+
+cfg$title <- "fix_som_test"
+cfg$gms$som <- "cellpool_aug16"
+
+start_run(cfg=cfg,codeCheck=TRUE)
+
+
