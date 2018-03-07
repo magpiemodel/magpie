bd0a0529f8878fb54ce48de410813b2dafb17135
florianh <humpenoeder@pik-potsdam.de>
2018-03-07 19:16:41 +0100

    update of start_run script
    Former-commit-id: d04f1aeb24ed7e0b8ba44935752952238dd6b0af [formerly 4ede9e7e5a8d6016a40dbbc2c4e6f356e0a8c884] [formerly 4ec941480ac00909e65b3e6f45f0263fba045851] [formerly 77417ef2f58ab3c9bd1713596e7c3ff7809022f9 [formerly 0807d0ea8fc140d96f0ed6a08888af0fc1e445fb] [formerly 4ec941480ac00909e65b3e6f45f0263fba045851 [formerly 00b64d60686fa3390d5faf7a0d53ae6b1db1ef86]]] [formerly af70381a81da6f7cee6f6d446980890a254a1f1d [formerly 5776c03300936a40f9b4b2a0c1de4b0be39932c1] [formerly 859a5546f9e82dbd09bc61e023414adff4483e6a] [formerly caa0e2ff48797100467394dd1939f87c23b6f8c8 [formerly 99fba72997ed18ceea89ddd2dc00ff68e2509b08] [formerly 859a5546f9e82dbd09bc61e023414adff4483e6a [formerly a779b45be92ae54c91e3856cc33aff59a96b6d9c]]]] [formerly 7aee8290aa4b5492daa1be8c4e2d375c44d24a75 [formerly 562b8f014314cff759e25de2db636d97d1cd27bf] [formerly e486053ef22841812fd877249b430e1b55c12238] [formerly cc030190e1b7b1971ae1c78e1f1086454c430b72 [formerly 6c1c7065270551beef474cfd421ffe224b07f815] [formerly e486053ef22841812fd877249b430e1b55c12238 [formerly 3acabbe0a92c919b07611990c3bda09c097a1564]]] [formerly 7d6c9c42feb8c67ba54f43380eca36e7083693f8 [formerly 1bc4e30a617d1a4496a0f990de99f181759671c3] [formerly 90f49165b7cd06e6d1cb725a6d2ca7d0ab5db90e] [formerly 711089cad142d249e3e474fb599a45b8d5a4d81c [formerly d33f7b34a58808afbcb82b036b26edf19aebf0cd] [formerly 90f49165b7cd06e6d1cb725a6d2ca7d0ab5db90e [formerly 90f49165b7cd06e6d1cb725a6d2ca7d0ab5db90e [formerly 35628ab59f87dec108b4a204f608b2406f6f0a6e]]]]]]
    Former-commit-id: 704f5e5bcbc8f20eaa11ef2cbc5b2d07ec0f8dea [formerly 9cdb2694d6d6a6f8fff3c2c6467d07ea10b70b4c] [formerly 609112221add7ef5c6f971bb4a7886f1658fdf88] [formerly a0383603a548b0d2075fff03d355f8a327feb8e1 [formerly 23134ee2d519986eda01ec664e1ecddca9fead01] [formerly 609112221add7ef5c6f971bb4a7886f1658fdf88 [formerly 54de46138cb44f5ff25e942d6145cd4bfab94494]]] [formerly 929f1fc800ef5d02111e255733e22cfcac6ae1e9 [formerly 47b4ee65ea79933a86a7d4129a72d80cff13ba06] [formerly 27e3d9551f751e0d0bc9c43d46ae0cd19e481710] [formerly 6d1e06d5d42e737bca16dad604b590afddcd5370 [formerly 80f7d46307abbd60f24583557332534f1b67a0ca] [formerly 27e3d9551f751e0d0bc9c43d46ae0cd19e481710 [formerly efea69b39647b87654ba22e088ad0d58f86ed0c0]]]]
    Former-commit-id: 87ad3f19633e9ee072f94dc6fde97d7005637b78 [formerly 83a52bccd7f2c8f740c779bb5c6c6e334b93c927] [formerly 703180c53abe34824a23f7db0c3dc27d44e14f9a] [formerly e50eabde1d7e2d9af0ab9d6113f5e0b617845a7a [formerly 0971f29b66508812ceae30c3ba2b30bf236357ba] [formerly 703180c53abe34824a23f7db0c3dc27d44e14f9a [formerly 918f87ce959338d94da53f92b6affa790f39a06a]]]
    Former-commit-id: 81ab42d26ef9fba03afffdf8aa06bc425ae80aba [formerly a64ce1cb0faf2751093bf2eb3bc89008508d5704] [formerly 3b113bdf58025acaf65c16311cea75fabe20a4fc [formerly 9b25925f3f6d42e7782a5c24306c10ab468ce608]]
    Former-commit-id: 3b113bdf58025acaf65c16311cea75fabe20a4fc
    Former-commit-id: b6e04c6bb4758b3b59041a675cac1bf0c16e4f17
    Former-commit-id: 1d8a1c72158472d1440a00d745736cd7e0abcfa6

--- scripts/start/land_conversion_costs.R
+++ scripts/start/land_conversion_costs.R
@@ -9,6 +9,8 @@
 #### Script to start a MAgPIE run ####
 ######################################
 
+library(lucode)
+
 # Load start_run(cfg) function which is needed to start MAgPIE runs
 source("scripts/start_functions.R")
 source("config/default.cfg")
@@ -16,11 +18,32 @@ source("config/default.cfg")
 cfg$results_folder <- "output/:title:"
 cfg$gms$c_timesteps <- "test_TS"
 
+cfg$recalibrate <- TRUE
+
 cfg$title <- "gdp_scaled_jun13"
 cfg$gms$landconversion <- "gdp_scaled_jun13"
 try(start_run(cfg=cfg, codeCheck=FALSE))
 
-cfg$title <- "gdp_vegetation_dev"
+cfg$recalibrate <- FALSE
+
+cfg$title <- "gdp_vegc_medium"
+manipulateConfig("modules/39_landconversion/gdp_vegetation_dev/input.gms",c39_cost_scenario="medium")
 cfg$gms$landconversion <- "gdp_vegetation_dev"
 try(start_run(cfg=cfg, codeCheck=FALSE))
 
+cfg$title <- "gdp_vegc_high"
+manipulateConfig("modules/39_landconversion/gdp_vegetation_dev/input.gms",c39_cost_scenario="high")
+cfg$gms$landconversion <- "gdp_vegetation_dev"
+try(start_run(cfg=cfg, codeCheck=FALSE))
+
+cfg$recalibrate <- TRUE
+
+cfg$title <- "gdp_vegc_medium_recal"
+manipulateConfig("modules/39_landconversion/gdp_vegetation_dev/input.gms",c39_cost_scenario="medium")
+cfg$gms$landconversion <- "gdp_vegetation_dev"
+try(start_run(cfg=cfg, codeCheck=FALSE))
+
+cfg$title <- "gdp_vegc_high_recal"
+manipulateConfig("modules/39_landconversion/gdp_vegetation_dev/input.gms",c39_cost_scenario="high")
+cfg$gms$landconversion <- "gdp_vegetation_dev"
+try(start_run(cfg=cfg, codeCheck=FALSE))
