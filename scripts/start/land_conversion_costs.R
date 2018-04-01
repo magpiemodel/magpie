56b46cf5e2f0f9511d4dbc1f23df37e631c06730
florianh <humpenoeder@pik-potsdam.de>
2018-04-01 18:19:11 +0200

    update of scripts
    Former-commit-id: 66a71bd89784f0a05937cbdb3693e4a173d66275 [formerly ac29141461d93502b296ea4569b9a847be6d475e] [formerly c4c70a993f363c7f2eb698131741aa1c87f316dc] [formerly b95fe2c67359a460d73e048813d0a9cef10e6e81 [formerly 8a47461f81b08f82ea21bde916acde874a22b453] [formerly c4c70a993f363c7f2eb698131741aa1c87f316dc [formerly a2b18ff9919ea9daf5f0fd14eaf7b190c705b744]]] [formerly deea0b2f6f56d68a16d17636203a6d77f3060a0a [formerly 379e8136525722a0c1672abb50750b9b95043c1e] [formerly 1440b67a731483b3d660ab109e9c1bb63f29da33 [formerly 77f635cd184a1b4f508050609a3a9f7b5eb9b49e]] [formerly c8bbf297d1241ff482bc4737df7b4efd3e63cb93 [formerly e2ced124178f4c587718e42de703e4fd553558f6] [formerly 77f635cd184a1b4f508050609a3a9f7b5eb9b49e [formerly a8b83e5f125ceb7c448ca42873340eef3aa13085]]]] [formerly 36682f2e075f6f5a62c4be2e79aa28f1966efaee [formerly c74f8173d966c4d86fdf70eeaec8bf4d8bc26d14] [formerly 3a1e164b3e9e4ebf71e9c0da686c9a8e36454c17 [formerly 392079baccbac7dc2fef5b480a4fcd9bc3d39693]] [formerly 45eb14337f9068b44c8183d0655576468cbe57a6 [formerly 5814a754d54ce8356041a94f170b83f80558f788] [formerly 392079baccbac7dc2fef5b480a4fcd9bc3d39693 [formerly 21fef28d7085210a4ca1ab790fe4d081d0ba87f0]]] [formerly 02022e2dc35b845b47a30c13f83c11d6f09ecb6e [formerly d12b039b80bac3b007da9b6d3337cd9e6f34800e] [formerly 2228d614ca609d6979ca744a95cec63014aa5473] [formerly 7463b6bca68edd531d781ce4a535e9b88caa5197 [formerly 4e2fba8bd4aa88c21cd423229ee2282723884cfd] [formerly 2228d614ca609d6979ca744a95cec63014aa5473 [formerly 2228d614ca609d6979ca744a95cec63014aa5473 [formerly 33f60a9a3823f5eb381bd6284676f05fae05c6fc]]]]]]
    Former-commit-id: 95f7c61a89dc16e303cb8b6213172875e397e6ce [formerly 6a4d5237eee10b0d8004d594f394f818b3ee4fca] [formerly c707caf2760c1b3e2f62ae40cfcc91c1614857cc] [formerly 63c8e2a8299a0ee8435f437f8740b494474aa424 [formerly 3c3732ebdb066ee7277823d7266b2d3bb8149c05] [formerly c707caf2760c1b3e2f62ae40cfcc91c1614857cc [formerly 90388ab07e27f3ef0053a14f1732ab95ced4c802]]] [formerly ee793ecfa23fd438f2942aca09c6c462376a6785 [formerly 527caf523f0b3db3cb1e692a617b33f561d496f8] [formerly 05518b0005c188673df3ca481d7fcd659b83c248] [formerly 616e93b67ad729b09991703a466dd5ef8cef86f1 [formerly 1af7e2c2268c1c41c1e4f2898d06a43db3d3b3e3] [formerly 05518b0005c188673df3ca481d7fcd659b83c248 [formerly 0edf098100fb99bec7d7e7c229394c1e90fd2b4d]]]]
    Former-commit-id: de650c3a44b2700e7a1ea72a08a8a6ad9b05eb7f [formerly a74526b43d41719368e9381ffadfde8f861581d6] [formerly efb061973aa95af714350596b6c16ee192842889] [formerly d4ef78b5c08ce424cf5bbeea30e55ffad083a908 [formerly fbe74d28c054485f63540204f7bc226ed6b46760] [formerly efb061973aa95af714350596b6c16ee192842889 [formerly fd9c6c4ebd973ef95840cb2e94695ab4ee991777]]]
    Former-commit-id: 810ca4d2e5a7433a454b4514dade8a9445d5dda4 [formerly 42a424702572dd8b0dd6e45ece04aa1c9fcb6012] [formerly 31f512a84fb6338a8ff24344e23a2b0a23c80213 [formerly d62cc82af02479d7dd488bf6d81dbaa04721e183]]
    Former-commit-id: 31f512a84fb6338a8ff24344e23a2b0a23c80213
    Former-commit-id: ca0360ba23cc4068b8cdee9fd7b502ac1350cdc4
    Former-commit-id: dcd8d08fd9c91b46c3d8d7393efb66c31b7e17a7

--- scripts/start/land_conversion_costs.R
+++ scripts/start/land_conversion_costs.R
@@ -17,21 +17,58 @@ source("config/default.cfg")
 
 cfg$results_folder <- "output/:title:"
 cfg$gms$c_timesteps <- "test_TS"
+#cfg$gms$s15_elastic_demand = 0
+#cfg$force_download <- TRUE
 
 cfg$recalibrate <- TRUE
+cfg$title <- "gdp_vegc_high"
+cfg$gms$c39_cost_scenario <- "high"
+cfg$gms$landconversion <- "gdp_vegc_mar18"
+try(start_run(cfg=cfg, codeCheck=FALSE))
+cfg$recalibrate <- FALSE
 
-cfg$title <- "gdp_vegc_low"
-manipulateConfig("modules/39_landconversion/gdp_vegc_mar18/input.gms",c39_cost_scenario="low")
+cfg$title <- "gdp_vegc_high2"
+cfg$gms$c39_cost_scenario <- "high2"
 cfg$gms$landconversion <- "gdp_vegc_mar18"
 try(start_run(cfg=cfg, codeCheck=FALSE))
 
 cfg$title <- "gdp_vegc_medium"
-manipulateConfig("modules/39_landconversion/gdp_vegc_mar18/input.gms",c39_cost_scenario="medium")
+cfg$gms$landconversion <- "gdp_vegc_mar18"
+cfg$gms$c39_cost_scenario <- "medium"
+try(start_run(cfg=cfg, codeCheck=FALSE))
+
+cfg$title <- "gdp_vegc_low"
+cfg$gms$landconversion <- "gdp_vegc_mar18"
+cfg$gms$c39_cost_scenario <- "low"
+try(start_run(cfg=cfg, codeCheck=FALSE))
+
+cfg$gms$c_timesteps <- "TS_benni"
+cfg$title <- "gdp_vegc_high_y2000"
+cfg$gms$c39_cost_scenario <- "high"
+cfg$gms$landconversion <- "gdp_vegc_mar18"
+try(start_run(cfg=cfg, codeCheck=FALSE))
+cfg$gms$c_timesteps <- "test_TS"
+
+
+cfg$recalibrate <- TRUE
+
+cfg$title <- "gdp_vegc_high_recal"
+cfg$gms$c39_cost_scenario <- "high"
 cfg$gms$landconversion <- "gdp_vegc_mar18"
 try(start_run(cfg=cfg, codeCheck=FALSE))
 
-cfg$title <- "gdp_vegc_high"
-manipulateConfig("modules/39_landconversion/gdp_vegc_mar18/input.gms",c39_cost_scenario="high")
+cfg$title <- "gdp_vegc_high2_recal"
+cfg$gms$c39_cost_scenario <- "high2"
 cfg$gms$landconversion <- "gdp_vegc_mar18"
+try(start_run(cfg=cfg, codeCheck=FALSE))
+
+cfg$title <- "gdp_vegc_medium_recal"
+cfg$gms$landconversion <- "gdp_vegc_mar18"
+cfg$gms$c39_cost_scenario <- "medium"
+try(start_run(cfg=cfg, codeCheck=FALSE))
+
+cfg$title <- "gdp_vegc_low_recal"
+cfg$gms$landconversion <- "gdp_vegc_mar18"
+cfg$gms$c39_cost_scenario <- "low"
 try(start_run(cfg=cfg, codeCheck=FALSE))
 
