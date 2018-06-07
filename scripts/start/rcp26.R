eb11dbe30f1f0d3e9cce220315df47064e69f2c1
florianh <humpenoeder@pik-potsdam.de>
2018-06-07 06:16:29 +0200

    added start script
    Former-commit-id: fe82317082d39bc8ce7a9b0a8f2f760af5ed56f5

---
+++ scripts/start/rcp26.R
@@ -0,0 +1,31 @@
+# |  (C) 2008-2018 Potsdam Institute for Climate Impact Research (PIK),
+# |  authors, and contributors see AUTHORS file
+# |  This file is part of MAgPIE and licensed under GNU AGPL Version 3
+# |  or later. See LICENSE file or go to http://www.gnu.org/licenses/
+# |  Contact: magpie@pik-potsdam.de
+
+
+######################################
+#### Script to start a MAgPIE run ####
+######################################
+
+library(lucode)
+library(magclass)
+
+# Load start_run(cfg) function which is needed to start MAgPIE runs
+source("scripts/start_functions.R")
+
+#start MAgPIE run
+source("config/default.cfg")
+cfg$results_folder <- "output/:title:"
+cfg$gms$c_timesteps <- "coup2100"
+
+cfg$title <- "SSP2-Ref-SPA0"
+cfg$gms$c56_pollutant_prices <- "SSP2-Ref-SPA0"
+cfg$gms$c60_2ndgen_biodem <- "SSP2-Ref-SPA0"
+start_run(cfg,codeCheck=FALSE)
+
+cfg$title <- "SSP2-26-SPA0"
+cfg$gms$c56_pollutant_prices <- "SSP2-26-SPA0"
+cfg$gms$c60_2ndgen_biodem <- "SSP2-26-SPA0"
+start_run(cfg,codeCheck=FALSE)
