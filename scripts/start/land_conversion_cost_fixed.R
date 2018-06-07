8309fa9f860b9ee4435cf84c91bc8a78c0ea680c
florianh <humpenoeder@pik-potsdam.de>
2018-06-07 15:03:05 +0200

    update starting scripts
    Former-commit-id: 423c82e93ca5760dbccf787598a183a5f39a99a8

--- scripts/start/land_conversion_cost_fixed.R
+++ scripts/start/land_conversion_cost_fixed.R
@@ -20,23 +20,25 @@ source("config/default.cfg")
 cfg$results_folder <- "output/:title:"
 cfg$gms$c_timesteps <- "coup2100"
 cfg$recalibrate <- TRUE
+cfg$gms$c80_nlp_solver <- "conopt4"
+cfg$gms$s80_optfile <- 0
 
 cfg$gms$factor_costs <- "fixed_per_ton_mar18"
 
-for(low in c(1000,2000,4000,5000,6000,8000)) {
-  for (high in c(10000,12000,14000,16000,18000,20000)) {
+for(low in c(250,500,1000,2000,4000,5000,6000)) {
+  for (high in c(8000,10000,12000,14000,20000)) {
     cfg$title <- paste("lcost",low,high,"fixed",sep="_")
     cfg$gms$landconversion <- "gdp_vegc_may18"
     a <- read.magpie("modules/39_landconversion/gdp_vegc_may18/input/f39_landclear_gdp.csv")
-    a[,,"high_estimate.low_gdp"] <- low
-    a[,,"high_estimate.high_gdp"] <- high
+    a[,,"medium_estimate.low_gdp"] <- low
+    a[,,"medium_estimate.high_gdp"] <- high
     write.magpie(a,"modules/39_landconversion/gdp_vegc_may18/input/f39_landclear_gdp.cs3")
     file.rename("modules/39_landconversion/gdp_vegc_may18/input/f39_landclear_gdp.cs3","modules/39_landconversion/gdp_vegc_may18/input/f39_landclear_gdp.csv")
     a <- read.magpie("modules/39_landconversion/gdp_vegc_may18/input/f39_establish_gdp.cs3")
-    a[,,"crop.high_estimate.low_gdp"] <- low
-    a[,,"crop.high_estimate.high_gdp"] <- high
-    a[,,"past.high_estimate.low_gdp"] <- low/2
-    a[,,"past.high_estimate.high_gdp"] <- high/2
+    a[,,"crop.medium_estimate.low_gdp"] <- low
+    a[,,"crop.medium_estimate.high_gdp"] <- high
+    a[,,"past.medium_estimate.low_gdp"] <- low/2
+    a[,,"past.medium_estimate.high_gdp"] <- high/2
     write.magpie(a,"modules/39_landconversion/gdp_vegc_may18/input/f39_establish_gdp.cs3")
     start_run(cfg,codeCheck=FALSE)
     cfg$recalc_npi_ndc <- FALSE
--- scripts/start/rcp26.R
+++ scripts/start/rcp26.R
@@ -24,12 +24,12 @@ cfg$gms$c_timesteps <- "coup2100"
 cfg$gms$c80_nlp_solver <- "conopt4"
 cfg$gms$s80_optfile <- 1
 
-cfg$title <- "t2_SSP2-Ref_conopt4_opt1"
+cfg$title <- "t3_SSP2-Ref_conopt4_opt1"
 cfg$gms$c56_pollutant_prices <- "SSP2-Ref-SPA0"
 cfg$gms$c60_2ndgen_biodem <- "SSP2-Ref-SPA0"
 start_run(cfg,codeCheck=FALSE)
 
-cfg$title <- "t2_SSP2-26_conopt4_opt1"
+cfg$title <- "t3_SSP2-26_conopt4_opt1"
 cfg$gms$c56_pollutant_prices <- "SSP2-26-SPA0"
 cfg$gms$c60_2ndgen_biodem <- "SSP2-26-SPA0"
 start_run(cfg,codeCheck=FALSE)
@@ -38,12 +38,12 @@ start_run(cfg,codeCheck=FALSE)
 cfg$gms$c80_nlp_solver <- "conopt4"
 cfg$gms$s80_optfile <- 0
 
-cfg$title <- "t2_SSP2-Ref_conopt4_opt0"
+cfg$title <- "t3_SSP2-Ref_conopt4_opt0"
 cfg$gms$c56_pollutant_prices <- "SSP2-Ref-SPA0"
 cfg$gms$c60_2ndgen_biodem <- "SSP2-Ref-SPA0"
 start_run(cfg,codeCheck=FALSE)
 
-cfg$title <- "t2_SSP2-26_conopt4_opt0"
+cfg$title <- "t3_SSP2-26_conopt4_opt0"
 cfg$gms$c56_pollutant_prices <- "SSP2-26-SPA0"
 cfg$gms$c60_2ndgen_biodem <- "SSP2-26-SPA0"
 start_run(cfg,codeCheck=FALSE)
@@ -52,12 +52,28 @@ start_run(cfg,codeCheck=FALSE)
 cfg$gms$c80_nlp_solver <- "conopt4+conopt3"
 cfg$gms$s80_optfile <- 1
 
-cfg$title <- "t2_SSP2-Ref_conopt43_opt1"
+cfg$title <- "t3_SSP2-Ref_conopt43_opt1"
 cfg$gms$c56_pollutant_prices <- "SSP2-Ref-SPA0"
 cfg$gms$c60_2ndgen_biodem <- "SSP2-Ref-SPA0"
 start_run(cfg,codeCheck=FALSE)
 
-cfg$title <- "t2_SSP2-26_conopt43_opt1"
+cfg$title <- "t3_SSP2-26_conopt43_opt1"
+cfg$gms$c56_pollutant_prices <- "SSP2-26-SPA0"
+cfg$gms$c60_2ndgen_biodem <- "SSP2-26-SPA0"
+start_run(cfg,codeCheck=FALSE)
+
+
+cfg$gms$c80_nlp_solver <- "conopt4"
+cfg$gms$s80_optfile <- 0
+
+cfg$gms$c_timesteps <- "coup2100_old"
+
+cfg$title <- "t3_SSP2-Ref_conopt4_opt0_no2000"
+cfg$gms$c56_pollutant_prices <- "SSP2-Ref-SPA0"
+cfg$gms$c60_2ndgen_biodem <- "SSP2-Ref-SPA0"
+start_run(cfg,codeCheck=FALSE)
+
+cfg$title <- "t3_SSP2-26_conopt4_opt0_no2000"
 cfg$gms$c56_pollutant_prices <- "SSP2-26-SPA0"
 cfg$gms$c60_2ndgen_biodem <- "SSP2-26-SPA0"
 start_run(cfg,codeCheck=FALSE)
