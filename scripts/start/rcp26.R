1c84477dbc597346828c228972677deae1d31ecb
florianh <humpenoeder@pik-potsdam.de>
2018-06-07 13:10:31 +0200

    update start script
    Former-commit-id: 79ee6907c41685654da0825fb980585fbce210e6

--- scripts/start/rcp26.R
+++ scripts/start/rcp26.R
@@ -22,26 +22,42 @@ cfg$gms$c_timesteps <- "coup2100"
 
 
 cfg$gms$c80_nlp_solver <- "conopt4"
+cfg$gms$s80_optfile <- 1
 
-cfg$title <- "SSP2-Ref_conopt4"
+cfg$title <- "t2_SSP2-Ref_conopt4_opt1"
 cfg$gms$c56_pollutant_prices <- "SSP2-Ref-SPA0"
 cfg$gms$c60_2ndgen_biodem <- "SSP2-Ref-SPA0"
 start_run(cfg,codeCheck=FALSE)
 
-cfg$title <- "SSP2-26_conopt4"
+cfg$title <- "t2_SSP2-26_conopt4_opt1"
+cfg$gms$c56_pollutant_prices <- "SSP2-26-SPA0"
+cfg$gms$c60_2ndgen_biodem <- "SSP2-26-SPA0"
+start_run(cfg,codeCheck=FALSE)
+
+
+cfg$gms$c80_nlp_solver <- "conopt4"
+cfg$gms$s80_optfile <- 0
+
+cfg$title <- "t2_SSP2-Ref_conopt4_opt0"
+cfg$gms$c56_pollutant_prices <- "SSP2-Ref-SPA0"
+cfg$gms$c60_2ndgen_biodem <- "SSP2-Ref-SPA0"
+start_run(cfg,codeCheck=FALSE)
+
+cfg$title <- "t2_SSP2-26_conopt4_opt0"
 cfg$gms$c56_pollutant_prices <- "SSP2-26-SPA0"
 cfg$gms$c60_2ndgen_biodem <- "SSP2-26-SPA0"
 start_run(cfg,codeCheck=FALSE)
 
 
 cfg$gms$c80_nlp_solver <- "conopt4+conopt3"
+cfg$gms$s80_optfile <- 1
 
-cfg$title <- "SSP2-Ref_conopt43"
+cfg$title <- "t2_SSP2-Ref_conopt43_opt1"
 cfg$gms$c56_pollutant_prices <- "SSP2-Ref-SPA0"
 cfg$gms$c60_2ndgen_biodem <- "SSP2-Ref-SPA0"
 start_run(cfg,codeCheck=FALSE)
 
-cfg$title <- "SSP2-26_conopt43"
+cfg$title <- "t2_SSP2-26_conopt43_opt1"
 cfg$gms$c56_pollutant_prices <- "SSP2-26-SPA0"
 cfg$gms$c60_2ndgen_biodem <- "SSP2-26-SPA0"
 start_run(cfg,codeCheck=FALSE)
