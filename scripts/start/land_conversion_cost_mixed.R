41fbdafe6e6bf022fda916e2d1056959bacf7857
florianh <humpenoeder@pik-potsdam.de>
2018-06-07 15:07:52 +0200

    bugfix
    Former-commit-id: ac7627e35000814439facbbafca4592348a8d7df

--- scripts/start/land_conversion_cost_mixed.R
+++ scripts/start/land_conversion_cost_mixed.R
@@ -10,6 +10,7 @@
 ######################################
 
 library(lucode)
+library(magclass)
 
 # Load start_run(cfg) function which is needed to start MAgPIE runs
 source("scripts/start_functions.R")
