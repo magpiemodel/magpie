4f99fd036f0d331f0d923537ab8eda139f6e1540
Jan Philipp Dietrich <dietrich@pik-potsdam.de>
2018-06-28 15:45:38 +0200

    fixed bugs in calibration script and added additional log calib_divergence.cs3

--- scripts/calibration/calc_calib.R
+++ scripts/calibration/calc_calib.R
@@ -10,24 +10,24 @@
 # *********************************************************************
 
 calibration_run<-function(putfolder,calib_magpie_name,logoption=3){
-
+  
   require(lucode)
-
+  
   # create putfolder for the calib run
   unlink(putfolder,recursive=TRUE)
   dir.create(putfolder)
-
+  
   # create a modified magpie.gms for the calibration run
   unlink(paste(calib_magpie_name,".gms",sep=""))
   unlink("fulldata.gdx")
-
+  
   if(!file.copy("main.gms",paste(calib_magpie_name,".gms",sep=""))){
     stop(paste("Unable to create",paste(calib_magpie_name,".gms",sep="")))
   }
   manipulateConfig(paste(calib_magpie_name,".gms",sep=""),c_timesteps=1)
   manipulateConfig(paste(calib_magpie_name,".gms",sep=""),s_use_gdx=0)
   file.copy(paste(calib_magpie_name,".gms",sep=""),putfolder)
-
+  
   # execute calibration run
   system(paste("gams ",calib_magpie_name,".gms"," -errmsg=1 -PUTDIR ./",putfolder," -LOGOPTION=",logoption,sep=""),wait=TRUE)
   file.copy("fulldata.gdx",putfolder)
@@ -45,14 +45,16 @@ get_areacalib <- function(gdx_file) {
   if(nregions(magpie)!=nregions(data) | !all(getRegions(magpie) %in% getRegions(data))) {
     stop("Regions in MAgPIE do not agree with regions in reference calibration area data set!")
   }
-  return(magpie/data)
+  out <- magpie/data
+  out[out==0] <- 1
+  return(out)
 }
 
 get_yieldcalib <- function(gdx_file) {
   require(magclass)
   require(gdx)
   require(luscale)
-
+  
   prep <- function(x) {
     # use maiz as surrogate for all crops
     elem <- c("maiz","pasture")
@@ -60,48 +62,63 @@ get_yieldcalib <- function(gdx_file) {
     getNames(y) <- c("crop","past")
     return(superAggregate(y,level="reg",aggr_type="mean", na.rm=TRUE))
   }
-
+  
   y_ini <- prep(readGDX(gdx_file,"i14_yields"))
   y     <- prep(readGDX(gdx_file,"vm_yld")[,,"l"])
-  return(y/y_ini)
+  
+  out <- y/y_ini
+  out[out==0] <- 1
+  return(out)
 }
 
 # Calculate the correction factor and save it
-update_calib<-function(gdx_file,calibrate_pasture=TRUE,calibrate_cropland=TRUE,damping_factor=0.6, calib_file, crop_max=1, calibration_step=""){
+update_calib<-function(gdx_file,calibrate_pasture=TRUE,calibrate_cropland=TRUE,damping_factor=0.8, calib_file, crop_max=1, calibration_step=""){
   require(magclass)
   require(magpie4)
   if(!(modelstat(gdx_file)[1,1,1]%in%c(1,2,7))) stop("Calibration run infeasible")
-
+  
   area_factor  <- get_areacalib(gdx_file)
   tc_factor    <- get_yieldcalib(gdx_file)
   calib_correction <- area_factor * tc_factor
-
+  calib_divergence <- abs(calib_correction-1)
+  
   old_calib        <- read.magpie(calib_file)
-  calib_factor     <- old_calib * damping_factor*(calib_correction-1) + 1
-
-  if(!is.null(crop_max)) calib_factor[,,"crop"][calib_factor[,,"crop"] > crop_max]  <- crop_max
-  if(calibrate_pasture==FALSE)  calib_factor[,,"past"] <- 1
-  if(calibrate_cropland==FALSE) calib_factor[,,"crop"] <- 1
-
+  calib_factor     <- old_calib * (damping_factor*(calib_correction-1) + 1)
+  
+  if(!is.null(crop_max)) {
+    above_limit <- (calib_factor[,,"crop"] > crop_max)
+    calib_factor[,,"crop"][above_limit]  <- crop_max
+    calib_divergence[,,"crop"][above_limit] <- 0
+  } 
+  if(!calibrate_pasture)  {
+    calib_factor[,,"past"] <- 1
+    calib_divergence[,,"past"] <- 0
+  }
+  if(!calibrate_cropland) {
+    calib_factor[,,"crop"] <- 1
+    calib_divergence[,,"crop"] <- 0
+  }
+  
   comment <- c(" description: Regional yield calibration file",
                " unit: -",
                " note: All values in the file are set to 1 if a new regional setup is used.",
                " origin: scripts/calibration/calc_calib.R (path relative to model main directory)",
                paste0(" creation date: ",date()))
   write.magpie(round(setYears(calib_factor,NULL),2), calib_file, comment = comment)
-
+  
   ### write down current calib factors (and area_factors) for tracking
   write_log <- function(x,file,calibration_step) {
-      x <- add_dimension(x, dim=3.1, add="iteration", nm=calibration_step)
-      write.magpie(round(setYears(calib_factor,NULL),2), file, append = (calibration_step!=1))
+    x <- add_dimension(x, dim=3.1, add="iteration", nm=calibration_step)
+    write.magpie(round(setYears(x,NULL),2), file, append = (calibration_step!=1))
   }
-
+  
   write_log(calib_factor,     "calib_factor.cs3"     , calibration_step)
   write_log(calib_correction, "calib_correction.cs3" , calibration_step)
+  write_log(calib_divergence, "calib_divergence.cs3" , calibration_step)
   write_log(area_factor,      "calib_area_factor.cs3", calibration_step)
   write_log(tc_factor,        "calib_tc_factor.cs3"  , calibration_step)
-
-  return(calib_correction)
+  
+  return(calib_divergence)
 }
 
 
@@ -116,24 +133,22 @@ calibrate_magpie <- function(n_maxcalib = 1,
                              data_workspace = NULL,
                              logoption = 3,
                              debug = FALSE) {
-
+  
   require(magclass)
-
+  
   for(i in 1:n_maxcalib){
     cat(paste("\nStarting calibration iteration",i,"\n"))
     calibration_run(putfolder=putfolder, calib_magpie_name=calib_magpie_name, logoption=logoption)
     if(debug) file.copy(paste0(putfolder,"/fulldata.gdx"),paste0("fulldata_calib",i,".gdx"))
-    new_calib <- update_calib(gdx_file=paste0(putfolder,"/fulldata.gdx"),calibrate_pasture=calibrate_pasture,calibrate_cropland=calibrate_cropland,damping_factor=damping_factor, calib_file=calib_file, calibration_step=i)
-    if(all(abs(1-new_calib) < calib_accuracy)){
-      cat("\n\nCalibration accuracy reached after ",i," iterations\n\n")
-      swlatex(swout,paste("Calibration accuracy reached after ",i," of ",n_maxcalib,"possible iterations\n\n"))
+    calib_divergence <- update_calib(gdx_file=paste0(putfolder,"/fulldata.gdx"),calibrate_pasture=calibrate_pasture,calibrate_cropland=calibrate_cropland,damping_factor=damping_factor, calib_file=calib_file, calibration_step=i)
+    if(all(calib_divergence < calib_accuracy)){
       break
     }
   }
-
+  
   # delete calib_magpie_gms in the main folder
   unlink(paste0(calib_magpie_name,".*"))
   unlink("fulldata.gdx")
-
+  
   cat("\ncalibration finished\n")
 }
