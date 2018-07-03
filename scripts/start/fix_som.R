03ec113abfa5d998bf7f6a542b39beb5f0e463d7
Kristine Karstens <karstens@pik-potsdam.de>
2018-07-03 12:35:48 +0200

    update start script for som tests

--- scripts/start/fix_som.R
+++ scripts/start/fix_som.R
@@ -13,13 +13,43 @@ library(lucode)
 source("scripts/start_functions.R")
 source("config/default.cfg")
 
-#general settings
-cfg$gms$c_timesteps <- 7
 
-cfg$title <- "fix_som_test"
+buildInputVector <- function(regionmapping   = "h12",
+                             project_name    = "isimip_rcp",
+                             climatescen_name= "rcp2p6",
+                             co2             = "noco2",
+                             climate_model   = "IPSL_CM5A_LR",
+                             resolution      = "h200",
+                             archive_rev     = "33",
+                             madrat_rev      = "3.33",
+                             validation_rev  = "3.33",
+                             calibration     = NULL,
+                             additional_data = "additional_data_rev3.37.tgz",
+                             npi_base        = "calibration_H12_29Jun18.tgz") {
+  mappings <- c(h11="8a828c6ed5004e77d1ba2025e8ea2261",
+                h12="690d3718e151be1b450b394c1064b1c5",
+                mag="c30c1c580039c2b300d86cc46ff4036a")
+  archive_name=paste(project_name,climate_model,climatescen_name,co2,sep="-")
+  archive <- paste0(archive_name, "_rev", archive_rev, "_", resolution, "_", mappings[regionmapping], ".tgz")
+  madrat  <- paste0("rev", madrat_rev, "_", mappings[regionmapping], "_magpie.tgz")
+  validation  <- paste0("rev", validation_rev, "_", mappings[regionmapping], "_validation.tgz")
+  return(c(archive,madrat,validation,calibration,additional_data,npi_base))
+}
+
+
+cfg$title <- "new_carbon_som_on"
+cfg$input <- buildInputVector()
 cfg$gms$som <- "cellpool_aug16"
-cfg$gms$c59_som_scenario  <- "cc"   # def = "nocc"
+cfg$gms$c59_som_scenario  <- "nocc"   # def = "nocc"
+start_run(cfg=cfg,codeCheck=FALSE)
 
-start_run(cfg=cfg,codeCheck=TRUE)
+cfg$title <- "new_carbon_som_off"
+cfg$input <- buildInputVector()
+cfg$gms$som <- "off"
+start_run(cfg=cfg,codeCheck=FALSE)
 
 
+cfg$title <- "old_carbon_som_off"
+cfg$input <- buildInputVector(archive_rev="32")
+cfg$gms$som <- "off"
+start_run(cfg=cfg,codeCheck=FALSE)
