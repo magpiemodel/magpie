59f3fc96aa44e34bc42a2fbdaaaaf1f46535154c
Abhijeet Mishra <mishra@pik-potsdam.de>
2018-07-19 17:22:41 +0200

    change to start script

--- scripts/start/ssp_deforestation.R
+++ scripts/start/ssp_deforestation.R
@@ -37,38 +37,32 @@ for(reg in c("BRA","H12")) {
     stop("Unknown region setting!")
   }
   cfg$input <- c(paste0("isimip_rcp-IPSL_CM5A_LR-rcp2p6-noco2_rev33_",cellcode,"_",regionscode,".tgz"),
-                 paste0("rev3.34_",regionscode,"_magpie.tgz"),
-                 paste0("rev3.34_",regionscode,"_validation.tgz"),
+                 paste0("rev3.35_",regionscode,"_magpie.tgz"),
+                 paste0("rev3.35_",regionscode,"_validation.tgz"),
                  "additional_data_rev3.43.tgz")
   
-  for (ssp in c("SSP2","SSP1","SSP3","SSP4","SSP5")) {
+  for (ssp in c("SSP2")) {
     for (rcp in c("ref","26")){
      #if(rcp=="26" && ssp %in% c("SSP3","SSP4")) next
-      for (lec in c("magpie3","pure_estab","high")) {
-        for (lcc in c("corrected","off")) {
-          for(tc in c("ptc15","lg")) {
-            cfg$title <- paste(reg,lec,lcc,tc,ssp,rcp,sep="_")
-            
-            cfg <- setScenario(cfg,c(ssp,if(rcp=="Ref") "NPI" else "NDC"))
-            cfg$gms$c56_pollutant_prices <- paste(if(ssp %in% c("SSP3","SSP4")) "SSP2" else ssp,rcp,"SPA0",sep="-")
-            cfg$gms$c60_2ndgen_biodem <- paste(if(ssp %in% c("SSP3","SSP4")) "SSP2" else ssp,rcp,"SPA0",sep="-")
-            
-            cfg$gms$c39_cost_scenario_establish <- lec
-            cfg$gms$c39_cost_scenario_clearing <- lcc
-            
-            if(tc=="ptc15") {
-              cfg$gms$s40_pasture_transport_costs <- 0.15 #ptc15
-              cfg$gms$disagg_lvst <- "off" 
-            } else if(tc=="lg") {
-              cfg$gms$s40_pasture_transport_costs <- 0
-              cfg$gms$disagg_lvst <- "simple_oct17" 
-            } else {
-              stop("Unknown transport cost setting!")
-            }
-            
-            start_run(cfg,codeCheck=FALSE)
-          }
-        }
+      for(tc in c("ptc15","lg","no")) {
+        cfg$title <- paste(reg,tc,ssp,rcp,sep="_")
+        
+        cfg <- setScenario(cfg,c(ssp,if(rcp=="Ref") "NPI" else "NDC"))
+        cfg$gms$c56_pollutant_prices <- paste(if(ssp %in% c("SSP3","SSP4")) "SSP2" else ssp,rcp,"SPA0",sep="-")
+        cfg$gms$c60_2ndgen_biodem <- paste(if(ssp %in% c("SSP3","SSP4")) "SSP2" else ssp,rcp,"SPA0",sep="-")
+
+        if(tc=="ptc15") {
+          cfg$gms$s40_pasture_transport_costs <- 0.15 #ptc15
+          cfg$gms$disagg_lvst <- "off" 
+        } else if(tc=="lg") {
+          cfg$gms$s40_pasture_transport_costs <- 0
+          cfg$gms$disagg_lvst <- "simple_oct17" 
+        } else if(tc=="no"){
+		 cfg$gms$s40_pasture_transport_costs <- 0
+          cfg$gms$disagg_lvst <- "off" 
+		} else stop("Unknown transport cost setting!")
+        
+		start_run(cfg,codeCheck=FALSE)
       }
     }
   }
