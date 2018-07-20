3fb5a561c702cb6371ca823fe735802c7385ff4e
Kristine Karstens <karstens@pik-potsdam.de>
2018-07-20 16:17:37 +0200

    update strat script

--- scripts/start/ssp_deforestation.R
+++ scripts/start/ssp_deforestation.R
@@ -36,33 +36,50 @@ for(reg in c("BRA","H12")) {
   } else {
     stop("Unknown region setting!")
   }
-  cfg$input <- c(paste0("isimip_rcp-IPSL_CM5A_LR-rcp2p6-noco2_rev33_",cellcode,"_",regionscode,".tgz"),
-                 paste0("rev3.35_",regionscode,"_magpie.tgz"),
-                 paste0("rev3.35_",regionscode,"_validation.tgz"),
-                 "additional_data_rev3.44.tgz")
   
-  for (ssp in c("SSP2")) {
+  forest_pro <- c(paste0("isimip_rcp-IPSL_CM5A_LR-rcp2p6-noco2_rev33_",cellcode,"_",regionscode,".tgz"),
+                         paste0("rev3.35_",regionscode,"_magpie.tgz"),
+                         paste0("rev3.35_",regionscode,"_validation.tgz"),
+                         "additional_data_rev3.44.tgz")
+  forest_nopro   <- c(paste0("isimip_rcp-IPSL_CM5A_LR-rcp2p6-noco2_rev33_",cellcode,"_",regionscode,".tgz"),
+                         paste0("rev3.35_",regionscode,"_magpie.tgz"),
+                         paste0("rev3.35_",regionscode,"_validation.tgz"),
+                         "additional_data_rev3.43.tgz")
+  
+  for (ssp in c("SSP2","SSP1","SSP5")) {
     for (rcp in c("ref","26")){
      #if(rcp=="26" && ssp %in% c("SSP3","SSP4")) next
-      for(tc in c("ptc15","lg","no")) {
-        cfg$title <- paste(reg,tc,ssp,rcp,sep="_")
-        
-        cfg <- setScenario(cfg,c(ssp,if(rcp=="Ref") "NPI" else "NDC"))
-        cfg$gms$c56_pollutant_prices <- paste(if(ssp %in% c("SSP3","SSP4")) "SSP2" else ssp,rcp,"SPA0",sep="-")
-        cfg$gms$c60_2ndgen_biodem <- paste(if(ssp %in% c("SSP3","SSP4")) "SSP2" else ssp,rcp,"SPA0",sep="-")
-
-        if(tc=="ptc15") {
-          cfg$gms$s40_pasture_transport_costs <- 0.15 #ptc15
-          cfg$gms$disagg_lvst <- "off" 
-        } else if(tc=="lg") {
-          cfg$gms$s40_pasture_transport_costs <- 0
-          cfg$gms$disagg_lvst <- "simple_oct17" 
-        } else if(tc=="no"){
-		 cfg$gms$s40_pasture_transport_costs <- 0
-          cfg$gms$disagg_lvst <- "off" 
-		} else stop("Unknown transport cost setting!")
-        
-		start_run(cfg,codeCheck=FALSE)
+      for(tc in c("lg","ptc")) {
+        for(jpn in c("JPNfp","JPNdf")){
+          for(tau in c("endo_JUN16","endo_jun18")){
+            
+            cfg$title <- paste(reg,tc,jpn,tau,ssp,rcp,sep="_")
+            
+            cfg <- setScenario(cfg,c(ssp,if(rcp=="Ref") "NPI" else "NDC"))
+            cfg$gms$c56_pollutant_prices <- paste(if(ssp %in% c("SSP3","SSP4")) "SSP2" else ssp,rcp,"SPA0",sep="-")
+            cfg$gms$c60_2ndgen_biodem <- paste(if(ssp %in% c("SSP3","SSP4")) "SSP2" else ssp,rcp,"SPA0",sep="-")
+            
+            cfg$gms$tc <- tau
+            
+            if(jpn=="JPNdf"){
+              cfg$input <- forest_nopro
+            } else if(jpn=="JPNfp"){
+              cfg$input <- forest_pro
+            } else stop("Unknown transport cost setting!")
+            
+            if(tc=="ptc15") {
+              cfg$gms$s40_pasture_transport_costs <- 0.15 #ptc15
+              cfg$damping_factor <- 0.7
+              cfg$gms$disagg_lvst <- "off" 
+            } else if(tc=="lg") {
+              cfg$gms$s40_pasture_transport_costs <- 0
+              cfg$damping_factor <- 0.98
+              cfg$gms$disagg_lvst <- "simple_oct17" 
+            } else stop("Unknown transport cost setting!")
+            
+            start_run(cfg,codeCheck=FALSE)
+          }  
+        }
       }
     }
   }
