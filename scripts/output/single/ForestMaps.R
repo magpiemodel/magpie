e58cc41ec39b796967cefc4c33b45c6035f65288
florianh <humpenoeder@pik-potsdam.de>
2018-08-10 12:52:00 +0200

    update output script

--- scripts/output/single/ForestMaps.R
+++ scripts/output/single/ForestMaps.R
@@ -26,3 +26,5 @@ plotmap2(defor_primforest,paste0(outputdir,"/",title,"_defor_primforest.pdf"),le
 defor_secdforest <- land_hr[,c(2020,2050,2100),"secdforest"] - setYears(land_hr[,1995,"secdforest"],NULL)
 plotmap2(defor_secdforest,paste0(outputdir,"/",title,"_defor_secdforest.pdf"),legend_range = c(-1,1),title = paste(title,"diff maps"),midpoint = 0,lowcol = "darkred",midcol = "grey95",highcol = "darkgreen",plot_height=15,plot_width=10)
 
+defor <- dimSums(land_hr[,c(2020,2050,2100),c("primforest","secdforest")],dim=3) - dimSums(setYears(land_hr[,1995,c("primforest","secdforest")],NULL),dim=3)
+plotmap2(defor,paste0(outputdir,"/",title,"_defor.pdf"),legend_range = c(-1,1),title = paste(title,"diff maps"),midpoint = 0,lowcol = "darkred",midcol = "grey95",highcol = "darkgreen",plot_height=15,plot_width=10)
