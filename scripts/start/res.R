578423216a03f198105c362fc98d1ea22855251a
florianh <humpenoeder@pik-potsdam.de>
2018-08-20 14:46:42 +0200

    update start script

--- scripts/start/res.R
+++ scripts/start/res.R
@@ -19,22 +19,15 @@ source("scripts/start_functions.R")
 source("config/default.cfg")
 cfg$results_folder <- "output/:title:"
 cfg$recalibrate <- TRUE
+cfg$calib_maxiter <- 3
 
-cfg$title <- "h200_def"
-start_run(cfg,codeCheck=FALSE)
-
-cfg$title <- "h200_weight2"
-cfg$input <- c("isimip_rcp-IPSL_CM5A_LR-rcp2p6-co2_rev33_h200_LAM2_SSA2_OAS2_690d3718e151be1b450b394c1064b1c5.tgz",
-               "rev4.2_690d3718e151be1b450b394c1064b1c5_magpie.tgz",
-               "rev4.2_690d3718e151be1b450b394c1064b1c5_validation.tgz",
-               "additional_data_rev3.48.tgz",
-               "calibration_H12_06Aug18.tgz")
-start_run(cfg,codeCheck=FALSE)
-
-cfg$title <- "h200_weight15"
-cfg$input <- c("isimip_rcp-IPSL_CM5A_LR-rcp2p6-co2_rev33_h200_LAM15_SSA15_OAS15_690d3718e151be1b450b394c1064b1c5.tgz",
-               "rev4.2_690d3718e151be1b450b394c1064b1c5_magpie.tgz",
-               "rev4.2_690d3718e151be1b450b394c1064b1c5_validation.tgz",
-               "additional_data_rev3.48.tgz",
-               "calibration_H12_06Aug18.tgz")
-start_run(cfg,codeCheck=FALSE)
+low_res <- "n200"
+for(res in c(low_res,paste0(low_res,"_LAM2"),paste0(low_res,"_LAM3"),paste0(low_res,"_LAM4"),paste0(low_res,"_LAM5"),paste0(low_res,"_LAM6"))) {
+  cfg$title <- res
+  cfg$input <- c(paste0("isimip_rcp-IPSL_CM5A_LR-rcp2p6-noco2_rev33_",res,"_690d3718e151be1b450b394c1064b1c5.tgz"),
+                 "rev4.61_690d3718e151be1b450b394c1064b1c5_magpie.tgz",
+                 "rev4.61_690d3718e151be1b450b394c1064b1c5_validation.tgz",
+                 "additional_data_rev3.49.tgz",
+                 "calibration_H12_06Aug18.tgz")
+  start_run(cfg,codeCheck=FALSE)
+}
