443892b84dffba5dd4933295ffa84bc53c50ca74
Abhijeet <mishra@pik-potsdam.de>
2019-02-13 22:56:02 +0100

    Bugfix in start script

--- scripts/start/ForestryTest.R
+++ scripts/start/ForestryTest.R
@@ -59,7 +59,7 @@ for(biodem in co2_price_scenarios){
   		t <- gsub(".*_", "", rl)
   		t <- gsub("rl","",t)
   		cfg$gms$c32_rot_length <- rl
-  		for(rl_estb in rl_all[!grepl(rl, rl_all)]){
+  		for(rl_estb in c("rlFAO_min","rlGTM")){
   		cfg$gms$c32_rot_length_estb <- rl_estb
   		t_estb <- gsub(".*_", "", rl_estb)
       	t_estb <- gsub("rl","",t_estb)
@@ -91,7 +91,7 @@ for(biodem in co2_price_scenarios){
   		t <- gsub(".*_", "", rl)
   		t <- gsub("rl","",t)
   		cfg$gms$c32_rot_length <- rl
-  		for(rl_estb in rl_all[!grepl(rl, rl_all)]){
+  		for(rl_estb in c("rlGTM","rlFAO_max"))){
   		cfg$gms$c32_rot_length_estb <- rl_estb
   		t_estb <- gsub(".*_", "", rl_estb)
       	t_estb <- gsub("rl","",t_estb)
