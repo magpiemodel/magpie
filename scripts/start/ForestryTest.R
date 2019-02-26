d23b70490bd2d0a3f0f896b9beb3a7eace5bc85f
Abhijeet <mishra@pik-potsdam.de>
2019-02-26 20:11:58 +0100

    Starting script change

--- scripts/start/ForestryTest.R
+++ scripts/start/ForestryTest.R
@@ -40,8 +40,8 @@ cfg$gms$optimization <- "nlp_apr17"
 #ALERT:  At the moment this script cannot download new data in case the input files are changed. Has to be set to true.
 cfg$force_download <- FALSE
 #rl_all<-c("rlGTM")
-#rl_all<-c("rlFAO_min","rlGTM","rlFAO_max")
-rl_all<-c("rlFAO_max")
+rl_all<-c("rlFAO_min","rlGTM","rlFAO_max")
+#rl_all<-c("rlFAO_max")
 
 co2_price_scenarios <- c("SSP2-Ref-SPA0")
 #co2_price_scenarios <- c("SSP2-Ref-SPA0","SSP2-26-SPA2")
