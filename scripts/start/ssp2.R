1b506853cf64d7cd1a7d814051bee803d3aa3133
florianh <humpenoeder@pik-potsdam.de>
2019-04-30 11:19:41 +0200

    update start script

--- scripts/start/ssp2.R
+++ scripts/start/ssp2.R
@@ -18,27 +18,29 @@ source("scripts/start_functions.R")
 #start MAgPIE run
 source("config/default.cfg")
 
+cfg$force_download <- TRUE
+
 cfg$results_folder <- "output/:title:"
 
-cfg$title <- "R2M41_SSP2_NPi_v4"
+cfg$title <- "R2M41_SSP2_NPi_v5"
 cfg <- setScenario(cfg,c("SSP2","NPI"))
 cfg$gms$c56_pollutant_prices <- "R2M41-SSP2-NPi"
 cfg$gms$c60_2ndgen_biodem <- "R2M41-SSP2-NPi"
 start_run(cfg,codeCheck=FALSE)
 
-cfg$title <- "R2M41_SSP2_NDC_v4"
+cfg$title <- "R2M41_SSP2_NDC_v5"
 cfg <- setScenario(cfg,c("SSP2","NDC"))
 cfg$gms$c56_pollutant_prices <- "R2M41-SSP2-NDC"
 cfg$gms$c60_2ndgen_biodem <- "R2M41-SSP2-NDC"
 start_run(cfg,codeCheck=FALSE)
 
-cfg$title <- "R2M41_SSP2_Budg600_v4"
+cfg$title <- "R2M41_SSP2_Budg600_v5"
 cfg <- setScenario(cfg,c("SSP2","NDC"))
 cfg$gms$c56_pollutant_prices <- "R2M41-SSP2-Budg600"
 cfg$gms$c60_2ndgen_biodem <- "R2M41-SSP2-Budg600"
 start_run(cfg,codeCheck=FALSE)
 
-cfg$title <- "SSPDB_SSP2_26_v4"
+cfg$title <- "SSPDB_SSP2_26_v5"
 cfg <- setScenario(cfg,c("SSP2","NDC"))
 cfg$gms$c56_pollutant_prices <- "SSPDB-SSP2-26-REMIND-MAGPIE"
 cfg$gms$c60_2ndgen_biodem <- "SSPDB-SSP2-26-REMIND-MAGPIE"
