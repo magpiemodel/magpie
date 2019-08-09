2c24ad60071b0005a3f1919fa0485d97fbaf5400
Abhijeet <mishra@pik-potsdam.de>
2019-08-09 12:59:30 +0200

    Region specific production ratio for timber using plantations (greater)

--- scripts/start/ForestryTest.R
+++ scripts/start/ForestryTest.R
@@ -9,7 +9,7 @@
 #### Script to MAgPIE test runs ####
 ##########################################################
 
-flag_run <- "WFselfsuff_WOOD_FORESTRY_PRODRATIO_EQUALITY" 
+flag_run <- "WFselfsuff_WOOD_FORESTRY_PRODRATIO_GREATER" 
 
 library(lucode)
 source("scripts/start_functions.R")
