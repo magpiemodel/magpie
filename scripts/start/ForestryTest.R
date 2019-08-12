84ba8b3c5a5bc5c6830eed3f7667b3aec0dde6f0
Abhijeet <mishra@pik-potsdam.de>
2019-08-12 12:24:28 +0200

    Establishment is strictly equal to future demand proportion

--- scripts/start/ForestryTest.R
+++ scripts/start/ForestryTest.R
@@ -9,7 +9,7 @@
 #### Script to MAgPIE test runs ####
 ##########################################################
 
-flag_run <- "SECDFtoOTHER_ESTB=l=RATIOFUTURE"
+flag_run <- "SECDFtoOTHER_ESTB=e=RATIOFUTURE"
 
 library(lucode)
 source("scripts/start_functions.R")
