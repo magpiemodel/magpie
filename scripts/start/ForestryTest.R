3422209c574b27af7e1b69e4ceb9eb514f8f956c
Abhijeet <mishra@pik-potsdam.de>
2019-08-13 16:48:02 +0200

    Updated title in start script

--- scripts/start/ForestryTest.R
+++ scripts/start/ForestryTest.R
@@ -9,7 +9,7 @@
 #### Script to MAgPIE test runs ####
 ##########################################################
 
-flag_run <- "NO_TIMBERPROD_INTERFACE-ownrotlength-RegProd"
+flag_run <- "Florian_fix"
 
 library(lucode)
 source("scripts/start_functions.R")
