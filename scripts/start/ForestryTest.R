fe91da4ea5e5cf80ee2e7c3ebc9a88988a974ff2
Abhijeet <mishra@pik-potsdam.de>
2019-09-13 12:40:25 +0200

    Start script change

--- scripts/start/ForestryTest.R
+++ scripts/start/ForestryTest.R
@@ -9,7 +9,7 @@
 #### Script to MAgPIE test runs ####
 ##########################################################
 
-flag_run <- "nonNPI"
+flag_run <- "NPI"
 
 library(lucode)
 source("scripts/start_functions.R")
