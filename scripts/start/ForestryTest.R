3c7e000a8ba0e2037bef809248afc1c9d4a98d44
Abhijeet <mishra@pik-potsdam.de>
2019-09-15 20:46:18 +0200

    Updated run flag

--- scripts/start/ForestryTest.R
+++ scripts/start/ForestryTest.R
@@ -9,7 +9,7 @@
 #### Script to MAgPIE test runs ####
 ##########################################################
 
-flag_run <- "Dem1995"
+flag_run <- "regDemand"
 
 library(lucode)
 source("scripts/start_functions.R")
