e76ac05866281babca7fb17e4ad68b6227551847
Abhijeet <mishra@pik-potsdam.de>
2019-09-27 13:24:45 +0200

    start script change

--- scripts/start/ForestryTest.R
+++ scripts/start/ForestryTest.R
@@ -9,7 +9,7 @@
 #### Script to MAgPIE test runs ####
 ##########################################################
 
-flag_run <- "noHeaven-OLDland"
+flag_run <- "noHeaven-OLDland-LOWwf"
 
 library(lucode)
 source("scripts/start_functions.R")
