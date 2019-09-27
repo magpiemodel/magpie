4373724563e9638d7d1f41b960488f37fb33e8ce
Abhijeet <mishra@pik-potsdam.de>
2019-09-27 11:42:44 +0200

    Default trade and no NPI

--- scripts/start/ForestryTest.R
+++ scripts/start/ForestryTest.R
@@ -9,7 +9,7 @@
 #### Script to MAgPIE test runs ####
 ##########################################################
 
-flag_run <- "noHeaven-NoHigh"
+flag_run <- "noHeaven-NoHigh-OLDland"
 
 library(lucode)
 source("scripts/start_functions.R")
@@ -28,18 +28,17 @@ codeCheck <- FALSE
 
 #general settings
 cfg$results_folder <- "output/:title:"
-cfg <- setScenario(cfg,c("SSP2","NPI"))
-#cfg <- setScenario(cfg,c("SSP2"))
+#cfg <- setScenario(cfg,c("SSP2","NPI"))
+cfg <- setScenario(cfg,c("SSP2","BASE"))
 
 ## Module settings
 cfg$gms$demand <- "sector_dec18"
 cfg$gms$trade <- "selfsuff_reduced"
-cfg$gms$forestry  <- "dynamic_oct19"
 cfg$gms$natveg  <- "dynamic_dec18"
 cfg$gms$optimization <- "nlp_apr17"
-cfg$gms$land <- "landmatrix_dec18"
+cfg$gms$land <- "feb15"
 cfg$gms$c80_nlp_solver <- "conopt4"
-cfg$gms$c21_trade_liberalization  <- "free2000"
+#cfg$gms$c21_trade_liberalization  <- "free2000"
 
 #ALERT:  At the moment this script cannot download new data in case the input files are changed. Has to be set to true.
 cfg$force_download <- FALSE
@@ -61,38 +60,42 @@ cfg$recalibrate <- "ifneeded"
 
 ## Setting up runs
 
-## Loop over mitigation-co2 prices
-#for(co2_price_scenarios in c("R2M41-SSP2-NPi","R2M41-SSP2-Budg1300" )){
-for(co2_price_scenarios in c("R2M41-SSP2-NPi")){
 
-	## Loop over climate impacts
-	for(climate_impacts in c(FALSE)){
-		if(climate_impacts){
-			cfg <- setScenario(cfg, "cc")
-			cc_flag = "CC"
-		} else {cc_flag = "noCC"}
+for(sector_test in c("dynamic_oct19","dynamic_dec18")){
+	cfg$gms$forestry  <- sector_test
+	## Loop over mitigation-co2 prices
+	#for(co2_price_scenarios in c("R2M41-SSP2-NPi","R2M41-SSP2-Budg1300" )){
+	for(co2_price_scenarios in c("R2M41-SSP2-NPi")){
 
-		## Set 2nd gen bioenergy demand and pollutant prices
-		cfg$gms$c56_pollutant_prices <- co2_price_scenarios
-		cfg$gms$c60_2ndgen_biodem <- co2_price_scenarios		# same as co2 price scenario from mag4.1
+		## Loop over climate impacts
+		for(climate_impacts in c(FALSE)){
+			if(climate_impacts){
+				cfg <- setScenario(cfg, "cc")
+				cc_flag = "CC"
+			} else {cc_flag = "noCC"}
 
-		## Set time step length
-		cfg$gms$c_timesteps = "5year"
+			## Set 2nd gen bioenergy demand and pollutant prices
+			cfg$gms$c56_pollutant_prices <- co2_price_scenarios
+			cfg$gms$c60_2ndgen_biodem <- co2_price_scenarios		# same as co2 price scenario from mag4.1
 
-		## Set clear cutting or selective logging flag
-		#for (sl_set in c(0.05,1.00)) {
-		#	if(sl_set == 0.05) logging = "SelectiveLog"
-		#	if(sl_set == 1.00) logging = "ClearCut"
-		#	cfg$gms$s35_selective_logging_flag = sl_set
+			## Set time step length
+			cfg$gms$c_timesteps = "5year"
 
-			if(cfg$gms$c56_pollutant_prices == "R2M41-SSP2-Budg1300" ) {
-#				cfg$title<- paste0(cfg$gms$c_timesteps,"_",logging,"_","_CO2prices","_",cc_flag,"_",flag_run)
-				cfg$title<- paste0("Mitig-CO2prices","_",cc_flag,"-",flag_run)
-			} else {
-#				cfg$title<- paste0(cfg$gms$c_timesteps,"_",logging,"_",cc_flag,"_",flag_run)
-				cfg$title<- paste0(flag_run,"-",cfg$gms$c80_nlp_solver,"-",cfg$gms$c21_trade_liberalization)
-			}
-			start_run(cfg=cfg,codeCheck=codeCheck)
-	 #}
+			## Set clear cutting or selective logging flag
+			#for (sl_set in c(0.05,1.00)) {
+			#	if(sl_set == 0.05) logging = "SelectiveLog"
+			#	if(sl_set == 1.00) logging = "ClearCut"
+			#	cfg$gms$s35_selective_logging_flag = sl_set
+
+				if(cfg$gms$c56_pollutant_prices == "R2M41-SSP2-Budg1300" ) {
+	#				cfg$title<- paste0(cfg$gms$c_timesteps,"_",logging,"_","_CO2prices","_",cc_flag,"_",flag_run)
+					cfg$title<- paste0("Mitig-CO2prices","_",cc_flag,"-",flag_run)
+				} else {
+	#				cfg$title<- paste0(cfg$gms$c_timesteps,"_",logging,"_",cc_flag,"_",flag_run)
+					cfg$title<- paste0(flag_run,"-",cfg$gms$c80_nlp_solver,"-",cfg$gms$c21_trade_liberalization)
+				}
+				start_run(cfg=cfg,codeCheck=codeCheck)
+		 #}
+		}
 	}
 }
