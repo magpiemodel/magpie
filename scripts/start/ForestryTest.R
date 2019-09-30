a5ae0ce2e54e513c84ee9ca25f3a7914c0eb5d65
Abhijeet <mishra@pik-potsdam.de>
2019-09-30 12:05:09 +0200

    Start flag changed

--- scripts/start/ForestryTest.R
+++ scripts/start/ForestryTest.R
@@ -9,7 +9,7 @@
 #### Script to MAgPIE test runs ####
 ##########################################################
 
-flag_run <- "RED-WF-IND"
+flag_run <- "BugFix-"
 
 library(lucode)
 source("scripts/start_functions.R")
