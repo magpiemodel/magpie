05a6c5880b1635c1160f6ed603b84c0ec4b9960c
Abhijeet <mishra@pik-potsdam.de>
2019-10-01 16:54:06 +0200

    removed additional log

--- scripts/start/ForestryTest.R
+++ scripts/start/ForestryTest.R
@@ -10,50 +10,50 @@
 ##########################################################
 
 flag_run <- "R001-"
-	library(lucode)
-	source("scripts/start_functions.R")
-	source("config/default.cfg")
+library(lucode)
+source("scripts/start_functions.R")
+source("config/default.cfg")
 
-	cfg$developer_mode <- TRUE
+cfg$developer_mode <- TRUE
 
-	## Location of input data
-	cfg$repositories <- append(list("https://rse.pik-potsdam.de/data/magpie/public"=NULL,"/p/projects/landuse/users/mishra/additional_data_private_forestry"=NULL), getOption("magpie_repos"))
+## Location of input data
+cfg$repositories <- append(list("https://rse.pik-potsdam.de/data/magpie/public"=NULL,"/p/projects/landuse/users/mishra/additional_data_private_forestry"=NULL), getOption("magpie_repos"))
 
-	#set defaults
-	codeCheck <- FALSE
+#set defaults
+codeCheck <- FALSE
 
-	#general settings
-	cfg$results_folder <- "output/:title:"
-	cfg <- setScenario(cfg,c("SSP2","NPI"))
-	#cfg <- setScenario(cfg,c("SSP2","BASE"))
+#general settings
+cfg$results_folder <- "output/:title:"
+cfg <- setScenario(cfg,c("SSP2","NPI"))
+#cfg <- setScenario(cfg,c("SSP2","BASE"))
 
-	## Module settings
-	cfg$gms$demand <- "sector_dec18"
-	cfg$gms$trade <- "selfsuff_reduced_ff"
-	cfg$gms$natveg  <- "dynamic_dec18"
-	cfg$gms$optimization <- "nlp_apr17"
-	cfg$gms$land <- "feb15"
-	cfg$gms$c80_nlp_solver <- "conopt4"
+## Module settings
+cfg$gms$demand <- "sector_dec18"
+cfg$gms$trade <- "selfsuff_reduced_ff"
+cfg$gms$natveg  <- "dynamic_dec18"
+cfg$gms$optimization <- "nlp_apr17"
+cfg$gms$land <- "feb15"
+cfg$gms$c80_nlp_solver <- "conopt4"
 
-	#ALERT:  At the moment this script cannot download new data in case the input files are changed. Has to be set to true.
-	cfg$force_download <- FALSE
+#ALERT:  At the moment this script cannot download new data in case the input files are changed. Has to be set to true.
+cfg$force_download <- FALSE
 
-	## No interpolation later
-	cfg$output <- c("rds_report")
+## No interpolation later
+cfg$output <- c("rds_report")
 
-	## Using optfile
-	cfg$gms$s80_optfile <- 1
+## Using optfile
+cfg$gms$s80_optfile <- 1
 
-	## What outputs to generate
-	cfg$output <- c("rds_report")
+## What outputs to generate
+cfg$output <- c("rds_report")
 
-	## Food model covnvergence
-	cfg$gms$s15_convergence <- 0.005
+## Food model covnvergence
+cfg$gms$s15_convergence <- 0.005
 
-	## Should recalibration be made
-	cfg$recalibrate <- "ifneeded"
+## Should recalibration be made
+cfg$recalibrate <- "ifneeded"
 
-	## Setting up runs
+## Setting up runs
 
 ## Loop over climate impacts
 for(climate_impacts in c("nocc","cc")){
@@ -92,15 +92,6 @@ for(climate_impacts in c("nocc","cc")){
 				} else {
 					cfg$title<- paste0(flag_run,"-",forestry_tc,"_",logging,"_",climate_impacts,"_",rcp_scen)
 				}
-
-				text1 <- paste("With this run, the input data from isimip was set to",rcp_scen)
-				text2 <- paste("This means that now co2 price scenario was", co2_price_scenarios,"with", rcp_scen)
-				text3 <- paste("The run is based on LPJmL4 input. Climate change impact has been set to", climate_impacts)
-				text4 <- paste("Forestry management factors have been hardcoded.")
-
-				log_con <- file("output/",cfg$title,"/run_info_forestry.txt", open="a")
-				cat(text1,text2,text3,text4, file = log_con, sep="\n")
-				close(log_con)
 				start_run(cfg=cfg,codeCheck=codeCheck)
 			}
 		}
