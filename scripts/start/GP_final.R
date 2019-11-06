f9dcadcd7b0b027f3a0a2183fcc36e9dc72fe8d9
emolinab <mbacca@pik-potsdam.de>
2019-11-06 13:55:52 +0100

    No recalibration change in Nitrogen Uptake

--- scripts/start/GP_final.R
+++ scripts/start/GP_final.R
@@ -8,6 +8,9 @@ source("scripts/start_functions.R")
 # Sources the default configuration file
 source("config/default.cfg")
 
+#Should we avoid calibration
+cfg$recalibrate <- FALSE     # def = "ifneeded"
+
 #Change the results folder name
 cfg$title<-"Good_Practice_Nitrogen"
 
@@ -19,8 +22,8 @@ cfg$output <- c("rds_report")
 cfg$input <- c("magpie4.1_default_apr19.tgz","additional_data_rev3.68.tgz","isimip_rcp-IPSL_CM5A_LR-rcp2p6-co2_rev38_c200_690d3718e151be1b450b394c1064b1c5.tgz","rev4.32_690d3718e151be1b450b394c1064b1c5_magpie.tgz","rev4.32_690d3718e151be1b450b394c1064b1c5_validation.tgz")
 
 #NITROGEN UPTAKE SCENARIO DEFAULT 60_60. GoodPractice 85_85
-#cfg$gms$c50_scen_neff <- "neff85_85_starty2010"
-cfg$gms$c50_scen_neff <- "neff60_60_starty1990"
+cfg$gms$c50_scen_neff <- "neff85_85_starty2010" 
+#cfg$gms$c50_scen_neff <- "neff60_60_starty1990"
 
 #Manure share used in an anaerobic digester. 0.15 in 2015, 0.30 in 2030, 0.5 in 2050 and 0.7 in 2100
 #cfg$gms$c55_scen_conf<-"GoodPractice"
