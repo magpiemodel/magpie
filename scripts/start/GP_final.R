81e2396c5a029bd794d3b395e7d0a441a29b14fb
emolinab <mbacca@pik-potsdam.de>
2019-11-06 10:46:17 +0100

    Default change check

--- scripts/start/GP_final.R
+++ scripts/start/GP_final.R
@@ -20,7 +20,7 @@ cfg$input <- c("magpie4.1_default_apr19.tgz","additional_data_rev3.68.tgz","isim
 
 #NITROGEN UPTAKE SCENARIO DEFAULT 60_60. GoodPractice 85_85
 #cfg$gms$c50_scen_neff <- "neff85_85_starty2010"
-cfg$gms$c50_scen_neff <- "neff85_85_starty2010"
+cfg$gms$c50_scen_neff <- "neff60_60_starty1990"
 
 #Manure share used in an anaerobic digester. 0.15 in 2015, 0.30 in 2030, 0.5 in 2050 and 0.7 in 2100
 #cfg$gms$c55_scen_conf<-"GoodPractice"
