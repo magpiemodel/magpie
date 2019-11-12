77ad9ad8d6351887f2d3e43d70e5497eff6afa69
Abhijeet <mishra@pik-potsdam.de>
2019-11-12 16:40:00 +0100

    Secdf stays secdf. No dampeingn factors

--- scripts/start/ForestryTest.R
+++ scripts/start/ForestryTest.R
@@ -59,7 +59,7 @@ cfg$recalibrate <- "ifneeded"
 #################################
 ####### CHANGING THE FLAG #######
 #################################
-flag_run <- "R026--SecdftoOther--Nodampening--"
+flag_run <- "R026--SecdftoSecdf--Nodampening--"
 #################################
 #for(rl_scen in c("low","medium","high")) {
 for(rl_scen in c("medium")) {
