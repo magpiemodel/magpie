386f93114ba1ad107076ea7d4caf468457e3c749
florianh <humpenoeder@pik-potsdam.de>
2020-01-09 15:36:46 +0100

    bugfix afforestation constraint regional/cellular

--- scripts/start/aff_test.R
+++ scripts/start/aff_test.R
@@ -35,6 +35,8 @@ source("config/default.cfg")
 
 cfg$results_folder <- "output/:title:"
 
+prefix <- "aff6_"
+
 for (ssp in c("SSP1","SSP2","SSP5")) {
   
   cfg <- setScenario(cfg,c(ssp,"NDC"))
@@ -50,25 +52,25 @@ for (ssp in c("SSP1","SSP2","SSP5")) {
   
   getInput(paste0("/p/projects/piam/runs/coupled-magpie/output/coupled-remind_",ssp,"-PkBudg900-mag-4/fulldata.gdx"))
   
-  cfg$title <- paste0("aff5_",ssp,"_PkBudg900_cpricefuture_50%")
+  cfg$title <- paste0(prefix,ssp,"_PkBudg900_cpricefuture_50%")
   cfg$gms$ghg_policy  <- "price_jan20"
   cfg$gms$s56_c_price_aff_future <- 1
   cfg$gms$s56_cprice_red_factor <- 0.5
   start_run(cfg,codeCheck=FALSE)
   
-  cfg$title <- paste0("aff5_",ssp,"_PkBudg900_cpricepresent_50%")
+  cfg$title <- paste0(prefix,ssp,"_PkBudg900_cpricepresent_50%")
   cfg$gms$ghg_policy  <- "price_jan20"
   cfg$gms$s56_c_price_aff_future <- 0
   cfg$gms$s56_cprice_red_factor <- 0.5
   start_run(cfg,codeCheck=FALSE)
 
-  cfg$title <- paste0("aff5_",ssp,"_PkBudg900_cpricepresent_100%")
+  cfg$title <- paste0(prefix,ssp,"_PkBudg900_cpricepresent_100%")
   cfg$gms$ghg_policy  <- "price_jan20"
   cfg$gms$s56_c_price_aff_future <- 0
   cfg$gms$s56_cprice_red_factor <- 1
   start_run(cfg,codeCheck=FALSE)
   
-  cfg$title <- paste0("aff5_",ssp,"_PkBudg900_cpricepresent_100%_interest_7%")
+  cfg$title <- paste0(prefix,ssp,"_PkBudg900_cpricepresent_100%_interest_7%")
   cfg$gms$ghg_policy  <- "price_jan20"
   cfg$gms$s56_c_price_aff_future <- 0
   cfg$gms$s56_cprice_red_factor <- 1
@@ -76,7 +78,7 @@ for (ssp in c("SSP1","SSP2","SSP5")) {
   cfg$gms$c12_interest_rate <- "medium"
   start_run(cfg,codeCheck=FALSE)
   
-  cfg$title <- paste0("aff5_",ssp,"_PkBudg900_cpricepresent_100%_interest_5%")
+  cfg$title <- paste0(prefix,ssp,"_PkBudg900_cpricepresent_100%_interest_5%")
   cfg$gms$ghg_policy  <- "price_jan20"
   cfg$gms$s56_c_price_aff_future <- 0
   cfg$gms$s56_cprice_red_factor <- 1
