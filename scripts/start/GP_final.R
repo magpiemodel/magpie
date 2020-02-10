d3676e0c74362bc5cc55f3afcdf03a56904d8ae6
emolinab <mbacca@pik-potsdam.de>
2020-02-10 15:15:36 +0100

    Missing input files fixed

--- scripts/start/GP_final.R
+++ scripts/start/GP_final.R
@@ -19,7 +19,11 @@ cfg$output <- c("rds_report","interpolation")
 
 
 # INPUT FILES
-cfg$input <- c("magpie4.1_default_apr19.tgz","additional_data_rev3.68.tgz","isimip_rcp-IPSL_CM5A_LR-rcp2p6-co2_rev38_c200_690d3718e151be1b450b394c1064b1c5.tgz","rev4.33_690d3718e151be1b450b394c1064b1c5_validation.tgz","rev4.33_690d3718e151be1b450b394c1064b1c5_magpie.tgz")
+cfg$input <- c("magpie4.1_default_apr19.tgz","rev4.26_690d3718e151be1b450b394c1064b1c5_magpie.tgz",
+"rev4.26_690d3718e151be1b450b394c1064b1c5_validation.tgz",
+"calibration_H12_c200_12Sep18.tgz",
+"additional_data_rev3.76.tgz",
+"additional_data_rev3.68.tgz","isimip_rcp-IPSL_CM5A_LR-rcp2p6-co2_rev38_c200_690d3718e151be1b450b394c1064b1c5.tgz","rev4.33_690d3718e151be1b450b394c1064b1c5_validation.tgz","rev4.33_690d3718e151be1b450b394c1064b1c5_magpie.tgz")
 
 #NITROGEN UPTAKE SCENARIO
 cfg$gms$c50_scen_neff <- "neff85_85_starty2010"
