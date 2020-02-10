937351e5dc4d483588134e8cfeea1f14ff115b86
emolinab <mbacca@pik-potsdam.de>
2020-02-10 14:30:12 +0100

    GoodPractice Scenarios (NUE and AWMS)

--- scripts/start/GP_final.R
+++ scripts/start/GP_final.R
@@ -9,25 +9,23 @@ source("scripts/start_functions.R")
 source("config/default.cfg")
 
 #Should we avoid calibration
-cfg$recalibrate <- "ifneeded"     # def = "ifneeded" 
+cfg$recalibrate <- "TRUE"     # def = "ifneeded"
 
 #Change the results folder name
-cfg$title<-"Good_Practice_Nitrogen"
+cfg$title<-"Good_Practice_Scenario"
 
-#Output shouldnt include validation
-cfg$output <- c("rds_report")
+#Output
+cfg$output <- c("rds_report","interpolation")
 
 
 # INPUT FILES
-cfg$input <- c("magpie4.1_default_apr19.tgz","additional_data_rev3.68.tgz","isimip_rcp-IPSL_CM5A_LR-rcp2p6-co2_rev38_c200_690d3718e151be1b450b394c1064b1c5.tgz","rev4.32_690d3718e151be1b450b394c1064b1c5_magpie.tgz","rev4.32_690d3718e151be1b450b394c1064b1c5_validation.tgz")
+cfg$input <- c("magpie4.1_default_apr19.tgz","additional_data_rev3.68.tgz","isimip_rcp-IPSL_CM5A_LR-rcp2p6-co2_rev38_c200_690d3718e151be1b450b394c1064b1c5.tgz","rev4.33_690d3718e151be1b450b394c1064b1c5_validation.tgz","rev4.33_690d3718e151be1b450b394c1064b1c5_magpie.tgz")
 
-#NITROGEN UPTAKE SCENARIO DEFAULT 60_60 GoodPractice 85_85
-cfg$gms$c50_scen_neff <- "neff60_60_starty2010" 
-#cfg$gms$c50_scen_neff <- "neff60_60_starty1990"
+#NITROGEN UPTAKE SCENARIO
+cfg$gms$c50_scen_neff <- "neff85_85_starty2010"
 
 #Manure share used in an anaerobic digester 0.15 in 2015 0.30 in 2030, 0.5 in 2050 and 0.7 in 2100
-#cfg$gms$c55_scen_conf<-"GoodPractice"
-cfg$gms$c55_scen_conf<-"ssp2"
+cfg$gms$c55_scen_conf<-"GoodPractice"
 
 #start MAgPIE run
 start_run(cfg=cfg)
