a6193de22544f76cff487e7c0688e90361376ba7
florianh <humpenoeder@pik-potsdam.de>
2020-03-08 16:26:07 +0100

    update

--- scripts/start/ssp2.R
+++ scripts/start/ssp2.R
@@ -23,7 +23,7 @@ source("config/default.cfg")
 
 cfg$results_folder <- "output/:title:"
 
-cfg$title <- "F03_simple_greater"
+cfg$title <- "F03_simple_greater_timberOff"
 cfg <- setScenario(cfg,c("SSP2","NPI"))
 cfg$gms$c56_pollutant_prices <- "R2M41-SSP2-NPi"
 cfg$gms$c60_2ndgen_biodem <- "R2M41-SSP2-NPi"
@@ -34,4 +34,5 @@ cfg$gms$maccs  <- "off_jul16"
 cfg$gms$residues <- "off"
 cfg$gms$s15_elastic_demand <- 0
 #cfg$gms$c32_aff_policy <- "none"
+cfg$gms$s73_timber_demand <- 0
 start_run(cfg,codeCheck=FALSE)
