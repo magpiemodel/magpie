6255a8201717ad3be60b8f024bb95e116aea5e0e
florianh <humpenoeder@pik-potsdam.de>
2020-03-21 14:57:22 +0100

    bugfix

--- scripts/start/sdp_bugfix.R
+++ scripts/start/sdp_bugfix.R
@@ -39,28 +39,28 @@ cfg$output <- c("rds_report")
 
 cfg$gms$sm_fix_SSP2 <- 2020
 
-cfg$title <- "val17_SDP-NPI_2020_procOff"
+cfg$title <- "val17_SDP-NPI_2020"
 cfg <- setScenario(cfg,c("SDP","NPI"))
 cfg$gms$c56_pollutant_prices <- "coupling"
 cfg$gms$c60_2ndgen_biodem <- "coupling"
 getInput("/p/projects/piam/runs/coupled-magpie/output/C_SDP-NPi-mag-4/fulldata.gdx")
 start_run(cfg,codeCheck=FALSE)
 
-cfg$title <- "val17_SSP1-NPI_2020_procOff"
+cfg$title <- "val17_SSP1-NPI_2020"
 cfg <- setScenario(cfg,c("SSP1","NPI"))
 cfg$gms$c56_pollutant_prices <- "coupling"
 cfg$gms$c60_2ndgen_biodem <- "coupling"
 getInput("/p/projects/piam/runs/coupled-magpie/output/C_SSP1-NPi-mag-4/fulldata.gdx")
 start_run(cfg,codeCheck=FALSE)
 
-cfg$title <- "val17_SSP2-NPI_2020_procOff"
+cfg$title <- "val17_SSP2-NPI_2020"
 cfg <- setScenario(cfg,c("SSP2","NPI"))
 cfg$gms$c56_pollutant_prices <- "coupling"
 cfg$gms$c60_2ndgen_biodem <- "coupling"
 getInput("/p/projects/piam/runs/coupled-magpie/output/C_SSP2-NPi-mag-4/fulldata.gdx")
 start_run(cfg,codeCheck=FALSE)
 
-cfg$title <- "val17_SSP5-NPI_2020_procOff"
+cfg$title <- "val17_SSP5-NPI_2020"
 cfg <- setScenario(cfg,c("SSP5","NPI"))
 cfg$gms$c56_pollutant_prices <- "coupling"
 cfg$gms$c60_2ndgen_biodem <- "coupling"
