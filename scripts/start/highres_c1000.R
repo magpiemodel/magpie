746d312bd9ab68b119e40c17727ae728e1d04d2d
florianh <humpenoeder@pik-potsdam.de>
2020-03-27 18:36:08 +0100

    calib

--- scripts/start/highres_c1000.R
+++ scripts/start/highres_c1000.R
@@ -26,8 +26,8 @@ cfg$results_folder <- "output/:title:"
 
 cfg$output <- c("rds_report")
 
-prefix <- "hr01"
-res <- "c10000"
+prefix <- "hr02"
+res <- "c1000"
 
 cfg$input <- c(paste0("isimip_rcp-IPSL_CM5A_LR-rcp2p6-co2_rev42_",res,"_690d3718e151be1b450b394c1064b1c5.tgz"),
                "rev4.42_690d3718e151be1b450b394c1064b1c5_magpie.tgz",
@@ -42,8 +42,11 @@ file.copy(from = paste0("input/input_ghg_price_",co2_price_path,".cs3"), to = "m
 #Download data 
 #system('Rscript start.R runscripts=download_data_only submit=direct')
 
-for (ssp in c("SSP1","SSP2","SSP3","SSP4","SSP5")) {
-  cfg$title <- paste(prefix,ssp,"NPI",res,sep="_")
+calib <- TRUE
+
+if(calib) {
+  ssp <- "SSP2"
+  cfg$title <- paste(prefix,ssp,"NPI",res,"calib",sep="_")
   cfg <- setScenario(cfg,c(ssp,"NPI"))
   
   #get trade pattern from low resolution run with c200
@@ -60,8 +63,33 @@ for (ssp in c("SSP1","SSP2","SSP3","SSP4","SSP5")) {
   cfg$gms$optimization <- "nlp_par"
   cfg$gms$c60_bioenergy_subsidy <- 0
   
+  cfg$recalibrate <- TRUE
+  
   # cfg$gms$c56_pollutant_prices <- "coupling"
   # cfg$gms$c60_2ndgen_biodem <- "coupling"
   start_run(cfg,codeCheck=FALSE)
+} else {
+  for (ssp in c("SSP1","SSP2","SSP3","SSP4","SSP5")) {
+    cfg$title <- paste(prefix,ssp,"NPI",res,sep="_")
+    cfg <- setScenario(cfg,c(ssp,"NPI"))
+    
+    #get trade pattern from low resolution run with c200
+    gdx <- paste0("output/",paste(prefix,ssp,"NPI","c200",sep="_"),"/fulldata.gdx")
+    ov_prod_reg <- readGDX(gdx,"ov_prod_reg",select=list(type="level"))
+    ov_supply <- readGDX(gdx,"ov_supply",select=list(type="level"))
+    f21_trade_balance <- ov_prod_reg - ov_supply
+    #f21_trade_balance.cs3 will be deleted when downloading the high res data. Therefore renamed.
+    write.magpie(round(f21_trade_balance,6),paste0("modules/21_trade/input/f21_trade_balance2.cs3"))
+    manipulateFile("modules/21_trade/exo/input.gms",c("f21_trade_balance.cs3","f21_trade_balance2.cs3"))
+    
+    #use exo trade and parallel optimization
+    cfg$gms$trade <- "exo"
+    cfg$gms$optimization <- "nlp_par"
+    cfg$gms$c60_bioenergy_subsidy <- 0
+    
+    # cfg$gms$c56_pollutant_prices <- "coupling"
+    # cfg$gms$c60_2ndgen_biodem <- "coupling"
+    start_run(cfg,codeCheck=FALSE)
+  }
 }
 
