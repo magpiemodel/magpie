fd40ec137a494b27d8a6f3ab0c39553d4e579a1e
florianh <humpenoeder@pik-potsdam.de>
2020-03-27 22:50:01 +0100

    update

--- scripts/run_submit/submit.sh
+++ scripts/run_submit/submit.sh
@@ -4,9 +4,9 @@
 #SBATCH --job-name=mag-run
 #SBATCH --output=full.log
 #SBATCH --mail-type=END
-#SBATCH --cpus-per-task=12
+#SBATCH --cpus-per-task=13
 #SBATCH --partition=standard
-#SBATCH --mem=32000
+#SBATCH --mem-per-cpu=0
 
 Rscript submit.R
 
