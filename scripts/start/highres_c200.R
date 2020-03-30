6c8cca02f72b9d22a61731b2bd8db5a11a7dd948
florianh <humpenoeder@pik-potsdam.de>
2020-03-30 16:43:19 +0200

    update

--- scripts/start/highres_c200.R
+++ scripts/start/highres_c200.R
@@ -27,12 +27,12 @@ cfg$results_folder <- "output/:title:"
 cfg$output <- c("rds_report")
 
 prefix <- "hr02"
-res <- "c200"
+res <- "c1000"
 
 cfg$input <- c(paste0("isimip_rcp-IPSL_CM5A_LR-rcp2p6-co2_rev42_",res,"_690d3718e151be1b450b394c1064b1c5.tgz"),
                "rev4.42_690d3718e151be1b450b394c1064b1c5_magpie.tgz",
                "rev4.42_690d3718e151be1b450b394c1064b1c5_validation.tgz",
-               "calibration_H12_c200_26Feb20.tgz",
+               paste0("calibration_H12_",res,"_highres.tgz"),
                "additional_data_rev3.78.tgz")
 
 co2_price_path <- "2deg"
@@ -41,7 +41,7 @@ file.copy(from = paste0("input/input_ghg_price_",co2_price_path,".cs3"), to = "m
 
 
 for (ssp in c("SSP1","SSP2","SSP3","SSP4","SSP5")) {
-  cfg$title <- paste(prefix,ssp,co2_price_path,res,sep="_")
+  cfg$title <- paste(prefix,ssp,co2_price_path,res,"glo",sep="_")
   if (co2_price_path == "2deg") {
     cfg <- setScenario(cfg,c(ssp,"NDC"))
   } else if (co2_price_path == "NPI") {
