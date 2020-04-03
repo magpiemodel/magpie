1dcae19d4db67773fa0d52005e59b4f20e682440
abhimishr <mishra.abhijeet28@gmail.com>
2020-04-03 02:08:41 +0200

    Scenario config adapted until PR154 fix. Merge from latest develop

--- scripts/start/dynamic_forestry_DIESNTANGLE.R
+++ scripts/start/dynamic_forestry_DIESNTANGLE.R
@@ -28,20 +28,20 @@ cfg$recalc_npi_ndc <- "ifneeded"
 log_folder <- "run_details"
 dir.create(log_folder,showWarnings = FALSE)
 
-identifier_flag <- "BF03x-DUMMY-"
-cat(paste0("Manual pull from latest develop 3rd Apr 2020. Needed because carbonstock calculation throws warning."),file=paste0(log_folder,"/",identifier_flag,".txt"),append=F)
+identifier_flag <- "BF04"
+cat(paste0("SCEN CONFIG HAD TO BE CHANGED ASK FELI FOR FIX.Manual copy paste from latest develop 3rd Apr 2020. Needed because carbonstock calculation throws warning."),file=paste0(log_folder,"/",identifier_flag,".txt"),append=F)
 
-for(ssp in c("SSP2")){
+for(ssp in c("SSP1","SSP2","SSP3","SSP4","SSP5")){
 
   for(c32_rotation_extension in c(0)){
 
     cfg$gms$c32_rotation_extension <- c32_rotation_extension
 
-    for(timber_demand in c("biomass_mar20")){ ## Add "off" here to turn off timber demand
+    for(timber_demand in c("biomass_mar20","off")){ ## Add "off" here to turn off timber demand
 
       cfg$gms$timber <- timber_demand
 
-      for (co2_price_path in c("NPI")) { ## Add "2deg" here for CO2 price runs
+      for (co2_price_path in c("NPI","2deg")) { ## Add "2deg" here for CO2 price runs
 
         if (co2_price_path == "NPI") {
           cfg <- setScenario(cfg,c(ssp,"NPI"))
@@ -49,12 +49,12 @@ for(ssp in c("SSP2")){
           cfg <- setScenario(cfg,c(ssp,"NDC"))
         }
 
-        for(emis_policy in c("redd+_nosoil")){ ## Add "ssp_nosoil" for policy penalizing only natveg emissions
+        for(emis_policy in c("redd+_nosoil","ssp_nosoil")){ ## Add "ssp_nosoil" for policy penalizing only natveg emissions
 
           cfg$gms$c56_emis_policy <- emis_policy
 
-          cfg$gms$c56_pollutant_prices <- "coupling"
-          cfg$gms$c60_2ndgen_biodem <- "coupling"
+          cfg$gms$c56_pollutant_prices_select <- "coupling"
+          cfg$gms$c60_2ndgen_biodem_select <- "coupling"
 
           file.copy(from = paste0("input/input_bioen_dem_",co2_price_path,".csv"), to = "modules/60_bioenergy/input/reg.2ndgen_bioenergy_demand.csv",overwrite = TRUE)
           file.copy(from = paste0("input/input_ghg_price_",co2_price_path,".cs3"), to = "modules/56_ghg_policy/input/f56_pollutant_prices_coupling.cs3",overwrite = TRUE)
@@ -67,7 +67,7 @@ for(ssp in c("SSP2")){
           if(emis_policy == "redd+_nosoil") pol_flag = "REDD"
           if(emis_policy == "ssp_nosoil") pol_flag = "SSP"
 
-          cfg$title <- paste0(identifier_flag,"_",demand_flag,"_","c",co2_price_path,"_",pol_flag,"_",cfg$gms$c32_aff_policy)
+          cfg$title <- paste0(identifier_flag,"_",ssp,"_",demand_flag,"_","c",co2_price_path,"_",pol_flag,"_",cfg$gms$c32_aff_policy)
 
           cfg$output <- c("rds_report")
 
@@ -97,17 +97,17 @@ cfg$results_folder <- "output/:title:"
 
 cfg$recalc_npi_ndc <- TRUE
 
-for(ssp in c("SSP2")){
+for(ssp in c("SSP1","SSP2","SSP3","SSP4","SSP5")){
 
   for(c32_rotation_extension in c(0)){
 
     cfg$gms$c32_rotation_extension <- c32_rotation_extension
 
-    for(timber_demand in c("biomass_mar20")){
+    for(timber_demand in c("biomass_mar20","off")){
 
       cfg$gms$timber <- timber_demand
 
-      for (co2_price_path in c("NPI")) {
+      for (co2_price_path in c("NPI")) { ## Add "2deg" here for CO2 price runs
 
         cfg <- setScenario(cfg,c(ssp,"NDC"))
 
@@ -129,13 +129,13 @@ for(ssp in c("SSP2")){
           if(emis_policy == "redd+_nosoil") pol_flag = "REDD"
           if(emis_policy == "ssp_nosoil") pol_flag = "SSP"
 
-          cfg$title <- paste0(identifier_flag,"_",demand_flag,"_","c",co2_price_path,"_",pol_flag,"_",cfg$gms$c32_aff_policy)
+          cfg$title <- paste0(identifier_flag,"_",ssp,"_",demand_flag,"_","c",co2_price_path,"_",pol_flag,"_",cfg$gms$c32_aff_policy)
 
           cfg$output <- c("rds_report")
 
 #          cat(cfg$title,"\n")
 
-#          start_run(cfg,codeCheck=FALSE)
+          start_run(cfg,codeCheck=FALSE)
         }
       }
     }
