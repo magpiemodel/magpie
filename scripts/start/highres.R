16ecce76244597fb7b79a1b87a4194d44e4059d0
florianh <humpenoeder@pik-potsdam.de>
2020-05-10 13:15:40 +0200

    update highres start script

--- scripts/start/highres.R
+++ scripts/start/highres.R
@@ -25,15 +25,15 @@ cfg$results_folder <- "output/:title:"
 
 cfg$output <- c("rds_report","highres")
 
-prefix <- "T06"
+prefix <- "T07"
 
 #cfg$qos <- "priority"
 lr <- "c200"
 cfg$input <- c(paste0("isimip_rcp-IPSL_CM5A_LR-rcp2p6-co2_rev44_",lr,"_690d3718e151be1b450b394c1064b1c5.tgz"),
-               "rev4.42_690d3718e151be1b450b394c1064b1c5_magpie.tgz",
-               "rev4.42_690d3718e151be1b450b394c1064b1c5_validation.tgz",
+               "rev4.44_h12_magpie.tgz",
+               "rev4.44_h12_validation.tgz",
                paste0("calibration_H12_",lr,"_26Feb20.tgz"),
-               "additional_data_rev3.78.tgz")
+               "additional_data_rev3.79.tgz")
 
 download_and_update(cfg)
 
@@ -42,7 +42,7 @@ scen <- "2p6"
   
 for (ssp in ssps) {
   cfg$title <- paste(prefix,ssp,scen,sep="_")
-  cfg <- setScenario(cfg,c(ssp,"BASE"))
+  cfg <- setScenario(cfg,c(ssp,"NDC"))
   cfg$gms$c56_pollutant_prices <- "SSPDB-SSP2-26-REMIND-MAGPIE"
   cfg$gms$c60_2ndgen_biodem <- "SSPDB-SSP2-26-REMIND-MAGPIE"
   start_run(cfg,codeCheck=FALSE)
