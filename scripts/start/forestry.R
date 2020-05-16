c0889c884f4910628ce8c0c58f01f2ffeed19d57
abhimishr <mishra.abhijeet28@gmail.com>
2020-05-16 20:47:04 +0200

    Clib bugfix in input files

--- scripts/start/forestry.R
+++ scripts/start/forestry.R
@@ -25,9 +25,9 @@ source("config/default.cfg")
 
 ### INPUT FILES
 cfg$input <- c("isimip_rcp-IPSL_CM5A_LR-rcp2p6-co2_rev44_c200_690d3718e151be1b450b394c1064b1c5.tgz",
-			   "calibration_H12_c200_26Feb20.tgz",
          "rev4.44forestry_h12_magpie.tgz",
          "rev4.44forestry_h12_validation.tgz",
+ 			   "calibration_H12_c200_26Feb20.tgz",
          "forestry_20200513.tgz",
          "coupling_co2_prices_apr20.tgz",
          "additional_data_rev3.80.tgz")
