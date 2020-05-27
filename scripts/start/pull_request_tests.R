3cb40d27728cd0bbe034ecae7d1ccf7865de3068
abhimishr <mishra.abhijeet28@gmail.com>
2020-05-27 17:44:22 +0200

    Update PR run script

--- scripts/start/pull_request_tests.R
+++ scripts/start/pull_request_tests.R
@@ -28,19 +28,17 @@ version <- cfg$model_version
 
 for(ssp in c("SSP2")) { ## Add SSP* here for testing other SSPs. Basic test should be for SSP2
 
-  for (co2_price_path in c("BAU","POL")) {
+  for (co2_price_path in c("BAU")) {
 
     if (co2_price_path == "BAU") {
       cfg <- setScenario(cfg,c(ssp,"NPI"))
-      co2_price_path_flag = "BAU"
     } else if (co2_price_path == "POL"){
       cfg <- setScenario(cfg,c(ssp,"NDC"))
-      co2_price_path_flag = "POL"
       cfg$gms$c56_pollutant_prices <- "R2M41-SSP2-Budg600" #update to most recent coupled runs asap
       cfg$gms$c60_2ndgen_biodem <- "R2M41-SSP2-Budg600" ##update to most recent coupled runs asap
     }
 
-    cfg$title <- paste0(pr_flag,"_",user,"_",version,"_",ssp,"-",co2_price_path_flag) #PR_mishra_4.2-forestry_SSP2-BAU
+    cfg$title <- paste0(pr_flag,"_",user,"_",version,"_",ssp,"-",co2_price_path) #PR_mishra_4.2-forestry_SSP2-BAU
 
     cfg$output <- c("rds_report")
 
