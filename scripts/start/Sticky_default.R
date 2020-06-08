78bfd9a239fc2cdfa0cc57c09ac0ddbff180a6f0
emolinab <mbacca@pik-potsdam.de>
2020-06-08 17:20:37 +0200

    Unconstrained

--- scripts/start/Sticky_default.R
+++ scripts/start/Sticky_default.R
@@ -15,7 +15,7 @@ resolutions<-c("200")
 #Factor cost realizations
 realization<-c("sticky_feb18")
 # Trade realization
-title<-c("Sticky_Constraint_invAnn_TRUECalib")
+title<-c("Sticky_UnConstraint_TRUECalib")
 
 for (i in 1:length(resolutions)){
 for(j in 1:length(realization)){
