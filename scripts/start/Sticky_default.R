ef1f3ff8bebd7998da482124962aa11d27eeca29
emolinab <mbacca@pik-potsdam.de>
2020-06-09 16:25:36 +0200

    Script and run with maximum Investment annuity of 1e7

--- scripts/start/Sticky_default.R
+++ scripts/start/Sticky_default.R
@@ -15,7 +15,7 @@ resolutions<-c("200")
 #Factor cost realizations
 realization<-c("sticky_feb18")
 # Trade realization
-title<-c("Sticky_UnConstraint_TRUECalib")
+title<-c("Sticky_Cons_InvAnn_1e7_TRUECalib")
 
 for (i in 1:length(resolutions)){
 for(j in 1:length(realization)){
