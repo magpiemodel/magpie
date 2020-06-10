2c29834f702dc592b42796560278b67d6745ba88
abhimishr <mishra.abhijeet28@gmail.com>
2020-06-10 17:34:17 +0200

    Combine PR runs and forestry runs in one script (will be edited before PR is approved)

--- scripts/start/forestry.R
+++ scripts/start/forestry.R
@@ -137,3 +137,51 @@ for(secdf_distribution in c(0)){ 					## (0) for all in highest acx (1) for equa
     }
   }
 }
+
+##### Version log (YYYYMMDD - Description - Author(s))
+## 20200527 - Default SSP2 Baseline and Policy runs - FH,AM,EMJB,JPD
+
+## Load lucode2 and gms to use setScenario later
+library(lucode2)
+library(gms)
+
+# Load start_run(cfg) function which is needed to start MAgPIE runs
+source("scripts/start_functions.R")
+
+# Source default cfg. This loads the object "cfg" in R environment
+source("config/default.cfg")
+
+# Use user name and model version defined in default.cfg for generating the titel
+identifier_flag <- "PR187_01"
+
+# Grab user name
+user <- Sys.info()[["user"]]
+#version <- cfg$model_version ## Havong this somehow throws compilation errors in maccs module
+
+cfg$results_folder <- "output/:title:"
+
+## Create a set of runs based on default.cfg
+
+for(ssp in c("SSP2")) { ## Add SSP* here for testing other SSPs. Basic test should be for SSP2
+
+  for (co2_price_path in c("BAU","POL")) {
+
+    if (co2_price_path == "BAU") {
+      cfg <- setScenario(cfg,c(ssp,"NPI"))
+      cfg$gms$c56_pollutant_prices <- "R2M41-SSP2-NPi" #update to most recent coupled runs asap
+      cfg$gms$c60_2ndgen_biodem <- "R2M41-SSP2-NPi" ##update to most recent coupled runs asap
+
+    } else if (co2_price_path == "POL"){
+      cfg <- setScenario(cfg,c(ssp,"NDC"))
+      cfg$gms$c56_pollutant_prices <- "SSPDB-SSP2-26-REMIND-MAGPIE" #update to most recent coupled runs asap
+      cfg$gms$c60_2ndgen_biodem <- "SSPDB-SSP2-26-REMIND-MAGPIE" ##update to most recent coupled runs asap
+    }
+
+    cfg$title <- paste0(identifier_flag,"_",user,"_",ssp,"-",co2_price_path) #Create easily distinguishable run title
+
+    cfg$output <- c("rds_report") # Only run rds_report after model run
+
+    start_run(cfg,codeCheck=TRUE) # Start MAgPIE run
+    #cat(cfg$title)
+  }
+}
