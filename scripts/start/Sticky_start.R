985f4a6df55c409b13ea8f132282c16d3fbbf44a
emolinab <mbacca@pik-potsdam.de>
2020-08-12 17:26:02 +0200

    try

--- scripts/start/Sticky_start.R
+++ scripts/start/Sticky_start.R
@@ -13,7 +13,7 @@ source("config/default.cfg")
 realization<-c("sticky_feb18","mixed_feb17")
 
 #inputs
-cfg$input <- c("isimip_rcp-IPSL_CM5A_LR-rcp2p6-co2_rev44_c200_690d3718e151be1b450b394c1064b1c5.tgz",
+cfg$input <- c("isimip_rcp-IPSL_CM5A_LR-rcp2p6-co2_rev45_c200_690d3718e151be1b450b394c1064b1c5.tgz",
 			   "rev4.44_h12_magpie.tgz",
 			   "rev4.44_h12_validation.tgz",
 			   "calibration_H12_c200_26Feb20.tgz",
