0849c418268bd17bcf973eec5ff4db15940e84ef
abhimishr <mishra.abhijeet28@gmail.com>
2020-09-11 12:17:22 +0200

    Start script update

--- scripts/start/projects/rcTest_default.R
+++ scripts/start/projects/rcTest_default.R
@@ -25,7 +25,7 @@ source("scripts/start_functions.R")
 source("config/default.cfg")
 
 # choose a meaningful Pull Request (PR) flag
-pr_flag <- "rc_Default_02"
+pr_flag <- "PR214_def01"
 
 # Grab user name
 user <- Sys.info()[["user"]]
