7816c50407afda32bfeb414e2ef2bbbf4d0c4e77
emolinab <mbacca@pik-potsdam.de>
2020-09-22 09:00:58 +0200

    Script to test runs

--- scripts/start/lpjml5_test_noMitigation.R
+++ scripts/start/lpjml5_test_noMitigation.R
@@ -8,47 +8,67 @@ source("scripts/start_functions.R")
 # Sources the default configuration file
 source("config/default.cfg")
 
-#List of resolutions forruns
-resolutions<-c("200")
+#List of clusterin types
+clustering<-c("c200","n200")
+
 #Factor cost realizations
 realization<-c("mixed_feb17","sticky_feb18")
 climate<-c("nocc","cc")
+factors<-c("mixed","sticky")
 
 
-for (i in 1:length(climate)){
-for(j in 1:length(realization)){
+for (k in 1:length(clustering)){
+  for (i in 1:length(climate)){
+    for(j in 1:length(realization)){
 
 
-#Inputs
-cfg$input <- c("rev4.47+mrmagpie6_h12_magpie.tgz",
-               "rev4.47+mrmagpie6_h12_cfc9a5551f05ca4efc6cbc7016516432_cellularmagpie.tgz",
-               "rev4.47+mrmagpie6_h12_validation.tgz",
-               "additional_data_rev3.85.tgz")
+        #Inputs
+        if(clustering=="c200"){
+
+          cfg$input <- c("rev4.47+mrmagpie6_h12_magpie.tgz",
+                 "rev4.47+mrmagpie7_h12_238dd4e69b15586dde74376b6b84cdec_cellularmagpie_debug.tgz",
+                 "rev4.47+mrmagpie6_h12_validation.tgz",
+                 "additional_data_rev3.85.tgz",
+                 paste0("calibration_H12_",factors[j],"_feb18_21Sep20.tgz")
+                )
+
+        }else if(clustering=="n200"){
+
+          cfg$input <- c("rev4.47+mrmagpie6_h12_magpie.tgz",
+                 "rev4.47+mrmagpie7_h12_4ade54491b634b981be2d6c4a0d17706_cellularmagpie_debug.tgz",
+                 "rev4.47+mrmagpie6_h12_validation.tgz",
+                 "additional_data_rev3.85.tgz",
+                calibration file
+                paste0("calibration_H12_",factors[j],"_feb18_21Sep20.tgz")
+              )
+        }
 
 
-#Change the results folder name
-cfg$title<-paste0("LPjmL_Tests_",realization[j],"_c200","_HadGEM2_ES","_","rcp6p0","_CO2_",climate[i])
+        #Change the results folder name
+        cfg$title<-paste0("LPj_T_",realization[j],"_",clustering[k],"_HadGEM2_ES","_","rcp2p6","_CO2_",climate[i])
 
 
-#recalibrate
-cfg$recalibrate <- TRUE
+        #recalibrate
+        cfg$recalibrate <- FALSE
 
-#recalc_npi_ndc
-cfg$recalc_npi_ndc <- TRUE
+        #recalc_npi_ndc
+        cfg$recalc_npi_ndc <- TRUE
 
-#forestry
-cfg$gms$forestry  <- "static_sep16"
+        #forestry
+        cfg$gms$forestry  <- "static_sep16"
 
 
-#Factor costs realization
-cfg$gms$factor_costs <- realization[j]
+        #Factor costs realization
+        cfg$gms$factor_costs <- realization[j]
 
-#Climate impact or not
-  cfg$gms$c14_yields_scenario  <- climate[i]
-  cfg$gms$c42_watdem_scenario  <- climate[i]
-  cfg$gms$c52_carbon_scenario  <- climate[i]
-  cfg$gms$c43_watavail_scenario<- climate[i]
-  cfg$gms$c59_som_scenario  <- climate[i]
+        #Climate impact or not
+        cfg$gms$c14_yields_scenario  <- climate[i]
+        cfg$gms$c42_watdem_scenario  <- climate[i]
+        cfg$gms$c52_carbon_scenario  <- climate[i]
+        cfg$gms$c43_watavail_scenario<- climate[i]
+        cfg$gms$c59_som_scenario  <- climate[i]
 
-start_run(cfg=cfg)
-}}
+        start_run(cfg=cfg)
+        }
+      }
+    }
