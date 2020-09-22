75d32cfab9220dfedc35b806f36da127d31acd75
Jan Philipp Dietrich <dietrich@pik-potsdam.de>
2020-09-22 11:24:21 +0200

    fixed disaggregation.R

--- scripts/output/extra/disaggregation.R
+++ scripts/output/extra/disaggregation.R
@@ -15,9 +15,9 @@ library(magpie4)
 library(luscale)
 library(madrat)
 
-############################# BASIC CONFIGURATION #######################################
+############################# BASIC CONFIGURATION ##############################
 if(!exists("source_include")) {
-  outputdir       <- "output/SSP2_Ref_c200"
+  outputdir <- "output/SSP2_Ref_c200"
   readArgs("outputdir")
 }
 map_file                   <- Sys.glob(path(outputdir, "clustermap_*.rds"))
@@ -26,7 +26,7 @@ land_hr_file               <- path(outputdir,"avl_land_t_0.5.mz")
 land_hr_out_file           <- path(outputdir,"cell.land_0.5.mz")
 land_hr_share_out_file     <- path(outputdir,"cell.land_0.5_share.mz")
 croparea_hr_share_out_file <- path(outputdir,"cell.croparea_0.5_share.mz")
-#########################################################################################
+################################################################################
 
 if(length(map_file)==0) stop("Could not find map file!")
 if(length(map_file)>1) {
@@ -40,7 +40,8 @@ land_lr   <- land(gdx,sum=FALSE,level="cell")
 land_ini  <- setYears(read.magpie(land_hr_file)[,"y1995",],NULL)
 land_ini  <- land_ini[,,getNames(land_lr)]
 if(any(land_ini < 0)) {
-  warning(paste0("Negative values in inital high resolution dataset detected and set to 0. Check the file ",land_hr_file))
+  warning(paste0("Negative values in inital high resolution dataset ",
+                 "detected and set to 0. Check the file ",land_hr_file))
   land_ini[which(land_ini < 0,arr.ind = T)] <- 0
 }
 
@@ -53,26 +54,33 @@ land_hr <- luscale::interpolate2(x     = land_lr,
 
 # Write outputs
 
-.tmpwrite <- function(x,file,comment) {
+.dissagcrop <- function(gdx, land_hr, map) {
+  message("Disaggregation crop types")
+  area     <- croparea(gdx, level="cell", products="kcr",
+                       product_aggr=FALSE,water_aggr = FALSE)
+  area_shr <- area/(dimSums(area,dim=3) + 10^-10)
+
+  # calculate share of crop land on total cell area
+  crop_shr <- land_hr/dimSums(land_hr, dim=3)
+  crop_shr <- setNames(crop_shr[,getYears(area_shr),"crop"],NULL)
+  # calculate crop area as share of total cell area
+  area_shr_hr <- madrat::toolAggregate(area_shr, map, to="cell") * crop_shr
+  return(area_shr_hr)
+}
+
+.tmpwrite <- function(x,file,comment,message) {
   write.magpie(x, file, comment=comment)
   write.magpie(x, sub(".mz",".nc",file), comment=comment, verbose=FALSE)
 }
 
-message("Write outputs cell.land")
-.tmpwrite(land_hr, land_hr_out_file, comment="unit: Mha per grid-cell")
+.tmpwrite(land_hr, land_hr_out_file, comment="unit: Mha per grid-cell",
+          message="Write outputs cell.land")
+.tmpwrite(land_hr/dimSums(land_hr,dim=3.1), land_hr_share_out_file,
+          comment="unit: grid-cell land area fraction",
+          message="Write outputs cell.land_share")
 
-message("Write outputs cell.land_share")
-.tmpwrite(land_hr/dimSums(land_hr,dim=3.1), land_hr_share_out_file, comment="unit: grid-cell land area fraction")
+area_shr_hr <- .dissagcrop(gdx, land_hr, map=map_file)
 
-message("Disaggregation crop types")
-# total crop tpye specific croparea
-area     <- croparea(gdx,level="cell",products="kcr",product_aggr=FALSE,water_aggr = FALSE)
-area_shr <- area/(dimSums(area,dim=3) + 10^-10)
-
-# calculate share of crop land on total cell area
-crop_shr <- setNames(land_hr/dimSums(land_hr, dim=3)[getYears(area_shr),"crop"],NULL)
-# calculate crop area as share of total cell area
-area_shr_hr <- madrat::toolAggregate(area_shr,map_file) * crop_shr
-
-message("Write outputs cell.cropara_share")
-.tmpwrite(area_shr_hr, croparea_hr_share_out_file, comment="unit: grid-cell land area fraction")
+.tmpwrite(area_shr_hr, croparea_hr_share_out_file,
+          comment="unit: grid-cell land area fraction",
+          message="Write outputs cell.cropara_share")
