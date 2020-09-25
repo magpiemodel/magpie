e4a97c6f1103cc85d67f851252063f303465b47c
emolinab <mbacca@pik-potsdam.de>
2020-09-25 14:53:10 +0200

    Script rcp2p6 climate

--- scripts/start/lpjml5_test_noMitigation.R
+++ scripts/start/lpjml5_test_noMitigation.R
@@ -14,10 +14,10 @@ clustering<-c("n200","c200")
 #Factor cost realizations
 realization<-c("mixed_feb17","sticky_feb18")
 climate<-c("cc")
-
+AEI<-c("LUH2v2","Siebert")
 
 for (k in 1:length(clustering)){
-  for (i in 1:length(climate)){
+  for (i in 1:length(AEI)){
     for(j in 1:length(realization)){
 
 
@@ -25,7 +25,7 @@ for (k in 1:length(clustering)){
         if(clustering[k]=="c200"){
 
           cfg$input <- c("rev4.47+mrmagpie6_h12_magpie.tgz",
-                 "rev4.47+mrmagpie6_h12_cfc9a5551f05ca4efc6cbc7016516432_cellularmagpie.tgz",
+                 "rev4.47+mrmagpie7_h12_238dd4e69b15586dde74376b6b84cdec_cellularmagpie_debug.tgz",
                  "rev4.47+mrmagpie6_h12_validation.tgz",
                  "additional_data_rev3.85.tgz"
                 )
@@ -33,7 +33,7 @@ for (k in 1:length(clustering)){
         }else if(clustering[k]=="n200"){
 
           cfg$input <- c("rev4.47+mrmagpie6_h12_magpie.tgz",
-                 "rev4.47+mrmagpie6_h12_a6b8f5fe756c420d9f350b2b6fb8b4c2_cellularmagpie.tgz",
+                 "rev4.47+mrmagpie7_h12_4ade54491b634b981be2d6c4a0d17706_cellularmagpie_debug.tgz",
                  "rev4.47+mrmagpie6_h12_validation.tgz",
                  "additional_data_rev3.85.tgz"
               )
@@ -41,7 +41,7 @@ for (k in 1:length(clustering)){
 
 
         #Change the results folder name
-        cfg$title<-paste0("LPj_T_CALIB_",realization[j],"_",clustering[k],"_HadGEM2_ES","_","rcp2p6","_CO2_",climate[i])
+        cfg$title<-paste0("CalibRuns/LPj_T_CALIB_",realization[j],"_",clustering[k],"_",AEI[i],"_","rcp2p6","_CO2_",climate[i])
 
 
         #recalibrate
@@ -53,16 +53,18 @@ for (k in 1:length(clustering)){
         #forestry
         cfg$gms$forestry  <- "static_sep16"
 
+        #AEI
+        cfg$gms$c41_initial_irrigation_area  <- AEI[i]
 
         #Factor costs realization
         cfg$gms$factor_costs <- realization[j]
 
         #Climate impact or not
-        cfg$gms$c14_yields_scenario  <- climate[i]
-        cfg$gms$c42_watdem_scenario  <- climate[i]
-        cfg$gms$c52_carbon_scenario  <- climate[i]
-        cfg$gms$c43_watavail_scenario<- climate[i]
-        cfg$gms$c59_som_scenario  <- climate[i]
+        cfg$gms$c14_yields_scenario  <- climate[1]
+        cfg$gms$c42_watdem_scenario  <- climate[1]
+        cfg$gms$c52_carbon_scenario  <- climate[1]
+        cfg$gms$c43_watavail_scenario<- climate[1]
+        cfg$gms$c59_som_scenario  <- climate[1]
 
         start_run(cfg=cfg)
         }
