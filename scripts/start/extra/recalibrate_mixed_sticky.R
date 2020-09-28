c5bc81b046a0bd0284452aa9aa1ecd3000a43b86
emolinab <mbacca@pik-potsdam.de>
2020-09-28 15:12:30 +0200

    Calibration run for diferent factor costs, clustering and available irrigated area

--- scripts/start/extra/recalibrate_mixed_sticky.R
+++ scripts/start/extra/recalibrate_mixed_sticky.R
@@ -7,7 +7,7 @@
 
 # --------------------------------------------------------
 # description: calculate and store new calibration factors for different factor
-# costs realization
+# costs realization, clustering and available irrigated area
 # --------------------------------------------------------
 
 
@@ -20,20 +20,53 @@ source("scripts/start_functions.R")
 #start MAgPIE run
 source("config/default.cfg")
 
+remove <- dir(pattern=c("*.cs3")) # ?dir
+file.remove(remove,recursive=FALSE) # ?file.remove
+
 
 cfg$results_folder <- "output/:title:"
 cfg$recalibrate <- TRUE
 
 realization<-c("mixed_feb17","sticky_feb18")
+clustering<-c("n200","c200")
+AEI<-c("LUH2v2","Siebert")
 
 for (i in realization){
-cfg$title <- paste0("calib_run_",i)
+  for (k in clustering){
+    for (av in AEI)
+cfg$title <- paste0("calib_run_",i,"_",k,"_",av)
+
 #Selects factor costs realization
 cfg$gms$factor_costs <- i
 
 cfg$gms$c_timesteps <- 1
 cfg$output <- c("report")
 cfg$sequential <- TRUE
+
+#Selects inputs based on clustering
+if(clustering[k]=="c200"){
+
+  cfg$input <- c("rev4.47+mrmagpie7_h12_magpie_debug.tgz",
+         "rev4.47+mrmagpie7_h12_238dd4e69b15586dde74376b6b84cdec_cellularmagpie_debug.tgz",
+         "rev4.47+mrmagpie7_h12_validation_debug.tgz",
+         "additional_data_rev3.85.tgz"
+        )
+
+}else if(clustering[k]=="n200"){
+
+  cfg$input <- c("rev4.47+mrmagpie7_h12_magpie_debug.tgz",
+         "rev4.47+mrmagpie7_h12_4ade54491b634b981be2d6c4a0d17706_cellularmagpie_debug.tgz",
+         "rev4.47+mrmagpie7_h12_validation_debug.tgz",
+         "additional_data_rev3.85.tgz"
+      )
+
+
+# AEI switch
+
+cfg$gms$c41_initial_irrigation_area  <- av
+}
+
 start_run(cfg,codeCheck=FALSE)
-magpie4::submitCalibration(paste0("H12_",i))
+magpie4::submitCalibration(paste0("H12_",i,"_",k,"_",av))
+}
 }
