e2adec771091a6bfe952194edec1bbc3df8f10e7
emolinab <mbacca@pik-potsdam.de>
2020-10-19 14:41:02 +0200

    As many iterations as needed,calib working .(Ignore Paper_Runs.R script)

---
+++ scripts/start/Paper_Runs.R
@@ -0,0 +1,9 @@
+######################################################################################
+#### Script to start a MAgPIE run using different factor_costs realizations ##########
+######################################################################################
+
+# Load start_run(cfg) function which is needed to start MAgPIE runs
+source("scripts/start_functions.R")
+
+# Sources the default configuration file
+source("config/default.cfg")
