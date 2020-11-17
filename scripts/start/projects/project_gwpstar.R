500ffc75052f7527d6f9026ef80e8b2b374656d2
florianh <humpenoeder@pik-potsdam.de>
2020-11-17 15:02:54 +0100

    bugfix

--- scripts/start/projects/project_gwpstar.R
+++ scripts/start/projects/project_gwpstar.R
@@ -34,48 +34,39 @@ cfg$output <- c("rds_report","projects/agmip_report","validation","extra/disaggr
 
 prefix <- "V1"
 
-#################################################################
-# 1 Baseline SSP1-3 simulations "_NoMt_NoCC"                    #
-#################################################################
-
+cfg <- setScenario(cfg,c("SSP2","NPI"))
 
 cfg$title <- paste(prefix,"SSP2_RCPREF_C0000_REFDIET",sep = "_")
-cfg <- gms::setScenario(cfg,c("SSP2","NPI"))
+cfg$gms$c15_livescen_target <- "constant"
 cfg$gms$c56_pollutant_prices <- "R2M41-SSP2-NPi"
 cfg$gms$s56_gwpstar <- 0
 start_run(cfg,codeCheck=FALSE)
 
 cfg$title <- paste(prefix,"SSP2_RCPREF_C0000_LSPCUT",sep = "_")
-cfg <- gms::setScenario(cfg,c("SSP2","NPI"))
-cfg <- setScenario(cfg,c("SSP2","NPI"))
 cfg$gms$c15_livescen_target <- "lin_zero_20_70"
 cfg$gms$c56_pollutant_prices <- "R2M41-SSP2-NPi"
 cfg$gms$s56_gwpstar <- 0
 start_run(cfg,codeCheck=FALSE)
 
 cfg$title <- paste(prefix,"SSP2_RCPREF_C0150_REFDIET",sep = "_")
-cfg <- gms::setScenario(cfg,c("SSP2","NPI"))
+cfg$gms$c15_livescen_target <- "constant"
 cfg$gms$c56_pollutant_prices <- "coupling"
 cfg$gms$s56_gwpstar <- 0
 start_run(cfg,codeCheck=FALSE)
 
 cfg$title <- paste(prefix,"SSP2_RCPREF_C0150_GWPSTAR_REFDIET",sep = "_")
-cfg <- gms::setScenario(cfg,c("SSP2","NPI"))
+cfg$gms$c15_livescen_target <- "constant"
 cfg$gms$c56_pollutant_prices <- "coupling"
 cfg$gms$s56_gwpstar <- 1
 start_run(cfg,codeCheck=FALSE)
 
 cfg$title <- paste(prefix,"SSP2_RCPREF_C0150_LSPCUT",sep = "_")
-cfg <- gms::setScenario(cfg,c("SSP2","NPI"))
-cfg <- setScenario(cfg,c("SSP2","NPI"))
 cfg$gms$c15_livescen_target <- "lin_zero_20_70"
 cfg$gms$c56_pollutant_prices <- "coupling"
 cfg$gms$s56_gwpstar <- 0
 start_run(cfg,codeCheck=FALSE)
 
 cfg$title <- paste(prefix,"SSP2_RCPREF_C0150_GWPSTAR_LSPCUT",sep = "_")
-cfg <- gms::setScenario(cfg,c("SSP2","NPI"))
-cfg <- setScenario(cfg,c("SSP2","NPI"))
 cfg$gms$c15_livescen_target <- "lin_zero_20_70"
 cfg$gms$c56_pollutant_prices <- "coupling"
 cfg$gms$s56_gwpstar <- 1
