e59389c963537c17babb2aa1bfbf45ee73343366
florianh <humpenoeder@pik-potsdam.de>
2020-11-26 22:43:55 +0100

    update start script

--- scripts/start/projects/project_gwpstar.R
+++ scripts/start/projects/project_gwpstar.R
@@ -40,7 +40,7 @@ pollutant_prices <- function(price_ref=0, CH4_factor=1) {
   a[,,] <- 0
   
   for (y in seq(year_start,2150,by=5)) {
-    if (y < year_end) {
+    if (y <= year_end) {
       a[,y,"ch4"] <- price_ref*(1+growth_rate)^-(year_ref-y)*GWP100_CH4*CH4_factor
       a[,y,c("n2o_n_direct","n2o_n_indirect")] <- price_ref*(1+growth_rate)^-(year_ref-y)*N2O_N*GWP100_N2O
     } else {
@@ -61,18 +61,15 @@ cfg$output <- c("rds_report","projects/agmip_report","validation","extra/disaggr
 
 prefix <- "V12"
 
+cfg$gms$c_timesteps <- "5year2070"
+
 cfg <- setScenario(cfg,c("SSP2","NPI"))
-cfg$gms$c57_macc_version  <- "PBL_2007"   # def = PBL_2007
-cfg$gms$s56_limit_ch4_n2o_price <- 1000   # def = 1000
+cfg$gms$c57_macc_version  <- "PBL_2019"   # def = PBL_2007
+cfg$gms$s56_limit_ch4_n2o_price <- 4000   # def = 1000
 cfg$gms$c56_pollutant_prices <- "coupling"
 
 cfg$title <- paste(prefix,"SSP2_RCPREF_C0000_REFDIET",sep = "_")
 cfg$gms$c15_livescen_target <- "constant"
-pollutant_prices(price_ref = 0,CH4_factor = 1)
-start_run(cfg,codeCheck=FALSE)
-
-cfg$title <- paste(prefix,"SSP2_RCPREF_C0000_LSPCUT",sep = "_")
-cfg$gms$c15_livescen_target <- "lin_zero_20_70"
 pollutant_prices(price_ref = 0,CH4_factor = 1)
 start_run(cfg,codeCheck=FALSE)
 
@@ -81,9 +78,36 @@ cfg$gms$c15_livescen_target <- "constant"
 pollutant_prices(price_ref = 150,CH4_factor = 1)
 start_run(cfg,codeCheck=FALSE)
 
-cfg$title <- paste(prefix,"SSP2_RCPREF_C0150_GWPSTAR_REFDIET",sep = "_")
+cfg$title <- paste(prefix,"SSP2_RCPREF_CP0150STARF_REFDIET",sep = "_")
 cfg$gms$c15_livescen_target <- "constant"
 pollutant_prices(price_ref = 150,CH4_factor = 0.25)
+start_run(cfg,codeCheck=FALSE)
+
+cfg$title <- paste(prefix,"SSP2_RCPREF_CP0150STARM_REFDIET",sep = "_")
+cfg$gms$c15_livescen_target <- "constant"
+pollutant_prices(price_ref = 150,CH4_factor = 4)
+start_run(cfg,codeCheck=FALSE)
+
+
+cfg$title <- paste(prefix,"SSP2_RCPREF_C0500_REFDIET",sep = "_")
+cfg$gms$c15_livescen_target <- "constant"
+pollutant_prices(price_ref = 500,CH4_factor = 1)
+start_run(cfg,codeCheck=FALSE)
+
+cfg$title <- paste(prefix,"SSP2_RCPREF_CP0500STARF_REFDIET",sep = "_")
+cfg$gms$c15_livescen_target <- "constant"
+pollutant_prices(price_ref = 500,CH4_factor = 0.25)
+start_run(cfg,codeCheck=FALSE)
+
+cfg$title <- paste(prefix,"SSP2_RCPREF_CP0500STARM_REFDIET",sep = "_")
+cfg$gms$c15_livescen_target <- "constant"
+pollutant_prices(price_ref = 500,CH4_factor = 4)
+start_run(cfg,codeCheck=FALSE)
+
+
+cfg$title <- paste(prefix,"SSP2_RCPREF_C0000_LSPCUT",sep = "_")
+cfg$gms$c15_livescen_target <- "lin_zero_20_70"
+pollutant_prices(price_ref = 0,CH4_factor = 1)
 start_run(cfg,codeCheck=FALSE)
 
 cfg$title <- paste(prefix,"SSP2_RCPREF_C0150_LSPCUT",sep = "_")
@@ -91,8 +115,27 @@ cfg$gms$c15_livescen_target <- "lin_zero_20_70"
 pollutant_prices(price_ref = 150,CH4_factor = 1)
 start_run(cfg,codeCheck=FALSE)
 
-cfg$title <- paste(prefix,"SSP2_RCPREF_C0150_GWPSTAR_LSPCUT",sep = "_")
+cfg$title <- paste(prefix,"SSP2_RCPREF_CP0150STARF_LSPCUT",sep = "_")
 cfg$gms$c15_livescen_target <- "lin_zero_20_70"
 pollutant_prices(price_ref = 150,CH4_factor = 0.25)
 start_run(cfg,codeCheck=FALSE)
 
+cfg$title <- paste(prefix,"SSP2_RCPREF_CP0150STARM_LSPCUT",sep = "_")
+cfg$gms$c15_livescen_target <- "lin_zero_20_70"
+pollutant_prices(price_ref = 150,CH4_factor = 4)
+start_run(cfg,codeCheck=FALSE)
+
+cfg$title <- paste(prefix,"SSP2_RCPREF_C0500_LSPCUT",sep = "_")
+cfg$gms$c15_livescen_target <- "lin_zero_20_70"
+pollutant_prices(price_ref = 500,CH4_factor = 1)
+start_run(cfg,codeCheck=FALSE)
+
+cfg$title <- paste(prefix,"SSP2_RCPREF_CP0500STARF_LSPCUT",sep = "_")
+cfg$gms$c15_livescen_target <- "lin_zero_20_70"
+pollutant_prices(price_ref = 500,CH4_factor = 0.25)
+start_run(cfg,codeCheck=FALSE)
+
+cfg$title <- paste(prefix,"SSP2_RCPREF_CP0500STARM_LSPCUT",sep = "_")
+cfg$gms$c15_livescen_target <- "lin_zero_20_70"
+pollutant_prices(price_ref = 500,CH4_factor = 4)
+start_run(cfg,codeCheck=FALSE)
