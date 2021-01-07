2de19ce80ff174a06c57cda85bc5bd94180fe2ab
abhimishr <mishra.abhijeet28@gmail.com>
2021-01-07 18:20:30 +0100

    msg update

--- scripts/start/projects/forestry.R
+++ scripts/start/projects/forestry.R
@@ -25,7 +25,7 @@ dir.create(log_folder,showWarnings = FALSE)
 
 identifier_flag = "DEC45a"
 
-cat(paste0("Estb plantations for full demand"), file=paste0(log_folder,"/",identifier_flag,".txt"),append=F)
+cat(paste0("Estb according to abare data"), file=paste0(log_folder,"/",identifier_flag,".txt"),append=F)
 
 xx <- c()
 
