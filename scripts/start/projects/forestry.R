29b67dc74a0303832ae9296b0d8ebceafc5c79d2
abhimishr <mishra.abhijeet28@gmail.com>
2021-01-11 18:40:15 +0100

    coup2100 runs

--- scripts/start/projects/forestry.R
+++ scripts/start/projects/forestry.R
@@ -23,7 +23,7 @@ source("scripts/start_functions.R")
 log_folder = "run_details"
 dir.create(log_folder,showWarnings = FALSE)
 
-identifier_flag = "DEC47"
+identifier_flag = "DEC48"
 
 cat(paste0("Turn off optfile"), file=paste0(log_folder,"/",identifier_flag,".txt"),append=F)
 
@@ -34,7 +34,7 @@ for(ssp in c("SSP2")){
   for(s35_secdf_distribution in c(2)){
 
     for(s32_distribution_type in c(2)){
-      for(scen in c("forestry","NPI")){
+      for(scen in c("forestry","nocc")){
 
         for(s35_forest_damage in c(2)){
           source("config/default.cfg")
@@ -47,7 +47,7 @@ for(ssp in c("SSP2")){
 
           cfg = setScenario(cfg,c(ssp,scen))
 
-          cfg$gms$s80_optfile = 0
+          cfg$gms$c_timesteps <- "coup2100"
 
           cfg$gms$s32_distribution_type = s32_distribution_type
           cfg$gms$s35_forest_damage = s35_forest_damage
@@ -76,14 +76,14 @@ for(ssp in c("SSP2")){
           if(cfg$gms$s35_forest_damage == 1) damage_flg = "Wildfire"
           if(cfg$gms$s35_forest_damage == 2) damage_flg = "Combined"
 
-          if(scen=="NPI") scen_flag="MAgPIE"
-          if(scen=="forestry") scen_flag="MAgPIE Forestry"
+          if(scen=="nocc") scen_flag="Default"
+          if(scen=="forestry") scen_flag="Forestry"
 
           cfg$title   = paste0(identifier_flag,"_",scen_flag,"_",plant_area_flag)
           cfg$output  = c("extra/timestep_duration")
 
            xx = c(xx,cfg$title)
-           start_run(cfg,codeCheck=FALSE)
+           #start_run(cfg,codeCheck=FALSE)
         }
       }
     }
@@ -104,7 +104,6 @@ for(ssp in c("SSP2")){
         for(s35_forest_damage in c(2)){
           source("config/default.cfg")
 
-          cfg$gms$s80_optfile = 1
           cfg$gms$s80_maxiter = 5
 
           cfg$results_folder = "output/:title:"
@@ -113,13 +112,13 @@ for(ssp in c("SSP2")){
 
           cfg = setScenario(cfg,c(ssp,scen))
 
-          cfg$gms$s32_fix_plant <- 1
+          cfg$gms$c_timesteps <- "coup2100"
 
           cfg$gms$s32_distribution_type = s32_distribution_type
           cfg$gms$s35_forest_damage = s35_forest_damage
           cfg$gms$s35_secdf_distribution = s35_secdf_distribution
 
-          if(cfg$gms$s32_fix_plant == 1 && cfg$gms$s73_foresight == 1) break
+          cfg$gms$s32_fix_plant <- 1
 
           if(cfg$gms$s73_foresight == 1) foresight_flag = "Forward"
           if(cfg$gms$s73_foresight != 1) foresight_flag = "Myopic"
@@ -142,14 +141,14 @@ for(ssp in c("SSP2")){
           if(cfg$gms$s35_forest_damage == 1) damage_flg = "Wildfire"
           if(cfg$gms$s35_forest_damage == 2) damage_flg = "Combined"
 
-          if(scen=="NPI") scen_flag="MAgPIE"
-          if(scen=="forestry") scen_flag="MAgPIE Forestry"
+          if(scen=="nocc") scen_flag="Default"
+          if(scen=="forestry") scen_flag="Forestry"
 
           cfg$title   = paste0(identifier_flag,"_",scen_flag,"_",plant_area_flag)
           cfg$output  = c("extra/timestep_duration")
 
            xx = c(xx,cfg$title)
-           start_run(cfg,codeCheck=FALSE)
+           #start_run(cfg,codeCheck=FALSE)
         }
       }
     }
