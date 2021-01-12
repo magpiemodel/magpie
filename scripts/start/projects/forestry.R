237acc489655f7bed53b13296bc3472f0fcc96c1
abhimishr <mishra.abhijeet28@gmail.com>
2021-01-12 21:55:28 +0100

    Revert bugfix in forest protetcion according to initial distribution. Coup2100 runs

--- scripts/start/projects/forestry.R
+++ scripts/start/projects/forestry.R
@@ -23,9 +23,9 @@ source("scripts/start_functions.R")
 log_folder = "run_details"
 dir.create(log_folder,showWarnings = FALSE)
 
-identifier_flag = "DEC50"
+identifier_flag = "DEC51"
 
-cat(paste0("Revert bugfix in forest protetcion according to initial distribution"), file=paste0(log_folder,"/",identifier_flag,".txt"),append=F)
+cat(paste0("Revert bugfix in forest protetcion according to initial distribution. Coup2100 runs"), file=paste0(log_folder,"/",identifier_flag,".txt"),append=F)
 
 xx <- c()
 for(scen in c("forestry","nocc")){
