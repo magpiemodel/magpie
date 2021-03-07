01451cae7f5bd184860cdcc4493a786a206749d0
florianh <humpenoeder@pik-potsdam.de>
2021-03-07 17:28:45 +0100

    update

--- scripts/start/projects/paper_scp.R
+++ scripts/start/projects/paper_scp.R
@@ -30,22 +30,44 @@ cfg$output <- c("rds_report")
 
 cfg <- setScenario(cfg,c("SSP2","NPI"))
 
-prefix <- "SCP29"
+prefix <- "SCP30"
+cfg$gms$s80_optfile <- 1
+cfg$gms$s80_maxiter <- 5
 
 cfg$gms$s15_elastic_demand <- 0
 
 cfg$gms$c56_pollutant_prices <- "R2M41-SSP2-NPi"
 cfg$gms$c60_2ndgen_biodem <- "R2M41-SSP2-NPi"
 
-for (scp_level in c(0,20,50,80)) {
-  for (scp_type in c("mixed", "methane", "sugar", "cellulose","hydrogen")) {
-    cfg$title <- paste(prefix,paste0("SSP2-MP",scp_level),scp_type,sep="_")
-    cfg$gms$c20_scp_type <- scp_type
-    if (scp_level == 0) scp_scen <- "constant" else if (scp_level == 20) scp_scen <- "sigmoid_80pc_20_50" else if (scp_level == 50) scp_scen <- "sigmoid_50pc_20_50" else if (scp_level == 80) scp_scen <- "sigmoid_20pc_20_50"
-    cfg$gms$c15_rumdairy_scp_scen <- scp_scen
-    start_run(cfg,codeCheck=FALSE)
+for (pol in c("Ref","1p5deg")) {
+  for (scp_level in c(0,20,50,80)) {
+    for (scp_type in c("mixed")) {
+      if (pol == "Ref") {
+        cfg$gms$c56_pollutant_prices <- "R2M41-SSP2-NPi"
+        cfg$gms$c60_2ndgen_biodem <- "R2M41-SSP2-NPi"
+      } else if (pol == "1p5deg") {
+        cfg$gms$c56_pollutant_prices <- "R2M41-SSP2-Budg600"
+        cfg$gms$c60_2ndgen_biodem <- "R2M41-SSP2-Budg600"
+      }
+      cfg$title <- paste(prefix,paste0("SSP2-",pol,"-BioTech",scp_level),scp_type,sep="_")
+      cfg$gms$c20_scp_type <- scp_type
+      if (scp_level == 0) scp_scen <- "constant" else if (scp_level == 20) scp_scen <- "sigmoid_80pc_20_50" else if (scp_level == 50) scp_scen <- "sigmoid_50pc_20_50" else if (scp_level == 80) scp_scen <- "sigmoid_20pc_20_50"
+      cfg$gms$c15_rumdairy_scp_scen <- scp_scen
+      start_run(cfg,codeCheck=FALSE)
+    }
   }
 }
+
+
+# for (scp_level in c(0,20,50,80)) {
+#   for (scp_type in c("mixed", "methane", "sugar", "cellulose","hydrogen")) {
+#     cfg$title <- paste(prefix,paste0("SSP2-BioTech",scp_level),scp_type,sep="_")
+#     cfg$gms$c20_scp_type <- scp_type
+#     if (scp_level == 0) scp_scen <- "constant" else if (scp_level == 20) scp_scen <- "sigmoid_80pc_20_50" else if (scp_level == 50) scp_scen <- "sigmoid_50pc_20_50" else if (scp_level == 80) scp_scen <- "sigmoid_20pc_20_50"
+#     cfg$gms$c15_rumdairy_scp_scen <- scp_scen
+#     start_run(cfg,codeCheck=FALSE)
+#   }
+# }
 
 # ### Ref
 # cfg$gms$c56_pollutant_prices <- "R2M41-SSP2-NPi"
