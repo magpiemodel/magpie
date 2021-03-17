c05aaedc4b64ac7fc8d8a0d45e41b05b9f1d48da
abhimishr <mishra.abhijeet28@gmail.com>
2021-03-18 00:05:49 +0100

    typo fix

--- scripts/start/projects/forestry.R
+++ scripts/start/projects/forestry.R
@@ -61,7 +61,7 @@ for(c73_wood_scen in c("default")){
             xx = c(xx,cfg$title)
             cfg$gms$s80_optfile <- 1
             cfg$results_folder = "output/:title:"
-#            start_run(cfg,codeCheck=FALSE)
+            start_run(cfg,codeCheck=FALSE)
           }
         }
      }
