c2556f03c3377f398391986f4c129de702e304ee
emolinab <mbacca@pik-potsdam.de>
2021-05-07 14:22:14 +0200

    no gygma

--- scripts/start/extra/recalibrate_inputs.R
+++ scripts/start/extra/recalibrate_inputs.R
@@ -25,14 +25,14 @@ source("config/default.cfg")
 realization<-c("mixed_feb17")
 sticky_modes<-c("")
 
-combo<-c(#"rcp7p0_EPIC_GFDL","rcp7p0_CYGMA_GFDL",
-        "rcp8p5_CYGMA_GFDL","rcp8p5_pDSSAT_GFDL","rcp8p5_EPIC_GFDL",
+combo<-c(#"rcp7p0_EPIC_GFDL","rcp7p0_CYGMA_GFDL","rcp8p5_CYGMA_GFDL",
+        "rcp8p5_pDSSAT_GFDL","rcp8p5_EPIC_GFDL",
         "rcp8p5_EPIC_UKESM","rcp8p5_CYGMA_UKESM","rcp8p5_pDSSAT_UKESM",
         "rcp7p0_EPIC_UKESM","rcp7p0_CYGMA_UKESM"
         )
 
-hashes_combos<-c(#"669b91c3","c6f10324",
-                 "d972a1ce","5b2b868c","82675b72",
+hashes_combos<-c(#"669b91c3","c6f10324","d972a1ce",
+                 "5b2b868c","82675b72",
                  "c0547439","e61ed473","256c3ab7",
                  "6bd5239a","41ad9618")
 
