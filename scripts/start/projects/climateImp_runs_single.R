efedc5ec4a75b080766524128868a30c210388b7
emolinab <mbacca@pik-potsdam.de>
2021-05-10 16:53:43 +0200

    climate missing

--- scripts/start/projects/climateImp_runs_single.R
+++ scripts/start/projects/climateImp_runs_single.R
@@ -26,7 +26,7 @@ realization<-c("sticky_feb18","mixed_feb17")
 #         #"rcp8p5_EPIC_UKESM",
 #         #"rcp7p0_EPIC_UKESM",
 #         "rcp7p0_EPIC_GFDL")
-# 
+#
 # hashes_combos<-c(#"c6f10324","d972a1ce",
 #                 #"5b2b868c","e61ed473","256c3ab7",
 #                 #"41ad9618",
@@ -34,12 +34,12 @@ realization<-c("sticky_feb18","mixed_feb17")
 #         #         "c0547439",
 #         #         "6bd5239a",
 #                  "669b91c3")
-# 
+#
 # climate<-c("cc","nocc")
 # input<-c("additional_data_rev4.02.tgz",
 #          "rev4.59SmashingPumpkins+StickyFiles_h12_magpie_debug.tgz",
 #          "rev4.59SmashingPumpkins+ISIMIPyields_h12_validation_debug.tgz")
-# 
+#
 # calib<-list()
 # calib[["rcp7p0_CYGMA_GFDL"]][["mixed_feb17"]]<-"calibration_H12_rcp7p0_CYGMA_GFDL_mixed_feb17__08May21.tgz"
 # calib[["rcp8p5_CYGMA_GFDL"]][["mixed_feb17"]]<-"calibration_H12_rcp8p5_CYGMA_GFDL_mixed_feb17__08May21.tgz"
@@ -51,7 +51,7 @@ realization<-c("sticky_feb18","mixed_feb17")
 # calib[["rcp8p5_EPIC_UKESM"]][["mixed_feb17"]]<-"calibration_H12_rcp8p5_EPIC_UKESM_mixed_feb17__07May21.tgz"
 # calib[["rcp7p0_EPIC_UKESM"]][["mixed_feb17"]]<-"calibration_H12_rcp7p0_EPIC_UKESM_mixed_feb17__07May21.tgz"
 # calib[["rcp7p0_EPIC_GFDL"]][["mixed_feb17"]]<-"calibration_H12_rcp7p0_EPIC_GFDL_mixed_feb17__07May21.tgz"
-# 
+#
 # calib[["rcp7p0_CYGMA_GFDL"]][["sticky_feb18"]]<-"calibration_H12_rcp7p0_CYGMA_GFDL_sticky_feb18_dynamic_08May21.tgz"
 # calib[["rcp8p5_CYGMA_GFDL"]][["sticky_feb18"]]<-"calibration_H12_rcp8p5_CYGMA_GFDL_sticky_feb18_dynamic_08May21.tgz"
 # calib[["rcp8p5_pDSSAT_GFDL"]][["sticky_feb18"]]<-"calibration_H12_rcp8p5_pDSSAT_GFDL_sticky_feb18_dynamic_09May21.tgz"
@@ -62,9 +62,9 @@ realization<-c("sticky_feb18","mixed_feb17")
 # calib[["rcp8p5_EPIC_UKESM"]][["sticky_feb18"]]<-"calibration_H12_rcp8p5_EPIC_UKESM_sticky_feb18_dynamic_07May21.tgz"
 # calib[["rcp7p0_EPIC_UKESM"]][["sticky_feb18"]]<-"calibration_H12_rcp7p0_EPIC_UKESM_sticky_feb18_dynamic_07May21.tgz"
 # calib[["rcp7p0_EPIC_GFDL"]][["sticky_feb18"]]<-"calibration_H12_rcp7p0_EPIC_GFDL_sticky_feb18_dynamic_07May21.tgz"
-# 
+#
 # aux<-1
-# 
+#
 # ### Normal
 # for (i in realization){
 #   for (com in combo){
@@ -75,40 +75,40 @@ realization<-c("sticky_feb18","mixed_feb17")
 #         climate<-c("cc","nocc")
 #       }
 #               for (c in climate){
-# 
-# 
+#
+#
 #           cfg<-gms::setScenario(cfg,c)
 #           #configurations
 #           cfg$title<-paste0("NB_F_",com,"_",i,"_",so,"_",c,"_")
 #           cfg$recalibrate <- FALSE
-# 
+#
 #           cfg$input <- c(input,
 #                          paste0("rev4.59SmashingPumpkins+ISIMIPyields_h12_",hashes_combos[aux],"_cellularmagpie_debug.tgz"),
 #                          calib[[com]][[i]])
 #           cfg$output <- c("rds_report")
-# 
-# 
+#
+#
 #           #Special modules
 #           cfg$gms$factor_costs <- i
 #           if(i == "sticky_feb18"){
 #           cfg$gms$c38_sticky_mode  <- so
 #            }
 #           cfg$gms$yields  <- "managementcalib_aug19"
-# 
+#
 #          start_run(cfg,codeCheck=FALSE)
-# 
+#
 #          aux<-aux+1
 #        }
 #      }
 #    }
 # }
-# 
+#
  combo<-c("rcp7p0_LPJML_GFDL_newParam")
-# climate<-c("cc","nocc")
-# 
+ climate<-c("cc","nocc")
+#
 # #hashes_combos<-c("")
-# 
-# 
+#
+#
  input<-c("additional_data_rev4.02.tgz",
           "rev4.59SmashingPumpkins+StickyFiles_h12_magpie_debug.tgz",
           "rev4.59SmashingPumpkins+ISIMIPyields_h12_validation_debug.tgz")
