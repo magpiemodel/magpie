d4417cd2f110cd693dcc677493f2242bd199b37a
emolinab <mbacca@pik-potsdam.de>
2021-05-14 11:10:26 +0200

    script to test best calib switch

---
+++ scripts/start/extra/recalibrate_test_best_calib.R
@@ -0,0 +1,33 @@
+# |  (C) 2008-2021 Potsdam Institute for Climate Impact Research (PIK)
+# |  authors, and contributors see CITATION.cff file. This file is part
+# |  of MAgPIE and licensed under AGPL-3.0-or-later. Under Section 7 of
+# |  AGPL-3.0, you are granted additional permissions described in the
+# |  MAgPIE License Exception, version 1.0 (see LICENSE file).
+# |  Contact: magpie@pik-potsdam.de
+
+# --------------------------------------------------------
+# description: calculate and store new calibration factors
+# --------------------------------------------------------
+
+library(magpie4)
+library(magclass)
+
+# Load start_run(cfg) function which is needed to start MAgPIE runs
+source("scripts/start_functions.R")
+
+#start MAgPIE run
+source("config/default.cfg")
+
+for (best in c(TRUE,FALSE)){
+
+cfg$results_folder <- "output/:title:"
+cfg$recalibrate <- TRUE
+cfg$title <- "calib_run"
+cfg$gms$c_timesteps <- 1
+cfg$output <- c("rds_report")
+cfg$sequential <- TRUE
+cfg$best_calib <- best
+
+start_run(cfg,codeCheck=FALSE)
+magpie4::submitCalibration("H12_best_calib_",best)
+}
