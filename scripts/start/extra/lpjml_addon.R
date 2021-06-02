9be8d92f5415ebd275b005573cc2ea108fc49e75
Kristine Karstens <karstens@pik-potsdam.de>
2021-06-02 08:26:33 +0200

    update start script add on

--- scripts/start/extra/lpjml_addon.R
+++ scripts/start/extra/lpjml_addon.R
@@ -13,8 +13,6 @@ library(gms)
 source("scripts/start_functions.R")
 source("config/default.cfg")
 
-
-
 cfg$input <- c(cfg$input[grep("additional_data", cfg$input)],
                "rev4.59_h12_magpie.tgz",
                "rev4.59_h12_c5cdbf33_cellularmagpie_c200_GFDL-ESM4-ssp370_lpjml-47a77da3.tgz",
@@ -23,13 +21,18 @@ cfg$input <- c(cfg$input[grep("additional_data", cfg$input)],
 # see /p/projects/magpie/data/input/calibration for more available calibration factors
 
 cfg$title                            <- "default_lpjml5"
+
+# in case of recalibration, following settings should be applied
 cfg$crop_calib_max                   <- 1.5
 cfg$best_calib                       <- TRUE
+
+# preliminary test settings for new default 
+# (including new yield, crop, factor cost realizations)
 cfg$gms$yields                       <- "managementcalib_aug19"
 cfg$gms$s14_yld_past_switch          <- 0.25
 cfg$gms$processing                   <- "substitution_may21"
 cfg$gms$crop                         <- "endo_apr21"
-cfg$gms$factor_cost                  <- "sticky_feb18"
+cfg$gms$factor_costs                 <- "sticky_feb18"
 cfg$gms$c41_initial_irrigation_area  <- "LUH2v2"
 cfg                                  <- setScenario(cfg,"cc")
 
