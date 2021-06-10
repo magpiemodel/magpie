ee6706300c5fab7be9852a07147070b1e3201662
Jan Philipp Dietrich <dietrich@pik-potsdam.de>
2021-06-10 16:13:52 +0200

    updated test runs and added automated process to extract version number from CITATION.cff and store it in cfg

--- scripts/start/test_runs.R
+++ scripts/start/test_runs.R
@@ -6,13 +6,9 @@
 # |  Contact: magpie@pik-potsdam.de
 
 # ----------------------------------------------------------
-# description: test routine to run for new pull requests
+# description: Test rountine for standardized test runs
 # position: 5
 # ----------------------------------------------------------
-
-
-##### Version log (YYYYMMDD - Description - Author(s))
-## 20200527 - Default SSP2 Baseline and Policy runs - FH,AM,EMJB,JPD
 
 ## Load lucode2 and gms to use setScenario later
 library(lucode2)
@@ -24,36 +20,34 @@ source("scripts/start_functions.R")
 # Source default cfg. This loads the object "cfg" in R environment
 source("config/default.cfg")
 
-# choose a meaningful Pull Request (PR) flag. 
-# example for high risk modification requiring runs from the current develop and the feature branch to be merged: PR276_develop, PR276_TAUhistfree
-pr_flag <- "PRXXX"
+# create additional information to describe the runs
+cfg$info$flag <- "TEST" # choose a meaningful flag.
+cfg$info$user <- Sys.info()[["user"]] # Grab user name
 
-# Grab user name
-user <- Sys.info()[["user"]]
+cfg$output <- c("rds_report") # Only run rds_report after model run
 
-cfg$results_folder <- "output/:title:"
+# support function to create standardized title
+.title <- function(..., cfg=cfg) return(paste(...,cfg$info$flag, sep="_"))
 
-## Create a set of runs based on default.cfg
+# start a run with default settings
+cfg$title <- .title("default")
+start_run(cfg, codeCheck = TRUE)
 
-for(ssp in c("SSP1","SSP2")) { ## Add SSP* here for testing other SSPs. Basic test should be for at least two SSPs to check if results until 2020 are identical
-    for (climpol in c("Ref","1p5deg")) {
-      if (climpol == "Ref") {
-        cfg <- setScenario(cfg,c(ssp,"NPI"))
-        cfg$gms$c56_pollutant_prices <- "R2M41-SSP2-NPi" #update to most recent coupled runs asap
-        cfg$gms$c60_2ndgen_biodem <- "R2M41-SSP2-NPi" ##update to most recent coupled runs asap
-        
-      } else if (climpol == "1p5deg"){
-        cfg <- setScenario(cfg,c(ssp,"NDC"))
-        cfg$gms$c56_pollutant_prices <- "R2M41-SSP2-Budg600" #update to most recent coupled runs asap
-        cfg$gms$c60_2ndgen_biodem <- "R2M41-SSP2-Budg600" ##update to most recent coupled runs asap
-      }
-      
-#      cfg$title <- paste0(pr_flag,"_",user,"_",ssp,"-",climpol) #Create easily distinguishable run title
-      cfg$title <- paste0(pr_flag,"_",ssp,"-",climpol) #Create easily distinguishable run title
-      
-      cfg$output <- c("rds_report") # Only run rds_report after model run
-      
-      start_run(cfg,codeCheck=TRUE) # Start MAgPIE run
-      #cat(cfg$title)
-    }
+# create a set of runs based on default.cfg
+for(ssp in c("SSP1","SSP2")) { # Add SSP* here for testing other SSPs.
+                               # Basic test should be for at least two SSPs to
+                               #check if results until 2020 are identical
+
+  cfg$title <- .title("ref", ssp)
+  cfg <- setScenario(cfg,c(ssp,"NPI"))
+  cfg$gms$c56_pollutant_prices <- "R2M41-SSP2-NPi"
+  cfg$gms$c60_2ndgen_biodem    <- "R2M41-SSP2-NPi"
+  start_run(cfg, codeCheck = FALSE)
+
+  cfg$title <- .title("1p5deg", ssp)
+  cfg <- setScenario(cfg,c(ssp,"NDC"))
+  cfg$gms$c56_pollutant_prices <- "R2M41-SSP2-Budg600"
+  cfg$gms$c60_2ndgen_biodem    <- "R2M41-SSP2-Budg600"
+  start_run(cfg, codeCheck = FALSE)
+
 }
