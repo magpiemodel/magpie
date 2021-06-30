46504daa9652e038d55bbf1d52adc7d9877ed080
florianh <humpenoeder@pik-potsdam.de>
2021-06-30 23:30:26 +0200

    update

--- scripts/start/test_runs_rc.R
+++ scripts/start/test_runs_rc.R
@@ -17,7 +17,7 @@ library(gms)
 # Load start_run(cfg) function which is needed to start MAgPIE runs
 source("scripts/start_functions.R")
 
-prefix <- "RC01"
+prefix <- "RC02"
 
 for (version in c("old","new")) {
   if (version == "old") {
@@ -30,12 +30,14 @@ for (version in c("old","new")) {
     cfg <- setScenario(cfg,c("SSP2","NPI"))
     cfg$gms$c56_pollutant_prices <- "R2M41-SSP2-NPi"
     cfg$gms$c60_2ndgen_biodem    <- "R2M41-SSP2-NPi"
+    print(cfg$title)
     start_run(cfg,codeCheck=FALSE)
     
     cfg$title <- paste(prefix,"SSP2-1p5deg",version,"mixed",sep="_")
     cfg <- setScenario(cfg,c("SSP2","NDC"))
     cfg$gms$c56_pollutant_prices <- "R2M41-SSP2-Budg600"
     cfg$gms$c60_2ndgen_biodem    <- "R2M41-SSP2-Budg600"
+    print(cfg$title)
     start_run(cfg,codeCheck=FALSE)
     
   } else if (version == "new") {
@@ -44,30 +46,31 @@ for (version in c("old","new")) {
     cfg$results_folder <- "output/:title:"
     cfg$output <- c("rds_report")
     cfg$gms$s80_optfile <- 1
-  }
-  
-  for (cost in c("mixed","sticky","fixed")) {
-    if (cost == "mixed") {
-      cfg$gms$factor_costs <- "mixed_feb17"
-    } else if (cost == "sticky") {
-      cfg$gms$factor_costs <- "sticky_feb18"
-      cfg$gms$c38_sticky_mode <- "dynamic"
-    } else if (cost == "fixed") {
-      cfg$gms$factor_costs <- "sticky_feb18"
-      cfg$gms$c38_sticky_mode <- "free"
+    for (cost in c("mixed","sticky","fixed")) {
+      if (cost == "mixed") {
+        cfg$gms$factor_costs <- "mixed_feb17"
+      } else if (cost == "sticky") {
+        cfg$gms$factor_costs <- "sticky_feb18"
+        cfg$gms$c38_sticky_mode <- "dynamic"
+      } else if (cost == "fixed") {
+        cfg$gms$factor_costs <- "sticky_feb18"
+        cfg$gms$c38_sticky_mode <- "free"
+      }
+      
+      cfg$title <- paste(prefix,"SSP2-NPI",version,cost,sep="_")
+      cfg <- setScenario(cfg,c("SSP2","NPI","rcp7p0"))
+      cfg$gms$c56_pollutant_prices <- "R2M41-SSP2-NPi"
+      cfg$gms$c60_2ndgen_biodem    <- "R2M41-SSP2-NPi"
+      print(cfg$title)
+      start_run(cfg,codeCheck=FALSE)
+      
+      cfg$title <- paste(prefix,"SSP2-1p5deg",version,cost,sep="_")
+      cfg <- setScenario(cfg,c("SSP2","NPI","rcp1p9"))
+      cfg$gms$c56_pollutant_prices <- "R2M41-SSP2-Budg600"
+      cfg$gms$c60_2ndgen_biodem    <- "R2M41-SSP2-Budg600"
+      print(cfg$title)
+      start_run(cfg,codeCheck=FALSE)
+      
     }
-    
-    cfg$title <- paste(prefix,"SSP2-NPI",version,cost,sep="_")
-    cfg <- setScenario(cfg,c("SSP2","NPI","rcp7p0"))
-    cfg$gms$c56_pollutant_prices <- "R2M41-SSP2-NPi"
-    cfg$gms$c60_2ndgen_biodem    <- "R2M41-SSP2-NPi"
-    start_run(cfg,codeCheck=FALSE)
-    
-    cfg$title <- paste(prefix,"SSP2-1p5deg",version,cost,sep="_")
-    cfg <- setScenario(cfg,c("SSP2","NPI","rcp1p9"))
-    cfg$gms$c56_pollutant_prices <- "R2M41-SSP2-Budg600"
-    cfg$gms$c60_2ndgen_biodem    <- "R2M41-SSP2-Budg600"
-    start_run(cfg,codeCheck=FALSE)
-    
   }
 }
