a20d71456d3a3776b9443610d5aae4e063413dc3
florianh <humpenoeder@pik-potsdam.de>
2021-10-29 17:50:25 +0200

    update

--- scripts/start/projects/project_LAMACLIMA_WP4.R
+++ scripts/start/projects/project_LAMACLIMA_WP4.R
@@ -24,7 +24,7 @@ source("scripts/start_functions.R")
 #start MAgPIE run
 source("config/default.cfg")
 
-prefix <- "LAMA78"
+prefix <- "LAMA79"
 cfg$force_replace <- TRUE
 
 cfg$gms$factor_costs <- "sticky_labor"
