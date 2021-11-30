efbb81312b514c77d27c56d76ced91b52b555dc8
florianh <humpenoeder@pik-potsdam.de>
2021-11-30 18:18:35 +0100

    update start script

--- scripts/start/projects/project_LAMACLIMA_WP4.R
+++ scripts/start/projects/project_LAMACLIMA_WP4.R
@@ -29,7 +29,7 @@ cfg$force_replace <- TRUE
 
 cfg$gms$factor_costs <- "sticky_labor"
 cfg$gms$c38_sticky_mode <- "dynamic"
-cfg$input["calibration"] <- "calibration_H12_sticky_feb18_dynamic_13Nov21.tgz"
+cfg$input["calibration"] <- "calibration_H12_sticky_feb18_dynamic_30Nov21.tgz"
 cfg$gms$c17_prod_init <- "off"
 
 cfg$gms$labor_prod <- "exo"
