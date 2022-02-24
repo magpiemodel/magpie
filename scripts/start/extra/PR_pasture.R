61172fea8f108995f01ae0c17e4be6e4c319c1bf
Marcos Alves <mppalves@gmail.com>
2022-02-24 09:58:25 +0100

    start script test

---
+++ scripts/start/extra/PR_pasture.R
@@ -0,0 +1,26 @@
+# |  (C) 2008-2021 Potsdam Institute for Climate Impact Research (PIK)
+# |  authors, and contributors see CITATION.cff file. This file is part
+# |  of MAgPIE and licensed under AGPL-3.0-or-later. Under Section 7 of
+# |  AGPL-3.0, you are granted additional permissions described in the
+# |  MAgPIE License Exception, version 1.0 (see LICENSE file).
+# |  Contact: magpie@pik-potsdam.de
+
+
+# ----------------------------------------------------------
+# description: creating runs to test the PR for pasture management
+# ----------------------------------------------------------
+
+# Load start_run(cfg) function which is needed to start MAgPIE runs
+source("scripts/start_functions.R")
+source("config/default.cfg")
+#start MAgPIE run
+tc13 <- c("endo_jan22", "exo")
+past31 <- c("grasslands_feb22", "endo_jun13", "static")
+for (i in tc13) {
+  cfg$gms$tc <- i
+  for (j in past31) {
+    cfg$gms$past <- j
+    cfg$title <- paste0(i,"_",j)
+    start_run(cfg=cfg)
+  }
+}
