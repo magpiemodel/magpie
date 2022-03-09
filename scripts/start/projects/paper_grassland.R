e734e4fa558db02b8399290d9e7f36f1998ca03f
Marcos Alves <mppalves@gmail.com>
2022-03-09 17:21:09 +0100

    starting script test

--- scripts/start/projects/paper_grassland.R
+++ scripts/start/projects/paper_grassland.R
@@ -6,22 +6,31 @@
 # |  Contact: magpie@pik-potsdam.de
 
 # ------------------------------------------------
-# description:Runs for grassland management paper. Should be replaced by updating
-# setScenario.csv with paramerts for cfg$gms$c31_past_suit_scen in the future.
+# description:
 # ------------------------------------------------
 library(gms)
 # Load start_run(cfg) function which is needed to start MAgPIE runs
 source("scripts/start_functions.R")
 
 #start MAgPIE run
-for(ssp_setting in c("SSP1","SSP2","SSP3","SSP4","SSP5")){
+
+for(reali in c("grasslands_mar22", "grasslands_feb22")){
   cfg="default.cfg"
-  cfg <- setScenario(cfg,ssp_setting)
-  cfg$title <- paste0("IPSL-CM6A-LR-",ssp_setting,"-set1-", substr(Sys.time(), 6,10),"-",gsub(":", "_", substr(Sys.time(), 12,16)))
-  if(ssp_setting == "SSP1") {cfg$gms$c31_past_suit_scen  <- "ssp126"}
-  if(ssp_setting == "SSP2") {cfg$gms$c31_past_suit_scen  <- "ssp126"}
-  if(ssp_setting == "SSP3") {cfg$gms$c31_past_suit_scen  <- "ssp370"}
-  if(ssp_setting == "SSP4") {cfg$gms$c31_past_suit_scen  <- "ssp585"}
-  if(ssp_setting == "SSP5") {cfg$gms$c31_past_suit_scen  <- "ssp585"}
-  start_run(cfg)
+  cfg$recalibrate <- TRUE
+  cfg$recalibrate_landconversion_cost <- TRUE
+  cfg$gms$past <- reali
+  for(ssp_setting in c("SSP1","SSP2","SSP3","SSP4","SSP5")){
+    if(ssp_setting != "SSP1") {
+      cfg$recalibrate <- FALSE
+      cfg$recalibrate_landconversion_cost <- FALSE
+    }
+    cfg <- setScenario(cfg,ssp_setting)
+    cfg$title <- paste0("PR-",ssp_setting,"_",reali, substr(Sys.time(), 6,10),"-",gsub(":", "_", substr(Sys.time(), 12,16)))
+    if(ssp_setting == "SSP1") {cfg$gms$c31_past_suit_scen  <- "ssp126"}
+    if(ssp_setting == "SSP2") {cfg$gms$c31_past_suit_scen  <- "ssp126"}
+    if(ssp_setting == "SSP3") {cfg$gms$c31_past_suit_scen  <- "ssp370"}
+    if(ssp_setting == "SSP4") {cfg$gms$c31_past_suit_scen  <- "ssp585"}
+    if(ssp_setting == "SSP5") {cfg$gms$c31_past_suit_scen  <- "ssp585"}
+    start_run(cfg)
+  }
 }
