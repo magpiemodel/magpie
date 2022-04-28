84ea476908deeb60a039288b5c9c18eb707a0f36
Felicitas <beier@pik-potsdam.de>
2022-04-28 14:13:05 +0200

    added climate scenario input cfg

--- scripts/start/projects/project_FSEC_Scenarios.R
+++ scripts/start/projects/project_FSEC_Scenarios.R
@@ -36,7 +36,7 @@ general_settings <- function(title) {
 
   # Climate change impacts activated, SSP2 default settings, NDC activated, endogenous forestry activated
   cfg <- gms::setScenario(cfg, c("cc", "rcp7p0", "SSP2", "NDC", "ForestryEndo"))
-  input['cellular'] <- "rev4.68FSECmodeling_e2bdb6cd_fd712c0b_cellularmagpie_c200_MRI-ESM2-0-ssp370_lpjml-8e6c5eb1.tgz"
+  cfg$input['cellular'] <- "rev4.68FSECmodeling_e2bdb6cd_fd712c0b_cellularmagpie_c200_MRI-ESM2-0-ssp370_lpjml-8e6c5eb1.tgz"
 
   # Nitrogen module with IPCC emissions factors rescaled with efficiency
   cfg$gms$nitrogen                <- "rescaled_jan21"
@@ -349,7 +349,7 @@ start_run(cfg = cfg, codeCheck = codeCheck)
 #################################################
 cfg <- general_settings(title = "FSEC_SDP")
 # Climate scenario: RCP 2.6
-input['cellular'] <- "rev4.68FSECmodeling_e2bdb6cd_6819938d_cellularmagpie_c200_MRI-ESM2-0-ssp126_lpjml-8e6c5eb1.tgz"
+cfg$input['cellular'] <- "rev4.68FSECmodeling_e2bdb6cd_6819938d_cellularmagpie_c200_MRI-ESM2-0-ssp126_lpjml-8e6c5eb1.tgz"
 ### (1) Population and Health ###
 cfg <- population_transformation(cfg = cfg)
 ### (2) Reduced inequality and Education Transformation ###
