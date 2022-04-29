5739fe8ec4cf0cbe2efc33f32ea62cec6f5852ff
florianh <humpenoeder@pik-potsdam.de>
2022-04-29 07:43:38 +0200

    TEST

--- scripts/start/extra/carbon_pricing.R
+++ scripts/start/extra/carbon_pricing.R
@@ -24,7 +24,9 @@ source("config/default.cfg")
 #download_and_update(cfg)
 
 # create additional information to describe the runs
-cfg$info$flag <- "PR_CO2_after15" 
+cfg$info$flag <- "PR_CO2_after16" 
+
+cfg$gms$s13_max_gdp_shr <- 1
 
 cfg$output <- c("rds_report") # Only run rds_report after model run
 cfg$results_folder <- "output/:title:"
