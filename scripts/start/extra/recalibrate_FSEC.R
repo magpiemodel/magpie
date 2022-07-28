7142a36166d96c54e5c6b4323b919e2eeb44f7d5
Felicitas <beier@pik-potsdam.de>
2022-07-28 08:11:58 +0200

    updated recalibration script

--- scripts/start/extra/recalibrate_FSEC.R
+++ scripts/start/extra/recalibrate_FSEC.R
@@ -21,7 +21,7 @@ codeCheck <- FALSE
 # -----------------------------------------------------------------------------------------------------------------
 # Calibration run
 cfg       <- fsecScenario(scenario = "c_BAU")
-cfg$title <- "calibration_FSEC_27Jul22"
+cfg$title <- "calibration_FSEC_28Jul22a"
 cfg$results_folder                  <- "output/:title:"
 cfg$recalibrate                     <- TRUE
 cfg$recalibrate_landconversion_cost <- TRUE
