54b7d3c8a82c5befdff5d66a8d093d2402a35c19
Felicitas <beier@pik-potsdam.de>
2022-07-28 09:01:28 +0200

    included new pasture realization

--- scripts/start/extra/recalibrate_FSEC.R
+++ scripts/start/extra/recalibrate_FSEC.R
@@ -21,7 +21,7 @@ codeCheck <- FALSE
 # -----------------------------------------------------------------------------------------------------------------
 # Calibration run
 cfg       <- fsecScenario(scenario = "c_BAU")
-cfg$title <- "calibration_FSEC_28Jul22a"
+cfg$title <- "calibration_FSEC_28Jul22b"
 cfg$results_folder                  <- "output/:title:"
 cfg$recalibrate                     <- TRUE
 cfg$recalibrate_landconversion_cost <- TRUE
