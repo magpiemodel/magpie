46956cbb559544e3ea4f94885d1e78a27912b304
Felicitas <beier@pik-potsdam.de>
2022-08-24 17:12:38 +0200

    bugfix in FSDP_collect.R

--- scripts/output/projects/FSDP_collect.R
+++ scripts/output/projects/FSDP_collect.R
@@ -118,7 +118,7 @@ for (i in 1:length(outputdir)) {
     } else missing <- c(missing,outputdir[i])
 
     ## Water
-    nc_file <- file.path(outputdir[i], paste(cfg$title, "efvVolume.mz", sep = "-"))
+    nc_file <- file.path(outputdir[i], paste("efvVolume.mz", sep = "-"))
     if (file.exists(nc_file)) {
       a <- read.magpie(nc_file)[, years, ]
       getNames(a) <- "environmental flow violations (km3)"
@@ -127,7 +127,7 @@ for (i in 1:length(outputdir)) {
       y <- mbind(y, a)
     } else missing <- c(missing, outputdir[i])
 
-    nc_file <- file.path(outputdir[i], paste(cfg$title, "watStress.mz", sep = "-"))
+    nc_file <- file.path(outputdir[i], paste("watStress.mz", sep = "-"))
     if (file.exists(nc_file)) {
       a <- read.magpie(nc_file)[, years, ]
       getNames(a) <- "water withdrawal to availability ratio (ratio)"
