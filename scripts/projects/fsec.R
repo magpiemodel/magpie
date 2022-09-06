fbc370a96a262997b5acd17ce10b39ccd4bd8e87
Felicitas <beier@pik-potsdam.de>
2022-09-06 13:57:33 +0200

    test commit

--- scripts/projects/fsec.R
+++ scripts/projects/fsec.R
@@ -13,7 +13,7 @@ fsecScenario <- function(scenario) {
 
   source("config/default.cfg")
 
-  # version number
+  # Version number
   v <- "v19_FSEC"
 
   x <- list(c_BAU            = list(standard = c("cc", "rcp7p0", "SSP2", "NDC", "ForestryEndo"),
