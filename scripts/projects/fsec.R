68264000b2b22f3395f00566a480e62677252c19
Felicitas <beier@pik-potsdam.de>
2022-09-06 10:39:26 +0200

    updated version number

--- scripts/projects/fsec.R
+++ scripts/projects/fsec.R
@@ -14,7 +14,7 @@ fsecScenario <- function(scenario) {
   source("config/default.cfg")
 
   # version number
-  v <- "v17_FSEC"
+  v <- "v19_FSEC"
 
   x <- list(c_BAU            = list(standard = c("cc", "rcp7p0", "SSP2", "NDC", "ForestryEndo"),
                                   fsec = "FSEC"),
