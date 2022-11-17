e69c8fee4090520fd63d2e0d5145ca438e454312
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2022-11-17 16:22:42 +0100

    remove renv scripts

--- scripts/renv_scripts/restoreRenv.R
+++
@@ -1,5 +0,0 @@
-local({
-  argv <- commandArgs(trailingOnly = TRUE)
-  lockfile <- if (length(argv) >= 1) argv else Sys.glob(c("output/*/renv.lock", "renv/archive/*renv.lock"))
-  piamenv::restoreRenv(lockfile)
-})
