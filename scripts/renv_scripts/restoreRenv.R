67bc08fe94373dbcd72d77d1f1f9b9c7dc962787
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2022-11-17 14:01:36 +0100

    use piamenv

--- scripts/renv_scripts/restoreRenv.R
+++ scripts/renv_scripts/restoreRenv.R
@@ -1,26 +1,5 @@
-# restores current renv to the state described in selected lockfile e.g. from the archive,
-# and writes the restored state to renv.lock
 local({
-  stopifnot(`No renv active. Try starting the R session in the repo root.` = !is.null(renv::project()))
-
   argv <- commandArgs(trailingOnly = TRUE)
-  if (length(argv) == 1) {
-    lockfile <- argv
-  } else {
-    suppressMessages(here::i_am(file.path("scripts", "renv_scripts", "restoreRenv.R")))
-    lockfiles <- c(list.files(here::here("renv/archive"), full.names = TRUE),
-                   Filter(file.exists, file.path(list.files(here::here("output"), full.names = TRUE), "renv.lock")))
-    message(paste0(seq_along(lockfiles), ": ",
-                   sub(here::here(""), "", lockfiles, fixed = TRUE),
-                   collapse = "\n"))
-    message("Number of the lockfile to restore: ", appendLF = FALSE)
-
-    lockfile <- lockfiles[as.integer(gms::getLine())]
-  }
-
-  if (!isTRUE(file.exists(lockfile))) {
-    stop("lockfile '", file.path(normalizePath("."), lockfile), "' does not exist.")
-  }
-  renv::restore(lockfile = lockfile, clean = TRUE, prompt = FALSE)
-  renv::snapshot(prompt = FALSE) # ensure main renv.lock is in sync with library
+  lockfile <- if (length(argv) >= 1) argv else Sys.glob(c("output/*/renv.lock", "renv/archive/*renv.lock"))
+  piamenv::restoreRenv(lockfile)
 })
--- scripts/renv_scripts/updateRenv.R
+++ scripts/renv_scripts/updateRenv.R
@@ -1,14 +1,1 @@
-# Updates all pik-piam R packages in the current renv, updates renv.lock, and archives the renv.lock
-local({
-  stopifnot(`No renv active. Try starting the R session in the repo root.` = !is.null(renv::project()))
-  suppressMessages(here::i_am(file.path("scripts", "renv_scripts", "updateRenv.R")))
-
-  # get all packages in pik-piam r-universe
-  packagesUrl <- "https://pik-piam.r-universe.dev/src/contrib/PACKAGES"
-  pikPiamPackages <- sub("^Package: ", "", grep("^Package: ", readLines(packagesUrl), value = TRUE))
-
-  # update pik-piam packages only
-  renv::update(intersect(utils::installed.packages()[, "Package"], pikPiamPackages), prompt = FALSE)
-
-  source(here::here("scripts", "renv", "archiveRenv.R"))
-})
+piamenv::updateRenv()
