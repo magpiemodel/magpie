5bfd715e4c4b6ec1140146a129ecd01d8961094a
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2022-12-09 14:58:18 +0100

    compile main gms

---
+++ scripts/start/compile_main_gms.R
@@ -0,0 +1,16 @@
+# |  (C) 2008-2021 Potsdam Institute for Climate Impact Research (PIK)
+# |  authors, and contributors see CITATION.cff file. This file is part
+# |  of MAgPIE and licensed under AGPL-3.0-or-later. Under Section 7 of
+# |  AGPL-3.0, you are granted additional permissions described in the
+# |  MAgPIE License Exception, version 1.0 (see LICENSE file).
+# |  Contact: magpie@pik-potsdam.de
+
+# ------------------------------------------------
+# description: download input and compile main.gms
+# position: 7
+# ------------------------------------------------
+
+source("scripts/start/download_data.R")
+
+# compile main.gms
+system("gams main.gms action=c")
