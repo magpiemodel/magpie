c24e4044727e92bf78b582179c12142c6101a798
PrantikaDas <prantikadas16@gmail.com>
2023-01-10 13:49:50 +0100

    added sub-national validation output script for India

---
+++ scripts/output/projects/FABLE_India_subnational_validation.R
@@ -0,0 +1,105 @@
+# |  (C) 2008-2021 Potsdam Institute for Climate Impact Research (PIK)
+# |  authors, and contributors see CITATION.cff file. This file is part
+# |  of MAgPIE and licensed under AGPL-3.0-or-later. Under Section 7 of
+# |  AGPL-3.0, you are granted additional permissions described in the
+# |  MAgPIE License Exception, version 1.0 (see LICENSE file).
+# |  Contact: magpie@pik-potsdam.de
+
+# --------------------------------------------------------------
+# description: India subnational validation 
+# comparison script: FALSE
+# ---------------------------------------------------------------
+
+#Version 1.00 - Miodrag Stevanovic, Prantika Das
+# 1.00: first working version
+
+library(lucode2)
+library(magpie4)
+library(madrat)
+library(mrfable)
+library(mip)
+library(mrvalidation)
+library(magpiesets)
+library(lusweave)
+
+print("Start India state level validation output runscript")
+
+############################# BASIC CONFIGURATION #######################################
+
+if(!exists("source_include")) {
+  
+  outputdir <- "output//v4p77/h12_2022-12-15_22.09.46"
+  
+  ###Define arguments that can be read from command line
+  readArgs("outputdir")
+}
+# hist     <- "runs/validation.mif"
+file    <- file.path(outputdir,paste0("India_subnational_validation_",format(Sys.time(), "%Y%H%M%S"),".pdf"))
+#########################################################################################
+
+print(paste0("Script started for output directory: ",outputdir))
+
+# Determine cells for India
+mapping <- toolGetMapping("scripts/npi_ndc/policies/country2cell.rds")
+mapping$iso_mag <- paste(mapping$iso,mapping$cell,sep=".")
+# load Indian states
+ind_states <- read.csv(system.file("extdata", "regional/india_state_code.csv", package = "mrfable"))
+ind_cells <- which(mapping$ind %in% ind_states$State_code)
+
+# Subnational validation data of specific crops
+land_hr <- read.magpie(file.path(outputdir,"cell.land_0.5.mz"))[ind_cells,,]
+croparea_hr_share <- read.magpie(file.path(outputdir,"cell.croparea_0.5_share.mz"))[ind_cells,,]
+cell_hr <- dimSums(land_hr, dim=3)
+croparea_hr <- croparea_hr_share[,,]*setItems(cell_hr[,1,], dim=2, NULL)
+croparea_hr <- dimSums(croparea_hr, dim=3.2)
+
+# aggregate land from grid to state level
+mapping_ind <- mapping[ind_cells,]
+croparea_state <- toolAggregate(croparea_hr, rel=mapping_ind, from="iso_mag", to="ind")
+
+# Observed data from mrfable
+setConfig(extramappings = "mappingIndiaAPY.csv")
+ind_apy_mapping <- read.csv(system.file("extdata", "regional/mappingIndiaAPY.csv", package = "mrfable"))
+h <- calcOutput("IndiaFoodcrop", subtype = "Area", aggregate = "Region")
+# Data cleaning:
+h <- h[ind_states$State_code,,]
+h <- collapseNames(h)
+h <- collapseNames(h[,,"total"])
+h <- h/1000
+# Crops in the APY database:
+mappingCropsAPY <- as.matrix(data.frame(APYcrop=c("Bajra", "Barley", "Gram",
+                                                  "Jowar",  "Tur", "Maize",
+                                                  "Wheat", "Ragi", "Rice"), 
+                                              k=c("trce", "tece", "puls_pro", 
+                                                  "trce", "puls_pro", "maiz",
+                                                  "tece", "trce", "rice_pro")))
+# Aggregate to magpie crops:
+h <- toolAggregate(h, rel=mappingCropsAPY, from="APYcrop", to="k", dim=3)
+h <- add_dimension(h, dim=3.1, add="scenario", nm="historical")
+h <- add_dimension(h, dim=3.2, add="model", nm="APY")
+h <- setItems(h, dim=3.3, reportingnames(getItems(h,dim=3.3)))
+
+croparea_state <- croparea_state[,,unique(mappingCropsAPY[,"k"])]
+names(dimnames(croparea_state)) <- c("state","year","crop")
+croparea_state <- add_dimension(croparea_state, dim=3.1, add="scenario", nm="")
+croparea_state <- add_dimension(croparea_state, dim=3.2, add="model", nm="MAgPIE")
+croparea_state <- setItems(croparea_state, dim=3.3, reportingnames(getItems(croparea_state,dim=3.3)))
+
+sw <- swopen(file)
+swlatex(sw,"\\huge")
+swlatex(sw,"\\textbf{MAgPIE state level croparea comparison for India}\\newline")
+swlatex(sw,"\\normalsize")
+swlatex(sw,"\\newline")
+swlatex(sw,"\\tableofcontents")
+
+for(k in getItems(h, dim=3.3)){
+  for(state in getItems(h, dim=1)){
+    swlatex(sw,"\\newpage")
+    swlatex(sw,paste0("\\section{Croparea ",k," in ",ind_states[which(ind_states$State_code==state),"State_name"],"}"))
+    swfigure(sw, print, mipLineHistorical(croparea_state[state,,k],h[state,,k],ylab="Mha"))
+  }
+}
+
+swclose(sw,clean_output=TRUE)
+
+
