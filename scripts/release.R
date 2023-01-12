73772d4dcdcc4b1ab98d4b6d3fef93831f8bc054
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2023-01-12 10:59:27 +0100

    minor

--- scripts/release.R
+++ scripts/release.R
@@ -1,9 +1,10 @@
 release <- function(newVersion) {
+  githubUrl <- "https://github.com/magpiemodel/magpie/compare/"
   readLines("CHANGELOG.md") |>
     sub(pattern = "## [Unreleased]", replacement = paste0("## [", newVersion, "]"), fixed = TRUE) |>
-    sub(pattern = "\\[Unreleased\\]: https://github.com/magpiemodel/magpie/compare/v([0-9]+\\.[0-9]+\\.[0-9]+)...develop",
-        replacement = paste0("[Unreleased]: https://github.com/magpiemodel/magpie/compare/v", newVersion, "...develop\n",
-                             "[", newVersion, "]: https://github.com/magpiemodel/magpie/compare/v\\1...v", newVersion)) |>
+    sub(pattern = "\\[Unreleased\\]: ", githubUrl, "v([0-9]+\\.[0-9]+\\.[0-9]+)...develop",
+        replacement = paste0("[Unreleased]: ", githubUrl, "v", newVersion, "...develop\n",
+                             "[", newVersion, "]: ", githubUrl, "v\\1...v", newVersion)) |>
     writeLines("CHANGELOG.md")
 }
 
