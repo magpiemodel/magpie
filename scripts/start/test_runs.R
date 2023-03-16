8dcc89eefda98547a242593b16b53b2904a4605b
florianh <humpenoeder@pik-potsdam.de>
2023-03-16 14:31:20 +0100

    bugfix

--- scripts/start/test_runs.R
+++ scripts/start/test_runs.R
@@ -54,8 +54,9 @@ for(ssp in c("SSP1","SSP2","SSP5")) {
   cfg$title <- .title(cfg, paste(ssp,"NDC",sep="-"))
   cfg <- setScenario(cfg,c(ssp,"NDC","rcp4p5"))
   cfg$gms$c56_mute_ghgprices_until <- "y2150"
-  cfg$gms$c56_pollutant_prices <- paste0("R21M42-",ssp,"-NDC")
-  cfg$gms$c60_2ndgen_biodem    <- paste0("R21M42",ssp,"-NDC")
+# Input for NDC from R21M42 is not available, therefore NPi is used.
+  cfg$gms$c56_pollutant_prices <- paste0("R21M42-",ssp,"-NPi")
+  cfg$gms$c60_2ndgen_biodem    <- paste0("R21M42",ssp,"-NPi")
   start_run(cfg, codeCheck = FALSE)
 
   cfg$title <- .title(cfg, paste(ssp,"PkBudg900",sep="-"))
