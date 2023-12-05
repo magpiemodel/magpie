2e8003c3235955cabb335867215f229c23c51709
Felicitas <beier@pik-potsdam.de>
2023-12-05 11:43:22 +0100

    updated preprocessing for EAT runs

--- scripts/start/projects/project_EAT2p0.R
+++ scripts/start/projects/project_EAT2p0.R
@@ -70,7 +70,7 @@ bau <- function(cfg) {
   cfg$gms$c60_2ndgen_biodem    <- "R21M42-SSP2-NPi"      # default
 
   # Climate Change
-  cfg$input['cellular'] <- "rev4.94_h12_fd712c0b_cellularmagpie_c200_MRI-ESM2-0-ssp370_lpjml-8e6c5eb1.tgz"
+  cfg$input['cellular'] <- "WARNINGS2_rev4.95_h12_c6a7458f_cellularmagpie_c200_IPSL-CM6A-LR-ssp370_lpjml-8e6c5eb1.tgz"
 
   return(cfg)
 }
@@ -141,7 +141,7 @@ noCC <- function(cfg) {
 ### RCP 2.6 ###
 # Decomposition Scenario. Apply lower climate impacts based on RCP 2.6 to BAU
 rcp26 <- function(cfg) {
-  cfg$input['cellular'] <- "rev4.94_h12_6819938d_cellularmagpie_c200_MRI-ESM2-0-ssp126_lpjml-8e6c5eb1.tgz"
+  cfg$input['cellular'] <- "WARNINGS4_rev4.95_h12_5033a1ce_cellularmagpie_c200_IPSL-CM6A-LR-ssp126_lpjml-8e6c5eb1.tgz"
   return(cfg)
 }
 
