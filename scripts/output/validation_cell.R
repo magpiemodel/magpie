537a9535aa352c1c0e453a039b35148f7a17dfa8
emolinab <mbacca@pik-potsdam.de>
2024-02-23 13:58:21 +0100

    correction indicators  definition

--- scripts/output/validation_cell.R
+++ scripts/output/validation_cell.R
@@ -6,7 +6,7 @@
 # |  Contact: magpie@pik-potsdam.de
 
 # ---------------------------------------------------------------------------------------------------
-# description: Writes a validation file where land use types and main crops (soybean, maize, rice and 
+# description: Writes a validation file where land use types and main crops (soybean, maize, rice and
 # temparate cereals/wheat) at cellular level are compared to LUH and SPAM initial values.
 # ---------------------------------------------------------------------------------------------------
 
@@ -22,7 +22,8 @@ library(magpiesets)
 map_file <- Sys.glob(file.path(outputdir, "/clustermap_*.rds"))
 gdx <- file.path(outputdir, "/fulldata.gdx")
 cfg <- gms::loadConfig(file.path(outputdir, "/config.yml"))
-crops<-c("maiz","rice_pro","soybean","tece")
+crops <- c("maiz", "rice_pro", "soybean", "tece")
+mapping <- readRDS(map_file)
 ################################################################################
 
 #### Folder for saving results
@@ -33,35 +34,39 @@ if (!dir.exists(out_dir)) dir.create(out_dir)
 magpie_LU <- land(gdx, level = "cell")
 historical_LU <- readGDX(gdx, "f10_land")
 
-LU <- plotCorrHist2D(x = historical_LU, y = magpie_LU, xlab = "LUH2 \n (mio.ha)", ylab = "MAgPIE \n (mio.ha)", bins = 35,
-    folder = out_dir, file = "/validationLUcell", breaks = c(1,10,100), nrows=2, ncols=2, axisFont=10,
-                           axisTitleFont=11,TitleFontSize=12,legendTitleFont=10,legendTextFont=8,statFont=2,stat=FALSE, table=TRUE,tag="year")
+LU <- plotCorrHist2D(
+    x = historical_LU, y = magpie_LU, xlab = "LUH2 \n (mio.ha)", ylab = "MAgPIE \n (mio.ha)", bins = 35,
+    folder = out_dir, file = "/validationLUcell", breaks = c(1, 10, 100), nrows = 2, ncols = 2, axisFont = 10,
+    axisTitleFont = 11, TitleFontSize = 12, legendTitleFont = 10, legendTextFont = 8, statFont = 2, stat = FALSE, table = TRUE, tag = "year"
+)
 
 ######## Crop types ("maiz","rice_pro","soybean","tece") comparison  with LUH ########
 magpie <- croparea(gdx, level = "cell", product_aggr = FALSE, water_aggr = TRUE)[, getYears(historical_LU), crops]
 historical1 <- dimOrder(readGDX(gdx, "fm_croparea")[, getYears(historical_LU), crops], perm = c(2, 1), dim = 3)
-historical1 <- dimSums(historical1,dim=3.2)
+historical1 <- dimSums(historical1, dim = 3.2)
 
 
 Crops <- plotCorrHist2D(
     x = historical1, y = magpie, xlab = "LUH2 \n (mio.ha)", ylab = "MAgPIE \n (mio.ha)", bins = 35,
-    folder = out_dir, file = "/validationCropCell", breaks = c(1,10,50), nrows=2, ncols=2, axisFont=10,
-                           axisTitleFont=11,TitleFontSize=12,legendTitleFont=10,legendTextFont=8,statFont=2,stat=FALSE, table=TRUE,tag="year")
-
-######## Crop types ("maiz","rice_pro","soybean","tece") comparison  with SPAM ########
-
-mapping <- readRDS(map_file)
-historical <- readSource(type="MAPSPAM",subtype = "physical")[,,crops]
-historical <- magpiesort(toolAggregate(historical,rel=mapping,from="cell",to="cluster"))/1e6
-historical <- dimSums(historical, dim=3.2)
-
-CropsSPAM <- plotCorrHist2D(
-    x = historical, y = magpie[,getYears(historical),], xlab = "MAPSPAM \n (mio.ha)", ylab = "MAgPIE \n (mio.ha)", bins = 35,
-    folder = out_dir, file = "/validationCropCellSPAM", breaks = c(1, 10, 50), nrows = 2, ncols = 2, axisFont=10,
-                           axisTitleFont=11,TitleFontSize=12,legendTitleFont=10,legendTextFont=8,statFont=2,stat=FALSE, table=TRUE,
-                           palette = "PiYG",tag="year"
+    folder = out_dir, file = "/validationCropCell", breaks = c(1, 10, 50), nrows = 2, ncols = 2, axisFont = 10,
+    axisTitleFont = 11, TitleFontSize = 12, legendTitleFont = 10, legendTextFont = 8, statFont = 2, stat = FALSE, table = TRUE, tag = "year"
 )
 
+######## Crop types ("maiz","rice_pro","soybean","tece") comparison  with SPAM ########
+cells <- if (length(mapping$cell) == 59199) "magpiecell" else if (length(mapping$cell) == 67420) "lpjcell"
+if (cells == "lpjcell") {
+    mapping <- readRDS(map_file)
+    historical <- readSource(type = "MAPSPAM", subtype = "physical")[, , crops]
+    historical <- magpiesort(toolAggregate(historical, rel = mapping, from = "cell", to = "cluster")) / 1e6
+    historical <- dimSums(historical, dim = 3.2)
+
+    CropsSPAM <- plotCorrHist2D(
+        x = historical, y = magpie[, getYears(historical), ], xlab = "MAPSPAM \n (mio.ha)", ylab = "MAgPIE \n (mio.ha)", bins = 35,
+        folder = out_dir, file = "/validationCropCellSPAM", breaks = c(1, 10, 50), nrows = 2, ncols = 2, axisFont = 10,
+        axisTitleFont = 11, TitleFontSize = 12, legendTitleFont = 10, legendTextFont = 8, statFont = 2, stat = FALSE, table = TRUE,
+        palette = "PiYG", tag = "year"
+    )
+}
 ######################################### Generates document ##############################################
 template <- c(
     "\\documentclass[a4paper, portrait ]{article}",
@@ -82,42 +87,43 @@ template <- c(
 )
 
 ############################## Plots LUH vs. magpie land use types ##########################################################
-x<-LU[[1]]
-y<-as.data.frame(LU[[2]])
-colnames(y)<-y[1,]
-rownames(y)<-NULL
-y<-(y[2:(nrow(y)),])
-y[,3:ncol(y)]<-round(as.numeric(unlist(y[,3:ncol(y)])),3)
-colnames(y)[2]<-"Year"
-colnames(y)[6] <- "MAEH*"
+x <- LU[[1]]
+y <- as.data.frame(LU[[2]])
+colnames(y) <- y[1, ]
+rownames(y) <- NULL
+y <- (y[2:(nrow(y)), ])
+y[, 3:ncol(y)] <- round(as.numeric(unlist(y[, 3:ncol(y)])), 3)
+colnames(y) <- c("Year", "Variable", "R2", "MAE", "MPE", "MAPE")
 
-tmpplot <- function(x,range){
-    plotLU<-function(x,range){
-    print(gridExtra::marrangeGrob(x[range], nrow = 3, ncol = 2, top = NULL))
+tmpplot <- function(x, range) {
+    plotLU <- function(x, range) {
+        print(gridExtra::marrangeGrob(x[range], nrow = 3, ncol = 2, top = NULL))
     }
-     a <- capture.output(plotLU(x,range))
- }
+    a <- capture.output(plotLU(x, range))
+}
 
 namesReport <- as.character(magpiesets::reportingnames(getNames(magpie_LU)))
 
-sw <- swopen(outfile = paste0(outputdir,"/CellValidation.pdf"), template = template)
-swlatex(sw, c("\\title{MAgPIE Validation on Cellular level}",  "\\maketitle", "\\tableofcontents"))
-swlatex(sw, "\\section{Land use between 1995-2020}")
+sw <- swopen(outfile = paste0(outputdir, "/CellValidation.pdf"), template = template)
+swlatex(sw, c("\\title{MAgPIE Validation on Cellular level}", "\\maketitle", "\\tableofcontents"))
+swlatex(sw, "\\section{Land use between 1995-2020 compared to LUH2}")
 
-b<-0
+b <- 0
 
-for(i in 1:length(namesReport)){
-swlatex(sw, paste0("\\subsection{",namesReport[i],"}"))
-a<-b+1
-b<-a+length(getYears(historical_LU))-1
-swfigure(sw, tmpplot,x=x,c(a:b))
-swtable(sw, collapseNames(as.magpie(y[a:b, ])), table.placement = "H", caption.placement = "top", 
-        transpose = FALSE, caption = paste0("Compilation of error measurements for ", namesReport[i] , " data comparing MAgPIE outputs \\& the LUH2 set at cluster level"), vert.lines = 0, align = "c")
+for (i in 1:length(namesReport)) {
+    swlatex(sw, paste0("\\subsection{", namesReport[i], "}"))
+    a <- b + 1
+    b <- a + length(getYears(historical_LU)) - 1
+    swfigure(sw, tmpplot, x = x, c(a:b))
+    swtable(sw, collapseNames(as.magpie(y[a:b, ])),
+        table.placement = "H", caption.placement = "top",
+        transpose = FALSE, caption = paste0("Compilation of error measurements for ", namesReport[i], " data comparing MAgPIE outputs \\& the LUH2 set at cluster level"), vert.lines = 0, align = "c"
+    )
 }
 swlatex(sw, "\\newpage")
 
 ############################## Plots LUH vs. magpie crop types ##########################################################
-swlatex(sw, "\\section{Crop types 1995-2015 LUH}")
+swlatex(sw, "\\section{Crop types between 1995-2015 compared to LUH2}")
 
 namesReport1 <- as.character(magpiesets::reportingnames(getNames(magpie)))
 x1 <- Crops[[1]]
@@ -126,8 +132,7 @@ colnames(y1) <- y1[1, ]
 rownames(y1) <- NULL
 y1 <- (y1[2:(nrow(y1)), ])
 y1[, 3:ncol(y1)] <- round(as.numeric(unlist(y1[, 3:ncol(y1)])), 3)
-colnames(y1)[2] <- "Year"
-colnames(y1)[6] <- "MAEH*"
+colnames(y1) <- c("Year", "Variable", "R2", "MAE", "MPE", "MAPE")
 
 tmpplot1 <- function(x1, range) {
     plotLU1 <- function(x1, range) {
@@ -144,51 +149,55 @@ for (i in 1:length(namesReport1)) {
     swfigure(sw, tmpplot1, x = x1, c(a1:b1))
     swtable(sw, collapseNames(as.magpie(y1[a1:b1, ])),
         table.placement = "H", caption.placement = "top",
-        transpose = FALSE, caption = paste0("Compilation of error measurements for ", namesReport[i] , " data comparing MAgPIE outputs \\& the LUH2 set at cluster level"), vert.lines = 0, align = "c"
+        transpose = FALSE, caption = paste0("Compilation of error measurements for ", namesReport[i], " data comparing MAgPIE outputs \\& the LUH2 set at cluster level"), vert.lines = 0, align = "c"
     )
 }
 
 
 ############################## Plots MAPSPAM vs. magpie crop types ##########################################################
-swlatex(sw, "\\newpage")
-swlatex(sw, "\\section{Crop types 2000-2010 - MAPSPAM and LUH comparison}")
 
-x2 <-CropsSPAM[[1]]
-names(x2) <- paste0(names(x2),"MAP")
-x3<-Crops[[1]][names(CropsSPAM[[1]])]
-y2 <- as.data.frame(CropsSPAM[[2]])
-colnames(y2) <- y2[1, ]
-rownames(y2) <- NULL
-y2 <- (y2[2:(nrow(y2)), ])
-y2[, 3:ncol(y2)] <- round(as.numeric(unlist(y2[, 3:ncol(y2)])), 3)
-colnames(y2)[2] <- "Year"
-colnames(y2)[6] <- "MAEH*"
-
-tmpplot2 <- function(x2,x3,range) {
-    plotLU2 <- function(x2,x3,range) {
-        print(gridExtra::marrangeGrob(c(x2[range],x3[range]), nrow = 3, ncol = 2, top = NULL))
-    }
-    a <- capture.output(plotLU2(x2,x3, range))
-}
-
-b2 <- 0
-for (i in 1:length(namesReport1)) {
-    swlatex(sw, paste0("\\subsection{", namesReport1[i], "}"))
-    swlatex(sw, "\\qquad \\textbf{MAPSPAM} \\qquad \\qquad \\qquad \\qquad \\qquad \\qquad \\qquad \\qquad \\qquad \\qquad \\textbf{LUH2}")
-    a2 <- b2 + 1
-    b2 <- a2 + length(getYears(historical)) - 1
-    swfigure(sw, tmpplot2, x2 = x2, x3 = x3, range= c(a2:b2))
-    swtable(sw, collapseNames(as.magpie(y2[a2:b2, ])),
-        table.placement = "H", caption.placement = "top",
-        transpose = FALSE, caption = paste0("Compilation of error measurements for ", namesReport[i] , " data comparing MAgPIE outputs \\& the LUH2 set at cluster level"), vert.lines = 0, align = "c")
+if (cells == "lpjcell") {
     swlatex(sw, "\\newpage")
-    
-}
+    swlatex(sw, "\\section{Crop types between 1995-2015 compared to MAPSPAM}")
 
-########## Details about MAEH ##########
+    x2 <- CropsSPAM[[1]]
+    names(x2) <- paste0(names(x2), "MAP")
+    x3 <- Crops[[1]][names(CropsSPAM[[1]])]
+    y2 <- as.data.frame(CropsSPAM[[2]])
+    colnames(y2) <- y2[1, ]
+    rownames(y2) <- NULL
+    y2 <- (y2[2:(nrow(y2)), ])
+    y2[, 3:ncol(y2)] <- round(as.numeric(unlist(y2[, 3:ncol(y2)])), 3)
+    colnames(y2) <- c("Year", "Variable", "R2", "MAE", "MPE", "MAPE")
+
+    tmpplot2 <- function(x2, x3, range) {
+        plotLU2 <- function(x2, x3, range) {
+            print(gridExtra::marrangeGrob(c(x2[range], x3[range]), nrow = 3, ncol = 2, top = NULL))
+        }
+        a <- capture.output(plotLU2(x2, x3, range))
+    }
+
+    b2 <- 0
+    for (i in 1:length(namesReport1)) {
+        swlatex(sw, paste0("\\subsection{", namesReport1[i], "}"))
+        swlatex(sw, "\\qquad \\textbf{MAPSPAM} \\qquad \\qquad \\qquad \\qquad \\qquad \\qquad \\qquad \\qquad \\qquad \\qquad \\textbf{LUH2}")
+        a2 <- b2 + 1
+        b2 <- a2 + length(getYears(historical)) - 1
+        swfigure(sw, tmpplot2, x2 = x2, x3 = x3, range = c(a2:b2))
+        swtable(sw, collapseNames(as.magpie(y2[a2:b2, ])),
+            table.placement = "H", caption.placement = "top",
+            transpose = FALSE, caption = paste0("Compilation of error measurements for ", namesReport[i], " data comparing MAgPIE outputs \\& the MAPSPAM set at cluster level"), vert.lines = 0, align = "c"
+        )
+        swlatex(sw, "\\newpage")
+    }
+}
+########## Details about statistics ##########
 swlatex(sw, "\\newpage")
-swlatex(sw, "*MAEH Stands for Mean Absolute Error per hectar. It is calculated as $MAE=\\sum(|error|)/area$ . Here, error represents the difference between LUH/MAPSPAM values and MAgPIE Outputs.")
-swlatex(sw, " Area is the overal world area for the specific land use or crop type in time t as reported by LUH2 or MAPSPAM")
+swlatex(sw, "\\section{Details about statistical measuremens of error and goodness of fit}")
+swlatex(sw, "\\begin{itemize} \\item R2 represents the coefficient of determination. \\end{itemize}")
+swlatex(sw, "\\begin{itemize} \\item MAE stands for mean absolute error and is calculated as $MAE=\\frac{1}{n}\\sum(|error|)$. Where, error represents the difference between LUH/MAPSPAM values and MAgPIE Outputs.  \\end{itemize}")
+swlatex(sw, "\\begin{itemize} \\item MPE stands for mean percentage error (a measure of bias) and is calculated as $MPE=\\frac{100\\%}{n}\\sum(RE)$. Where, RE (relative error) represents the relative difference between LUH/MAPSPAM values and MAgPIE Outputs.  \\end{itemize}")
+swlatex(sw, "\\begin{itemize} \\item Finally, MAPE stands for mean absolute percentage error and is calculated as $MAPE=\\frac{100\\%}{n}\\sum(|RE|)$. Where, RE (relative error) represents the relative difference between LUH/MAPSPAM values and MAgPIE Outputs.  \\end{itemize}")
 
 ####### Closes the pdf document
 swclose(sw, clean_output = FALSE, engine = "knitr")
@@ -197,11 +206,13 @@ swclose(sw, clean_output = FALSE, engine = "knitr")
 
 ##### Erase latex style and output files
 
-fileErase <- c(Sys.glob(file.path(outputdir, "/CellValidation.*")),
-               Sys.glob(file.path(outputdir, "/Sweave.sty")),
-               Sys.glob(file.path(outputdir, "figure/*")),
-               Sys.glob(file.path(outputdir, "figure")))
+fileErase <- c(
+    Sys.glob(file.path(outputdir, "/CellValidation.*")),
+    Sys.glob(file.path(outputdir, "/Sweave.sty")),
+    Sys.glob(file.path(outputdir, "figure/*")),
+    Sys.glob(file.path(outputdir, "figure"))
+)
 fileErase <- fileErase[!(fileErase %in% paste0(outputdir, "//CellValidation.pdf"))]
 
 
-unlink(fileErase,recursive = TRUE)
+unlink(fileErase, recursive = TRUE)
