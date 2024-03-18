32256fdd6158ef1d58a4cb550a14fe3b9ec750a4
Felicitas <beier@pik-potsdam.de>
2024-03-18 10:36:23 +0100

    activated yield calibration for FSEC runs

--- scripts/start/extra/recalibrate_FSEC.R
+++ scripts/start/extra/recalibrate_FSEC.R
@@ -22,7 +22,7 @@ source("scripts/projects/fsec.R")
 cfg       <- fsecScenario(scenario = "c_BAU")
 cfg$title <- "FSEC13Mar2024"
 cfg$results_folder                  <- "output/:title:"
-cfg$recalibrate                     <- FALSE # required when penality_apr22 activated
+cfg$recalibrate                     <- TRUE # required when penality_apr22 activated
 cfg$best_calib                      <- TRUE
 cfg$recalibrate_landconversion_cost <- TRUE
 cfg$best_calib_landconversion_cost  <- FALSE
