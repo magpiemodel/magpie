25cbd4f574b9570086a8cb27bfdb8945e26fe324
florianh <humpenoeder@pik-potsdam.de>
2024-06-17 20:25:53 +0200

    update input data

--- scripts/start/projects/paper_peatlandTax.R
+++ scripts/start/projects/paper_peatlandTax.R
@@ -23,126 +23,108 @@ source("scripts/start_functions.R")
 source("config/default.cfg")
 
 # create additional information to describe the runs
-cfg$info$flag <- "V14LRc500"
+cfg$info$flag <- "PTax02H16"
 
 cfg$results_folder <- "output/:title:"
 cfg$results_folder_highres <- "output"
 cfg$force_replace <- TRUE
-cfg$qos <- "priority_maxMem"
-#cfg$qos <- "standby_maxMem_dayMax"
+#cfg$qos <- "priority_maxMem"
+cfg$qos <- "standby_maxMem_dayMax"
 
 # support function to create standardized title
 .title <- function(cfg, ...) return(paste(cfg$info$flag, sep="_",...))
 
+cfg$repositories <- append(list("https://rse.pik-potsdam.de/data/magpie/public"=NULL,
+                                "./patch_inputdata"=NULL),
+                           getOption("magpie_repos"))
 
-# EUR <- "ALB,AUT,BEL,BGR,CYP,CZE,DEU,DNK,ESP,EST,FIN,FRA,FRO,
-#                 GBR,GGY,GIB,GRC,HRV,HUN,IMN,IRL,ITA,JEY,LTU,LUX,LVA,MLT,
-#                 NLD,POL,PRT,ROU,SVK,SVN,SWE"
+cfg$input['regional'] <- "rev4.108_36f73207_magpie.tgz"
+cfg$input['validation'] <- "rev4.108_36f73207_validation.tgz"
+cfg$input['calibration'] <- "calibration_H16_14Jun24.tgz"
+cfg$input['zzzpatchGHGprices'] <- "patchGHGprices.tgz"
 
-ssp <- "SSP2"
-#cfg$input['calibration'] <- "calibration_H12c400_16Apr24.tgz"
-cfg$input['calibration'] <- "calibration_H12c500_15Apr24.tgz"
 cfg$gms$c_timesteps <- "5year"
 
-limitAff <- function(cfg,gdx) {
-  #download input files with high resolution
-  download_and_update(cfg)
+cfg$output <- c(cfg$output, "extra/highres")
+ssp <- "SSP2"
+cfg <- setScenario(cfg,c(ssp,"NPI","rcp7p0"))
+cfg$input['cellular'] <- "rev4.108_36f73207_2ca80312_cellularmagpie_c400_MRI-ESM2-0-ssp370_lpjml-8e6c5eb1.tgz"
+cfg$gms$c60_2ndgen_biodem    <- paste0("R32M46-", if (ssp=="SSP2") "SSP2EU" else ssp,"-NPi")
+cfg$gms$c56_pollutant_prices_noselect <- "T0-CO2"
+cfg$gms$policy_countries56  <- isoCountriesEUR
 
-  # #get tau from low resolution run
-  # ov_tau <- readGDX(gdx, "ov_tau",select=list(type="level"))
-  # write.magpie(ov_tau,"modules/13_tc/input/f13_tau_scenario.csv")
-  # cfg$gms$tc <- "exo"
-
-  #get regional afforestation patterns from low resolution run with c200
-  aff <- dimSums(landForestry(gdx)[,,c("aff","ndc")],dim=3)
-  #Take away initial NDC area for consistency with global afforestation limit
-  aff <- aff-setYears(aff[,1,],NULL)
-  #calculate maximum regional afforestation over time
-  aff_max <- setYears(aff[,1,],NULL)
-  for (r in getRegions(aff)) {
-    aff_max[r,,] <- max(aff[r,,])
-  }
-  aff_max[aff_max < 0] <- 0
-  write.magpie(aff_max,"modules/32_forestry/input/f32_max_aff_area.cs4")
-  cfg$gms$s32_max_aff_area_glo <- 0
-  #check
-  if(cfg$gms$s32_max_aff_area < Inf) {
-    indicator <- abs(sum(aff_max)-cfg$gms$s32_max_aff_area)
-    if(indicator > 1e-06) warning(paste("Global and regional afforestation limit differ by",indicator,"Mha"))
-  }
-  return(cfg)
+calc_ghgprice <- function() {
+  T0 <- read.magpie("modules/56_ghg_policy/input/f56_pollutant_prices.cs3")
+  T0 <- collapseNames(T0[,,getNames(T0,dim=2)[1]])
+  T0[,,] <- 0
+  
+  #T200 200 USD/tCO2 in 2050
+  T200 <- new.magpie(getRegions(T0),c(seq(1995,2025,by=5),2050,2100,2150),getNames(T0),fill = 0)
+  T200[,"y2025","co2_c"] <- 0
+  T200[,"y2050","co2_c"] <- 200
+  T200[,"y2100","co2_c"] <- 200
+  T200[,"y2150","co2_c"] <- 200
+  T200 <- time_interpolate(T200,seq(2015,2100,by=5),TRUE)
+  T200 <- time_interpolate(T200,seq(2100,2150,by=5),TRUE)
+  T200[,,"ch4"] <- T200[,,"co2_c"]*28
+  T200[,,"n2o_n_direct"] <- T200[,,"co2_c"]*265*44/28
+  T200[,,"n2o_n_indirect"] <- T200[,,"co2_c"]*265*44/28
+  T200[,,"co2_c"] <- T200[,,"co2_c"]*44/12
+  
+  T50 <- T200 * 0.25
+  T100 <- T200 * 0.5
+  
+  GHG <- mbind(add_dimension(T0, dim = 3.2, add = "scen", nm = "T0-GHG"), 
+               add_dimension(T50, dim = 3.2, add = "scen", nm = "T50-GHG"), 
+               add_dimension(T100, dim = 3.2, add = "scen", nm = "T100-GHG"),
+               add_dimension(T200, dim = 3.2, add = "scen", nm = "T200-GHG"))
+  
+  CO2 <- GHG
+  CO2[,,c("ch4", "n2o_n_direct", "n2o_n_indirect")] <- 0
+  getNames(CO2,dim=2) <- gsub("GHG","CO2", getNames(CO2,dim=2))
+  
+  GHGCH4GWP20 <- GHG
+  GHGCH4GWP20[,,"ch4"] <- GHGCH4GWP20[,,"ch4"]/28*84
+  getNames(GHGCH4GWP20,dim=2) <- gsub("GHG","GHG-CH4GWP20", getNames(GHGCH4GWP20,dim=2))
+  
+  GHG <- mbind(CO2, GHG, GHGCH4GWP20)
+  if (!dir.exists("./patch_inputdata")) dir.create("./patch_inputdata")
+  if (dir.exists("./patch_inputdata/patchGHGprices")) unlink("./patch_inputdata/patchGHGprices", recursive = TRUE)
+  dir.create("./patch_inputdata/patchGHGprices")
+  write.magpie(GHG,file_name = "patch_inputdata/patchGHGprices/f56_pollutant_prices.cs3")
+  tardir("patch_inputdata/patchGHGprices", "patch_inputdata/patchGHGprices.tgz")
+  
+  # scen56 <- magclass::read.magpie("modules/56_ghg_policy/input/f56_emis_policy.csv",file_type = "cs3")
+  # scen56 <- magclass::getNames(scen56,dim=1)
+  # 
+  # sets <- list(list(name = "ghgscen56",
+  #                   desc = "ghg price scenarios",
+  #                   items = ghgscen56),
+  #              list(name = "scen56",
+  #                   desc = "emission policy scenarios",
+  #                   items = scen56))
+  # gms::writeSets(sets, "modules/56_ghg_policy/price_aug22/sets.gms")
+  unlink("patch_inputdata/patchGHGprices", recursive=TRUE)
+  return(getNames(GHG, dim = 2))
 }
+ghgscen56 <- calc_ghgprice()
 
 
-cfg$output <- c("rds_report")
-cfg$sequential <- TRUE
-cfg$title <- .title(cfg, paste(ssp,"1p5deg","PricePeatOff","AffEndo",sep="-"))
-cfg <- setScenario(cfg,c(ssp,"NDC","rcp1p9","ForestryEndo"))
-#cfg$input['cellular'] <- "rev4.104_h12_6aa915b6_cellularmagpie_c400_MRI-ESM2-0-ssp119_lpjml-8e6c5eb1.tgz"
-cfg$input['cellular'] <- "rev4.104_h12_4c83b5e5_cellularmagpie_c500_MRI-ESM2-0-ssp119_lpjml-8e6c5eb1.tgz"
-cfg$gms$c56_mute_ghgprices_until <- "y2030"
-cfg$gms$c56_pollutant_prices <- paste0("R32M46-",ifelse(ssp=="SSP2",paste0(ssp,"EU"),ssp),"-PkBudg650")
-cfg$gms$c60_2ndgen_biodem    <- paste0("R32M46-",ifelse(ssp=="SSP2",paste0(ssp,"EU"),ssp),"-PkBudg650")
-cfg$gms$c56_emis_policy <- "reddnatveg_nosoil_nopeat"
-start_run(cfg, codeCheck = FALSE)
-cfg$sequential <- FALSE
-
-PricePeatOff <- .title(cfg, paste(ssp,"1p5deg","PricePeatOff","AffEndo",sep="-"))
-
-#limitAff runs
-cfg$output <- c("extra/highres", "extra/disaggregation", "rds_report", "rds_report_iso")
-
-cfg$title <- .title(cfg, paste(ssp,"NDC",sep="-"))
-cfg <- setScenario(cfg,c(ssp,"NDC","rcp4p5","ForestryEndo"))
-#cfg$input['cellular'] <- "rev4.104_h12_3d941455_cellularmagpie_c400_MRI-ESM2-0-ssp245_lpjml-8e6c5eb1.tgz"
-cfg$input['cellular'] <- "rev4.104_h12_9ab12c13_cellularmagpie_c500_MRI-ESM2-0-ssp245_lpjml-8e6c5eb1.tgz"
-cfg <- limitAff(cfg,file.path("output",PricePeatOff,"fulldata.gdx"))
+cfg$title <- .title(cfg, paste(ssp,"Ref",sep="-"))
 cfg$gms$c56_mute_ghgprices_until <- "y2150"
-cfg$gms$c56_pollutant_prices <- paste0("R32M46-",ifelse(ssp=="SSP2",paste0(ssp,"EU"),ssp),"-NDC")
-cfg$gms$c60_2ndgen_biodem    <- paste0("R32M46-",ifelse(ssp=="SSP2",paste0(ssp,"EU"),ssp),"-NDC")
+cfg$gms$c56_pollutant_prices <- "T0-CO2"
 cfg$gms$c56_emis_policy <- "reddnatveg_nosoil_nopeat"
+download_and_update(cfg)
 start_run(cfg, codeCheck = FALSE)
 
-cfg$title <- .title(cfg, paste(ssp,"1p5deg","PricePeatOff",sep="-"))
-cfg <- setScenario(cfg,c(ssp,"NDC","rcp1p9","ForestryEndo"))
-#cfg$input['cellular'] <- "rev4.104_h12_6aa915b6_cellularmagpie_c400_MRI-ESM2-0-ssp119_lpjml-8e6c5eb1.tgz"
-cfg$input['cellular'] <- "rev4.104_h12_4c83b5e5_cellularmagpie_c500_MRI-ESM2-0-ssp119_lpjml-8e6c5eb1.tgz"
-cfg <- limitAff(cfg,file.path("output",PricePeatOff,"fulldata.gdx"))
-cfg$gms$c56_mute_ghgprices_until <- "y2030"
-cfg$gms$c56_pollutant_prices <- paste0("R32M46-",ifelse(ssp=="SSP2",paste0(ssp,"EU"),ssp),"-PkBudg650")
-cfg$gms$c60_2ndgen_biodem    <- paste0("R32M46-",ifelse(ssp=="SSP2",paste0(ssp,"EU"),ssp),"-PkBudg650")
-cfg$gms$c56_emis_policy <- "reddnatveg_nosoil_nopeat"
-start_run(cfg, codeCheck = FALSE)
 
-cfg$title <- .title(cfg, paste(ssp,"1p5deg","PricePeatCO2",sep="-"))
-cfg <- setScenario(cfg,c(ssp,"NDC","rcp1p9","ForestryEndo"))
-#cfg$input['cellular'] <- "rev4.104_h12_6aa915b6_cellularmagpie_c400_MRI-ESM2-0-ssp119_lpjml-8e6c5eb1.tgz"
-cfg$input['cellular'] <- "rev4.104_h12_4c83b5e5_cellularmagpie_c500_MRI-ESM2-0-ssp119_lpjml-8e6c5eb1.tgz"
-cfg <- limitAff(cfg,file.path("output",PricePeatOff,"fulldata.gdx"))
-cfg$gms$c56_mute_ghgprices_until <- "y2030"
-cfg$gms$c56_pollutant_prices <- paste0("R32M46-",ifelse(ssp=="SSP2",paste0(ssp,"EU"),ssp),"-PkBudg650")
-cfg$gms$c60_2ndgen_biodem    <- paste0("R32M46-",ifelse(ssp=="SSP2",paste0(ssp,"EU"),ssp),"-PkBudg650")
-cfg$gms$c56_emis_policy <- "reddnatveg_nosoil_peatCO2only"
-start_run(cfg, codeCheck = FALSE)
+cfg$gms$c56_mute_ghgprices_until <- "y2025"
 
-cfg$title <- .title(cfg, paste(ssp,"1p5deg","PricePeatGHG",sep="-"))
-cfg <- setScenario(cfg,c(ssp,"NDC","rcp1p9","ForestryEndo"))
-#cfg$input['cellular'] <- "rev4.104_h12_6aa915b6_cellularmagpie_c400_MRI-ESM2-0-ssp119_lpjml-8e6c5eb1.tgz"
-cfg$input['cellular'] <- "rev4.104_h12_4c83b5e5_cellularmagpie_c500_MRI-ESM2-0-ssp119_lpjml-8e6c5eb1.tgz"
-cfg <- limitAff(cfg,file.path("output",PricePeatOff,"fulldata.gdx"))
-cfg$gms$c56_mute_ghgprices_until <- "y2030"
-cfg$gms$c56_pollutant_prices <- paste0("R32M46-",ifelse(ssp=="SSP2",paste0(ssp,"EU"),ssp),"-PkBudg650")
-cfg$gms$c60_2ndgen_biodem    <- paste0("R32M46-",ifelse(ssp=="SSP2",paste0(ssp,"EU"),ssp),"-PkBudg650")
-cfg$gms$c56_emis_policy <- "reddnatveg_nosoil"
-start_run(cfg, codeCheck = FALSE)
-
-cfg$title <- .title(cfg, paste(ssp,"1p5deg","PricePeatGHG_CH4GWP20",sep="-"))
-cfg <- setScenario(cfg,c(ssp,"NDC","rcp1p9","ForestryEndo"))
-#cfg$input['cellular'] <- "rev4.104_h12_6aa915b6_cellularmagpie_c400_MRI-ESM2-0-ssp119_lpjml-8e6c5eb1.tgz"
-cfg$input['cellular'] <- "rev4.104_h12_4c83b5e5_cellularmagpie_c500_MRI-ESM2-0-ssp119_lpjml-8e6c5eb1.tgz"
-cfg <- limitAff(cfg,file.path("output",PricePeatOff,"fulldata.gdx"))
-cfg$gms$c56_mute_ghgprices_until <- "y2030"
-cfg$gms$c56_pollutant_prices <- paste0("R32M46-",ifelse(ssp=="SSP2",paste0(ssp,"EU"),ssp),"-PkBudg650")
-cfg$gms$c60_2ndgen_biodem    <- paste0("R32M46-",ifelse(ssp=="SSP2",paste0(ssp,"EU"),ssp),"-PkBudg650")
-cfg$gms$c56_emis_policy <- "reddnatveg_nosoil_CH4GWP20"
-start_run(cfg, codeCheck = FALSE)
+for (tax in ghgscen56) {
+  for (peat in c("peatOff", "peatOn")) {
+    cfg$title <- .title(cfg, paste(ssp,tax,peat,sep="-"))
+    cfg$gms$c56_pollutant_prices <- tax
+    cfg$gms$c56_emis_policy <- if (peat=="peatOff") "reddnatveg_nosoil_nopeat" else "reddnatveg_nosoil"
+    start_run(cfg, codeCheck = FALSE)
+  }
+}
