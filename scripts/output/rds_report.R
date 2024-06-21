790e82920b60289ce031873f78ddd3a241e66aa7
florianh <humpenoeder@pik-potsdam.de>
2024-06-21 14:04:28 +0200

    bugfix

--- scripts/output/rds_report.R
+++ scripts/output/rds_report.R
@@ -39,8 +39,7 @@ resultsarchive <- "/p/projects/rd3mod/models/results/magpie"
 report <- getReport(gdx, scenario = cfg$title, dir = outputdir)
 
 for (mapping in c("AR6", "NAVIGATE")) {
-  missingVariables <- setdiff(getMappingVariables(mapping,"M"),getNames(report,dim="variable"))
-  missingVariables <- deletePlus(missingVariables)
+  missingVariables <- setdiff(deletePlus(getMappingVariables(mapping,"M")),getNames(report,dim="variable"))
   if (length(missingVariables) > 0) {
     warning("# The following ", length(missingVariables), " variables are expected in the piamInterfaces package ",
             "for mapping ", mapping, ", but cannot be found in the MAgPIE report.\nPlease either fix in magpie4 or adjust the mapping in piamInterfaces.\n- ",
