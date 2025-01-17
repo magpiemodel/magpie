3d433ba9d991725be15847907d395c93d574afb9
Pascal Sauer <156898545+pascal-sauer@users.noreply.github.com>
2025-01-17 14:07:09 +0100

    start script to be run on release

--- scripts/start/test_runs.R
+++ scripts/start/test_runs.R
@@ -26,45 +26,8 @@ download_and_update(cfg)
 # create additional information to describe the runs
 cfg$info$flag <- "weeklyTests"
 
-cfg$results_folder <- "output/:title:"
-cfg$force_replace <- TRUE
-
-# support function to create standardized title
-.title <- function(cfg, ...) return(paste(cfg$info$flag, sep="_",...))
-
-# Single time step run
-timeSteps <- cfg$gms$c_timesteps
-cfg$gms$c_timesteps <- 1
-cfg$title <- .title(cfg, "singleTimeStep")
-start_run(cfg, codeCheck = TRUE)
-cfg$gms$c_timesteps <- timeSteps
-
-
-# Reference and Policy run for SSP1, SSP2 and SSP5
-for(ssp in c("SSP1","SSP2","SSP5")) {
-
-  cfg$title <- .title(cfg, paste(ssp,"Ref",sep="-"))
-  cfg <- setScenario(cfg,c(ssp,"NPI","rcp7p0"))
-  cfg$gms$c56_mute_ghgprices_until <- "y2150"
-  cfg$gms$c56_pollutant_prices <- paste0("R32M46-", if (ssp=="SSP2") "SSP2EU" else ssp,"-NPi")
-  cfg$gms$c60_2ndgen_biodem    <- paste0("R32M46-", if (ssp=="SSP2") "SSP2EU" else ssp,"-NPi")
-  start_run(cfg, codeCheck = FALSE)
-
-  cfg$title <- .title(cfg, paste(ssp,"NDC",sep="-"))
-  cfg <- setScenario(cfg,c(ssp,"NDC","rcp4p5"))
-  cfg$gms$c56_mute_ghgprices_until <- "y2150"
-  cfg$gms$c56_pollutant_prices <- paste0("R32M46-", if (ssp=="SSP2") "SSP2EU" else ssp,"-NDC")
-  cfg$gms$c60_2ndgen_biodem    <- paste0("R32M46-", if (ssp=="SSP2") "SSP2EU" else ssp,"-NDC")
-  start_run(cfg, codeCheck = FALSE)
-
-  cfg$title <- .title(cfg, paste(ssp,"PkBudg650",sep="-"))
-  cfg <- setScenario(cfg,c(ssp,"NDC","rcp1p9"))
-  cfg$gms$c56_mute_ghgprices_until <- "y2030"
-  cfg$gms$c56_pollutant_prices <- paste0("R32M46-", if (ssp=="SSP2") "SSP2EU" else ssp,"-PkBudg650")
-  cfg$gms$c60_2ndgen_biodem    <- paste0("R32M46-", if (ssp=="SSP2") "SSP2EU" else ssp,"-PkBudg650")
-  start_run(cfg, codeCheck = FALSE)
-
-}
+source("scripts/runTestRuns.R")
+runTestRuns(cfg)
 
 #####################################################
 ### FSEC Test runs (BAU + FSDP) with FSEC regions ###
