39539c396585ca55ddb2b4e153b89044b52cd8d4
Pascal Sauer <156898545+pascal-sauer@users.noreply.github.com>
2025-01-22 15:31:05 +0100

    minimal changes

--- scripts/start/test_runs.R
+++ scripts/start/test_runs.R
@@ -24,10 +24,52 @@ source("config/default.cfg")
 download_and_update(cfg)
 
 # create additional information to describe the runs
-cfg$info$flag <- "weeklyTests"
+arguments <- commandArgs(trailingOnly = TRUE)
+if (length(arguments) == 1) {
+  cfg$info$flag <- paste0("release-", arguments)
+} else {
+  cfg$info$flag <- "weeklyTests"
+}
 
-source("scripts/runTestRuns.R")
-runTestRuns(cfg)
+cfg$results_folder <- "output/:title:"
+cfg$force_replace <- TRUE
+
+# support function to create standardized title
+.title <- function(cfg, ...) return(paste(cfg$info$flag, sep="_",...))
+
+# Single time step run
+timeSteps <- cfg$gms$c_timesteps
+cfg$gms$c_timesteps <- 1
+cfg$title <- .title(cfg, "singleTimeStep")
+start_run(cfg, codeCheck = TRUE)
+cfg$gms$c_timesteps <- timeSteps
+
+
+# Reference and Policy run for SSP1, SSP2 and SSP5
+for(ssp in c("SSP1","SSP2","SSP5")) {
+
+  cfg$title <- .title(cfg, paste(ssp,"Ref",sep="-"))
+  cfg <- setScenario(cfg,c(ssp,"NPI","rcp7p0"))
+  cfg$gms$c56_mute_ghgprices_until <- "y2150"
+  cfg$gms$c56_pollutant_prices <- paste0("R32M46-", if (ssp=="SSP2") "SSP2EU" else ssp,"-NPi")
+  cfg$gms$c60_2ndgen_biodem    <- paste0("R32M46-", if (ssp=="SSP2") "SSP2EU" else ssp,"-NPi")
+  start_run(cfg, codeCheck = FALSE)
+
+  cfg$title <- .title(cfg, paste(ssp,"NDC",sep="-"))
+  cfg <- setScenario(cfg,c(ssp,"NDC","rcp4p5"))
+  cfg$gms$c56_mute_ghgprices_until <- "y2150"
+  cfg$gms$c56_pollutant_prices <- paste0("R32M46-", if (ssp=="SSP2") "SSP2EU" else ssp,"-NDC")
+  cfg$gms$c60_2ndgen_biodem    <- paste0("R32M46-", if (ssp=="SSP2") "SSP2EU" else ssp,"-NDC")
+  start_run(cfg, codeCheck = FALSE)
+
+  cfg$title <- .title(cfg, paste(ssp,"PkBudg650",sep="-"))
+  cfg <- setScenario(cfg,c(ssp,"NDC","rcp1p9"))
+  cfg$gms$c56_mute_ghgprices_until <- "y2030"
+  cfg$gms$c56_pollutant_prices <- paste0("R32M46-", if (ssp=="SSP2") "SSP2EU" else ssp,"-PkBudg650")
+  cfg$gms$c60_2ndgen_biodem    <- paste0("R32M46-", if (ssp=="SSP2") "SSP2EU" else ssp,"-PkBudg650")
+  start_run(cfg, codeCheck = FALSE)
+
+}
 
 #####################################################
 ### FSEC Test runs (BAU + FSDP) with FSEC regions ###
