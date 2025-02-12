00daca188879ee2372e7c3220121c1725a4282a4
Pascal Sauer <156898545+pascal-sauer@users.noreply.github.com>
2025-02-12 17:16:14 +0100

    undo changes to rds_report

--- scripts/output/rds_report.R
+++ scripts/output/rds_report.R
@@ -11,7 +11,6 @@
 # position: 2
 # ---------------------------------------------------------------
 
-# note: set environment variable DATA_CHANGELOG_PATH to prepend to data changelog file
 
 library(magclass)
 library(magpie4)
@@ -38,7 +37,6 @@ resultsarchive <- "/p/projects/rd3mod/models/results/magpie"
 
 
 report <- getReport(gdx, scenario = cfg$title, dir = outputdir)
-
 if (!all(grepl(" \\(([^\\()]*)\\)($|\\.)", getNames(report, fulldim = TRUE)$variable))) {
   warning("Variables should be in the format 'name (unit)' (the space between name and unit is important), ",
           "but the following are not:\n",
@@ -55,85 +53,6 @@ for (mapping in c("AR6", "NAVIGATE", "SHAPE", "AR6_MAgPIE")) {
             "Please either fix in magpie4 or adjust the mapping in piamInterfaces.\n- ",
             paste(missingVariables, collapse = ",\n- "), "\n")
   }
-}
-
-# append to data changelog if DATA_CHANGELOG_PATH is set
-changelog <- Sys.getenv("DATA_CHANGELOG_PATH")
-if (changelog != "") {
-  message("Appending to data changelog at ", changelog)
-
-  # TODO move addToDataChangelog to another place (gms? magpie4?)
-
-  #' addToDataChangelog
-  #'
-  #' Prepend data from the given report to the changelog.
-  #'
-  #' @param report data.frame as obtained by readRDS("report.rds")
-  #' @param changelog Path to the changelog file
-  #' @param versionId The model version identifier, e.g. a release number like 4.9.1 or a data like 2025-02-01
-  #' @param years For which years the variables should be read and put into the changelog
-  #' @param variables Which variables to read from the report (e.g. "Emissions|CO2|Land|+|Land-use Change")
-  #' @param ... Reserved for future expansion.
-  #' @param maxEntries The maximum number of versionIds to keep in the changelog, the oldest one is removed first.
-  #' @param roundDigits Numbers are rounded to this many decimal places before being written to the changelog.
-  #'
-  #' @author Pascal Sauer
-  #' @export
-  addToDataChangelog <- function(report, changelog, versionId, years, variables, ...,
-                                 maxEntries = 15, roundDigits = 2) {
-    x <- report[report$region == "World"
-                & report$variable %in% variables
-                & report$period %in% years,
-                c("variable", "period", "value")]
-    notFound <- setdiff(variables, x$variable)
-    if (length(notFound) > 0) {
-      warning("No data-changelog data found for ", paste(notFound, collapse = ", "))
-    }
-
-    # shorten variable names
-    variableNames <- names(variables)
-    names(variableNames) <- variables
-    x$variable <- variableNames[as.character(x$variable)]
-
-    colnames(x)[ncol(x)] <- rowname
-
-    out <- data.frame(version = setdiff(colnames(x), c("variable", "period")))
-    for (variableName in variableNames) {
-      for (year in years) {
-        newColumn <- x[x$variable == variableName & x$period == year, 3]
-        newColumn <- round(newColumn, roundDigits)
-        out <- cbind(out, newColumn)
-        colnames(out)[ncol(out)] <- paste0(variableName, year)
-      }
-    }
-
-    if (file.exists(changelog)) {
-      out <- rbind(out, read.csv(changelog))
-      out <- out[seq_len(min(maxEntries, nrow(out))), ]
-    }
-    write.csv(out, changelog, quote = FALSE, row.names = FALSE)
-    return(invisible(out))
-  }
-
-  variables <- c(
-    lucEmis = "Emissions|CO2|Land|+|Land-use Change", # +++++
-    tau = "Productivity|Landuse Intensity Indicator Tau", # +++++
-    cropland = "Resources|Land Cover|+|Cropland", # ++++++
-    irrigated = "Resources|Land Cover|Cropland|Area actually irrigated", # +++
-    pasture = "Resources|Land Cover|+|Pastures and Rangelands", # ++++
-    forest = "Resources|Land Cover|+|Forest", # +++
-    other = "Resources|Land Cover|+|Other Land", # ++
-    production = "Production", # +++ (in contrast to all other indicators here, this should be robust to calibration issues, but indicate changes in demand/trade)
-    costs = "Costs", # ++
-    foodExp = "Household Expenditure|Food|Expenditure" # +
-  )
-
-  load("runstatistics.rda") # load 'stats'
-
-  addToDataChangelog(report, changelog,
-                     rowname = format(stats$date, "%Y-%m-%d"),
-                     years = c(2050, 2100),
-                     variables = variables)
 }
 
 write.report(report, file = mif)
