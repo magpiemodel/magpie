3e6390c1331d011f8b0173b5775ab17c08723ee0
florianh <humpenoeder@pik-potsdam.de>
2025-03-30 08:30:05 +0200

    ScenarioMIP start script version increase

--- scripts/start/projects/project_ScenarioMIP.R
+++ scripts/start/projects/project_ScenarioMIP.R
@@ -21,7 +21,7 @@ source("scripts/start_functions.R")
 source("config/default.cfg")
 
 # create additional information to describe the runs
-cfg$info$flag <- "SMIP58"
+cfg$info$flag <- "SMIP59"
 
 cfg$results_folder <- "output/:title:"
 cfg$force_replace <- TRUE
