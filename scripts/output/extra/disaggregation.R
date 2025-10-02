5359301d97f8adbfd784c8c8093ae8ab237b6174
Georg Schroeter <Georg.Schroeter@pik-potsdam.de>
2025-10-02 14:53:07 +0200

    disaggBII removed deprecated dir

--- scripts/output/extra/disaggregation.R
+++ scripts/output/extra/disaggregation.R
@@ -477,7 +477,7 @@ land_ini_hr <- land_ini_hr[, , getNames(land_ini_lr)]
 getSets(land_ini_hr)["d3.1"] <- "land"
 
 # Disaggregate BII values to high resolution
-bii_hr <- .dissagBII(gdx, map_file = map_file, dir = outputdir)
+bii_hr <- .dissagBII(gdx, map_file = map_file)
 
 # Disaggregate land pools for BII estimation
 land_bii_hr <- interpolateAvlCroplandWeighted(
