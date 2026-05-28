d2dd6722cc921b09432f7243a4df55f577aac6a5
Patrick R <hi@patrickrein.de>
2026-05-28 10:32:10 +0200

    Simplifies devcontainer setup for magpie main repository

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,22 +1,6 @@
 #!/bin/bash
 
-git clone https://github.com/pik-piam/mrtutorial.git /workspaces/mrtutorial
-mkdir /workspaces/madrat-main-folder
-
 if [ -n "$GAMS_LICENSE" ]; then
   mkdir -p /opt/var/gams
   echo "$GAMS_LICENSE" > /opt/var/gams/gamslice.txt
 fi
-
-cd /workspaces/magpie
-Rscript -e '2 + 2; "dummy evaluation to trigger renv install"'
-
-cd /workspaces/mrtutorial
-Rscript -e 'pak::pak(".")'
-echo "" >> ~/.bashrc
-echo "export MADRAT_MAINFOLDER='/workspaces/madrat-main-folder'" >> ~/.bashrc
-echo "" >> ~/.bashrc
-
-# Also set the mainfolder for the current session already,
-# as the .bashrc is only loaded later.
-MADRAT_MAINFOLDER='/workspaces/madrat-main-folder'
